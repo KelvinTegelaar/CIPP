ac4ee5edd55db4a9ec209946d5fd7e7fe1f69d55
redanthrax <redanthraxx@gmail.com>
2021-10-15 14:42:49 -0700

    Added persistance for tenant selection

--- js/menu-loader.js
+++ js/menu-loader.js
@@ -1,21 +1,23 @@
 $(document).ready(function () {
-
+    let tenant = getParameterByName("Tenantfilter");
     let searchParams = new URLSearchParams(window.location.search)
     if (searchParams.has('page')) {
         let param = searchParams.get('page')
-
-        $('#bodycontent').load(param + '.html'), function () {
-        };
+        $('#bodycontent').load(param + '.html');
     }
 
     $(".SpecialNavLink").on('click', 'a', function (e) {
         e.preventDefault();
-        history.pushState(null, null, '?page=' + $(this).attr('href'));
-        $('#bodycontent').load($(this).attr('href') + '.html', function () {
+        if(tenant){
+            history.pushState(null, null, '?page=' + $(this).attr('href') + '&Tenantfilter=' + tenant);
+        }
+        else {
+            history.pushState(null, null, '?page=' + $(this).attr('href'));
+        }
 
-        });
-
+        $('#bodycontent').load($(this).attr('href') + '.html')
     });
+
     var jsonOptions = (function () {
         var json = null;
         $.ajax({
@@ -32,7 +34,13 @@ $(document).ready(function () {
 
     $('#usernamelower').text(jsonOptions.clientPrincipal.userDetails);
     $('#usernameupper').text(jsonOptions.clientPrincipal.userDetails);
-
-
 });
 
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
