65b0eff16de95073cf87b2c6e32348da504cea0a
msp4msps <msp4msps@tminus365.com>
2021-10-17 14:51:14 -0600

    datatable for MFA report

---
+++ js/datatables/datatablesMFAreport.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  let searchParams = new URLSearchParams(window.location.search);
+  if (searchParams.has("page")) {
+    var TenantID = searchParams.get("Tenantfilter");
+  }
+  var todayDate = new Date().toISOString().slice(0, 10);
+  $(".datatable-3").dataTable({
+    language: {
+      paginate: {
+        next: '<i class="fas fa-arrow-right"></i>',
+        previous: '<i class="fas fa-arrow-left"></i>',
+      },
+    },
+    columnDefs: [{ className: "dt-center", targets: [-1] }],
+    deferRender: true,
+    pageLength: 25,
+    responsive: true,
+    ajax: {
+      url: "/api/ListMFAStats?Tenantfilter=" + TenantID,
+      dataSrc: "",
+    },
+    dom: "fBlrtip",
+    buttons: [
+      { extend: "copyHtml5", className: "btn btn-primary" },
+      {
+        extend: "excelHtml5",
+        className: "btn btn-primary",
+        title: "Mailbox Statistics - " + TenantID + " - " + todayDate,
+      },
+      {
+        extend: "csvHtml5",
+        className: "btn btn-primary",
+        title: "Mailbox Statistics - " + TenantID + " - " + todayDate,
+      },
+      {
+        extend: "pdfHtml5",
+        className: "btn btn-primary",
+        orientation: "landscape",
+        title: "Mailbox Statistics - " + TenantID + " - " + todayDate,
+      },
+    ],
+    columns: [
+      { data: "Display Name" },
+      { data: "UserPrincipalName" },
+      { data: "Per User MFA" },
+      { data: "MFA Registered via CA Policy/Security Defaults" },
+    ],
+    order: [[0, "asc"]],
+  });
+  $(".dataTables_paginate").addClass("btn-group datatable-pagination");
+  $(".dataTables_paginate > a").wrapInner("<span />");
+});
