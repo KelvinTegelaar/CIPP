459ae6f761eff1271c445538ec556afe1e40c473
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-10-28 15:48:46 +0200

    bpa stuff

--- js/datatables/datatablesBestPracticeAnalyser.js
+++ js/datatables/datatablesBestPracticeAnalyser.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
                     text: 'Force Refresh All Data',
                     className: 'btn btn-primary btn-sm',
                     action: function (e, dt, button, config) {
-                        $('#APIContent').html('<label class="form-check-label" >Are you sure you want to start the Best Practice Analysis? This will slow down normal usage considerably.</label><br><nothing class="APIConfirmed"><a href="/api/BestPracticeAnalyser_OrchestrationStarter"><button id="Confirmed" class="btn btn-primary APIConfirmed">Yes</button></a></nothing><nothing class="APIDenied">  <button data-bs-dismiss="modal" class="btn btn-primary APIDenied">No</button>');
+                        $('#APIContent').html('<label class="form-check-label" >Are you sure you want to force the Best Practice Analysis to run? This will slow down normal usage considerably.</label><br><nothing class="APIConfirmed"><a href="/api/BestPracticeAnalyser_OrchestrationStarter"><button id="Confirmed" class="btn btn-primary APIConfirmed">Yes</button></a></nothing><nothing class="APIDenied">  <button data-bs-dismiss="modal" class="btn btn-primary APIDenied">No</button>');
 
                         document.getElementById("PopModal").click();
                     }
