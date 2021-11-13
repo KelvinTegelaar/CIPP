a7c8a4c7d1d1671c3e582733c1887805789e85a5
k-grube <grube.kevin@gmail.com>
2021-11-12 23:15:25 -0800

    Administration/Tenants display

--- src/store/modules/tenants.js
+++ src/store/modules/tenants.js
@@ -2,6 +2,7 @@ const initialState = {
   tenants: [],
   selectedTenant: {},
   loading: false,
+  loaded: false,
 }
 
 const LOADING = 'tenants/LOADING'
@@ -13,11 +14,11 @@ const SET_TENANT = 'tenants/SET_TENANT'
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case LOADING:
-      return { ...state, loading: true }
+      return { ...state, loading: true, loaded: false }
     case LOADING_SUCCESS:
-      return { ...state, tenants: action.result, loading: false }
+      return { ...state, tenants: action.result, loading: false, loaded: true }
     case LOADING_FAILURE:
-      return { ...state, loading: false, tenants: [] }
+      return { ...state, loading: false, loaded: false, tenants: [] }
     case SET_TENANT:
       return { ...state, selectedTenant: action.tenant }
     default:
