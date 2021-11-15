22947ef026c19f81ad37374d973f818a7da6e790
redanthrax <redanthraxx@gmail.com>
2021-11-15 10:58:10 -0800

    prettied the profile page

--- src/views/profile/ViewProfile.js
+++ src/views/profile/ViewProfile.js
@@ -1,9 +1,33 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { CCard, CCardImage, CCardTitle, CCardBody, CListGroup, CListGroupItem } from '@coreui/react'
+import avatar0 from './../../assets/images/avatars/0.jpg'
 
 const ViewProfile = () => {
   const profile = useSelector((state) => state.profile)
-  return <div>{JSON.stringify(profile)}</div>
+  console.log(profile)
+  return (
+    <CCard style={{ width: '30rem' }}>
+      <CCardImage orientation="top" src={avatar0} />
+      <CCardBody>
+        <CCardTitle>Identity: {profile.clientPrincipal.userDetails}</CCardTitle>
+      </CCardBody>
+      <CListGroup flush>
+        <CListGroupItem>
+          Identity Provider: {profile.clientPrincipal.identityProvider}
+        </CListGroupItem>
+        <CListGroupItem>User ID: {profile.clientPrincipal.userId}</CListGroupItem>
+        <CListGroupItem>
+          Roles ({profile.clientPrincipal.userRoles.length})
+          <ul>
+            {profile.clientPrincipal.userRoles.map((r, index) => (
+              <li key={index}>{r}</li>
+            ))}
+          </ul>
+        </CListGroupItem>
+      </CListGroup>
+    </CCard>
+  )
 }
 
 export default ViewProfile
