b7a8c00324540ebf62cf941c1e33a09920872cad
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-11-17 20:39:03 +0100

    prepwork listing compromised mailbox

---
+++ js/BECList.js
@@ -0,0 +1,39 @@
+$(".postbec").click(function (e) {
+    var form = $('#BECForm');
+    var postdata = getFormData(form);
+    $.ajax({
+        'async': true,
+        'global': false,
+        'url': 'api/ExecBECCheck',
+        'dataType': "json",
+        'contentType': "application/json",
+        'type': "post",
+        'data': JSON.stringify(postdata),
+        'success': function (data) {
+            var devicestable = []
+
+            data.SuspectUserDevices.forEach(function (item) {
+                devicestable.push(`<tr>
+                  <td>${item.ClientType}</td>
+                <td>${item.FirstSyncTime}</td>
+                <td>${item.DeviceUserAgent}</td>
+                </tr>`)
+            });
+            var devicetablecomplete = `
+            Active Sync Devices:
+            <table class="table">
+            <tr>
+            <th>Client Type</th>
+            <th>First Synced</th>
+            <th>User Agent</th>
+            </tr>
+            ${devicestable}
+            </table>`
+
+            $("#investigationdata").html(devicetablecomplete)
+        },
+        'error': function (xhr, ajaxOptions, thrownError) {
+            $("#investigationdata").html('NONO')
+        }
+    });
+})
