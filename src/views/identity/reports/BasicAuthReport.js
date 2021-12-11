a8019cf4f90d30a5f44bf9b218a3dc7126db46a4
k-grube <grube.kevin@gmail.com>
2021-12-10 22:46:50 -0800

    Update basic auth

--- src/views/identity/reports/BasicAuthReport.js
+++ src/views/identity/reports/BasicAuthReport.js
@@ -1,16 +1,7 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { loadBasicAuthReport } from '../../../store/modules/reports'
+import React from 'react'
+import { useSelector } from 'react-redux'
 import TenantSelector from '../../../components/cipp/TenantSelector'
-import ToolkitProvider, { CSVExport, Search } from 'react-bootstrap-table2-toolkit'
-import { CButton } from '@coreui/react'
-import BootstrapTable from 'react-bootstrap-table-next'
-import paginationFactory from 'react-bootstrap-table2-paginator'
-import { Loading } from '../../../components'
-
-const { SearchBar } = Search
-const { ExportCSVButton } = CSVExport
-const pagination = paginationFactory()
+import CippDatatable from '../../../components/cipp/CippDatatable'
 
 const columns = [
   {
@@ -26,60 +17,21 @@ const columns = [
 ]
 
 const BasicAuthReport = () => {
-  const dispatch = useDispatch()
   const tenant = useSelector((state) => state.app.currentTenant)
-  const basicAuth = useSelector((state) => state.reports.basicAuth)
-
-  const tenantSelected = tenant && tenant.defaultDomainName
-
-  const handleTenantSelect = (tenant) => {
-    dispatch(loadBasicAuthReport({ domain: tenant.defaultDomainName }))
-  }
-
-  useEffect(() => {
-    if (tenantSelected) {
-      dispatch(loadBasicAuthReport({ domain: tenant.defaultDomainName }))
-    }
-  }, [])
 
   return (
     <div>
-      <TenantSelector action={handleTenantSelect} />
+      <TenantSelector />
       <hr />
       <div className="bg-white rounded p-5">
         <h3>Basic Auth Report</h3>
-        {!tenantSelected && !basicAuth.loading && !basicAuth.loaded && <>Select a Tenant</>}
-        {basicAuth.loading && !basicAuth.loaded && (
-          <div style={{ padding: 20 }}>
-            <Loading />
-          </div>
-        )}
-        {basicAuth.loaded && !basicAuth.loading && (
-          <>
-            <hr />
-            <ToolkitProvider keyField="UPN" columns={columns} data={basicAuth.report} search>
-              {(props) => (
-                <div>
-                  {/* eslint-disable-next-line react/prop-types */}
-                  <SearchBar {...props.searchProps} />
-                  {/* eslint-disable-next-line react/prop-types */}
-                  <ExportCSVButton {...props.csvProps}>
-                    <CButton>CSV</CButton>
-                  </ExportCSVButton>
-                  <hr />
-                  {/*eslint-disable */}
-                  <BootstrapTable
-                    {...props.baseProps}
-                    pagination={pagination}
-                    striped
-                    wrapperClasses="table-responsive"
-                  />
-                  {/*eslint-enable */}
-                </div>
-              )}
-            </ToolkitProvider>
-          </>
-        )}
+        {Object.keys(tenant).length === 0 && <span>Select a tenant to get started.</span>}
+        <CippDatatable
+          reportName={`${tenant?.defaultDomainName}-Basic-Auth-Report`}
+          path="/api/ListBasicAuth"
+          columns={columns}
+          params={{ TenantFilter: tenant?.defaultDomainName }}
+        />
       </div>
     </div>
   )
