f8d89b8bfe973c4820b973d26d1779cb4e19fdcd
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2021-12-15 12:22:14 +0100

    editing profile

--- src/views/profile/ViewProfile.js
+++ src/views/profile/ViewProfile.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {
+  CRow,
+  CCol,
   CCard,
   CCardImage,
   CCardTitle,
@@ -10,36 +12,53 @@ import {
 } from '@coreui/react'
 import avatar0 from './../../assets/images/avatars/0.jpg'
 import { authApi } from '../../store/api/auth'
+import ThemeSwitcher from 'src/components/cipp/ThemeSwitcher'
 
 const ViewProfile = () => {
   const { data: profile, isLoading } = authApi.endpoints.loadClientPrincipal.useQueryState()
 
   return (
-    <CCard style={{ width: '30rem' }}>
-      {isLoading && <CSpinner />}
-      {!isLoading && (
-        <>
-          <CCardImage orientation="top" src={avatar0} />
-          <CCardBody>
-            <CCardTitle>Identity: {profile.clientPrincipal.userDetails}</CCardTitle>
-          </CCardBody>
-          <CListGroup flush>
-            <CListGroupItem>
-              Identity Provider: {profile.clientPrincipal.identityProvider}
-            </CListGroupItem>
-            <CListGroupItem>User ID: {profile.clientPrincipal.userId}</CListGroupItem>
-            <CListGroupItem>
-              Roles ({profile.clientPrincipal.userRoles.length})
-              <ul>
-                {profile.clientPrincipal.userRoles.map((r, index) => (
-                  <li key={index}>{r}</li>
-                ))}
-              </ul>
-            </CListGroupItem>
-          </CListGroup>
-        </>
-      )}
-    </CCard>
+    <>
+      <CRow xs={{ cols: 3 }} md={{ cols: 5 }} className="g-4">
+        <CCol xs>
+          <CCard className="w-100">
+            {isLoading && <CSpinner />}
+            {!isLoading && (
+              <>
+                <CCardImage orientation="top" src={avatar0} />
+                <CCardBody>
+                  <CCardTitle>{profile.clientPrincipal.userDetails}</CCardTitle>
+                </CCardBody>
+                <CListGroup flush>
+                  <CListGroupItem>
+                    Identity Provider: {profile.clientPrincipal.identityProvider}
+                  </CListGroupItem>
+                  <CListGroupItem>User ID: {profile.clientPrincipal.userId}</CListGroupItem>
+                  <CListGroupItem>
+                    Roles ({profile.clientPrincipal.userRoles.length})
+                    <ul>
+                      {profile.clientPrincipal.userRoles.map((r, index) => (
+                        <li key={index}>{r}</li>
+                      ))}
+                    </ul>
+                  </CListGroupItem>
+                </CListGroup>
+              </>
+            )}
+          </CCard>
+        </CCol>
+        <CCol xs>
+          <CCard className="w-50">
+            <ThemeSwitcher />
+          </CCard>
+        </CCol>
+        <CCol xs>
+          <CCard className="w-50">
+            <ThemeSwitcher />
+          </CCard>
+        </CCol>
+      </CRow>
+    </>
   )
 }
 
