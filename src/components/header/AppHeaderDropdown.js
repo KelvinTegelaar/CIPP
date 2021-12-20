8070e0462844758c80aacfafa034c17b67c60a31
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2021-12-20 21:02:00 +0100

    small prettification

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { CAvatar, CDropdown, CDropdownHeader, CDropdownMenu, CDropdownToggle } from '@coreui/react'
-import { faUser, faCog, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faCog, faBars, faBook } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
 import avatar0 from './../../assets/images/avatars/0.jpg'
@@ -22,9 +22,9 @@ const AppHeaderDropdown = () => {
           <FontAwesomeIcon icon={faUser} className="me-2" />
           Profile
         </Link>
-        <Link className="dropdown-item" to="/profile/settings">
-          <FontAwesomeIcon icon={faCog} className="me-2" />
-          Settings
+        <Link className="dropdown-item" to="/logs">
+          <FontAwesomeIcon icon={faBook} className="me-2" />
+          Logbook
         </Link>
       </CDropdownMenu>
     </CDropdown>
