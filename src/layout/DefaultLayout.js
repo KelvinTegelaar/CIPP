c804aa3d283260952ee269d1f0e84da6a034c959
k-grube <grube.kevin@gmail.com>
2021-12-20 17:45:27 -0800

    add fast switcher search

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,18 +1,31 @@
-import React, { useEffect, Suspense } from 'react'
+import React, { useEffect, Suspense, useCallback } from 'react'
 import { AppSidebar, AppFooter, AppHeader, FullScreenLoading } from '../components/index'
 import SharedModal from '../components/SharedModal'
 import Toasts from '../components/Toasts'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Outlet } from 'react-router-dom'
 import { CContainer } from '@coreui/react'
+import { toggleSwitcher } from '../store/features/switcher'
+import useKeyboardShortcut from '../hooks/useKeyboardShortcut'
+import FastSwitcher from '../components/FastSwitcher'
 
 const DefaultLayout = () => {
   const theme = useSelector((state) => state.app.currentTheme)
   useEffect(() => {
     document.body.classList.add(theme)
   })
+
+  const dispatch = useDispatch()
+  const handleFastSwitcher = useCallback(() => {
+    dispatch(toggleSwitcher())
+  }, [dispatch])
+
+  useKeyboardShortcut(['Control', 'K'], handleFastSwitcher, { overrideSystem: true })
+  useKeyboardShortcut(['Meta', 'K'], handleFastSwitcher)
+
   return (
     <div>
+      <FastSwitcher />
       <SharedModal />
       <Toasts />
       <AppSidebar />
