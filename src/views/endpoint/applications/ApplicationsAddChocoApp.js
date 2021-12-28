94d4c6585f4271ee1f6283b56c621daa25c5fe11
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2021-12-28 12:50:12 +0100

    adding chocoapp

--- src/views/endpoint/applications/ApplicationsAddChocoApp.js
+++ src/views/endpoint/applications/ApplicationsAddChocoApp.js
@@ -1,14 +1,5 @@
 import React from 'react'
-import {
-  CAlert,
-  CCard,
-  CCol,
-  CFormLabel,
-  CRow,
-  CCardHeader,
-  CCardBody,
-  CCardTitle,
-} from '@coreui/react'
+import { CAlert, CCard, CCol, CRow, CCardTitle, CCardHeader, CCardBody, CForm } from '@coreui/react'
 import { Field } from 'react-final-form'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
@@ -16,15 +7,9 @@ import { useDispatch, useSelector } from 'react-redux'
 import Wizard from '../../../components/Wizard'
 import WizardTableField from '../../../components/WizardTableField'
 import PropTypes from 'prop-types'
-import {
-  RFFCFormCheck,
-  RFFCFormInput,
-  RFFCFormSwitch,
-  RFFSelectSearch,
-} from '../../../components/RFFComponents'
+import { RFFCFormInput, RFFCFormRadio, RFFCFormSwitch } from '../../../components/RFFComponents'
 import { useListTenantsQuery } from '../../../store/api/tenants'
-import { TenantSelector } from 'src/components/cipp'
-import { useListUsersQuery } from 'src/store/api/users'
+import { useLazyGenericPostRequestQuery } from 'src/store/api/app'
 
 const Error = ({ name }) => (
   <Field
@@ -46,20 +31,27 @@ Error.propTypes = {
 }
 
 const requiredArray = (value) => (value && value.length !== 0 ? undefined : 'Required')
-const AddChocoApp = () => {
-  const tenantDomain = useSelector((state) => state.app.currentTenant.defaultDomainName)
-  const {
-    data: users = [],
-    isFetching: usersIsFetching,
-    error: usersError,
-  } = useListUsersQuery({ tenantDomain })
+
+const ApplyStandard = () => {
+  const { data: tenants = [] } = useListTenantsQuery()
+  const [genericPostRequest, postResults] = useLazyGenericPostRequestQuery()
 
   const dispatch = useDispatch()
 
   const handleSubmit = async (values) => {
-    alert(JSON.stringify(values, null, 2))
-    // @todo hook this up
-    // dispatch(applyStandards({ tenants: values.selectedTenants, standards: values.standards }))
+    // @todo: clean this up api sided so we don't need to perform weird tricks.
+    var result = Object.keys(values.standards).filter(function (x) {
+      if (values.standards[x] === false) {
+        delete values.standards[x]
+      }
+    })
+
+    const shippedTenants = values.selectedTenants.map(
+      (tenant) =>
+        (values.standards[`Select_${tenant.defaultDomainName}`] = tenant.defaultDomainName),
+    )
+    //filter on only objects that are 'true'
+    genericPostRequest({ url: 'api/AddStandardsDeploy', values: values.standards })
   }
 
   const formValues = {
@@ -68,118 +60,107 @@ const AddChocoApp = () => {
   }
 
   return (
-    <CCard>
+    <CCard className="col-8">
       <CCardHeader>
-        <CCardTitle className="text-primary">Add Chocolatey App Wizard</CCardTitle>
+        <CCardTitle className="text-primary">Standards Wizard</CCardTitle>
       </CCardHeader>
       <CCardBody>
         <CRow className="row justify-content-center">
-          <CCol xxl={8}>
-            <Wizard onSubmit={handleSubmit}>
+          <CCol xxl={12}>
+            <Wizard initialValues={{ ...formValues }} onSubmit={handleSubmit}>
               <Wizard.Page
                 title="Tenant Choice"
-                description="Choose the tenants for offboarding the user"
+                description="Choose the tenants to create the standard for."
               >
                 <center>
                   <h3 className="text-primary">Step 1</h3>
                   <h5 className="card-title mb-4">Choose a tenant</h5>
                 </center>
                 <hr className="my-4" />
-                <Field name="selectedTenants">{(props) => <TenantSelector />}</Field>
+                <Field name="selectedTenants" validate={requiredArray}>
+                  {(props) => (
+                    <WizardTableField
+                      keyField="customerId"
+                      data={tenants}
+                      columns={[
+                        {
+                          dataField: 'displayName',
+                          text: 'Tenant Name',
+                        },
+                        {
+                          dataField: 'defaultDomainName',
+                          text: 'Domain Name',
+                        },
+                      ]}
+                      fieldProps={props}
+                    />
+                  )}
+                </Field>
                 <Error name="selectedTenants" />
                 <hr className="my-4" />
               </Wizard.Page>
               <Wizard.Page
-                title="Select User"
-                description="Select the user to offboard from the tenant."
+                title="Select Standards"
+                description="Select which standards you want to apply."
               >
                 <center>
                   <h3 className="text-primary">Step 2</h3>
-                  <h5>Select the user that will be offboarded</h5>
+                  <h5 className="card-title mb-4">Supply the app information</h5>
                 </center>
                 <hr className="my-4" />
-                <div className="mb-2">
-                  <RFFSelectSearch
-                    label={'Users in ' + tenantDomain}
-                    values={users?.map((user) => ({
-                      value: user.id,
-                      name: user.displayName,
-                    }))}
-                    placeholder={!usersIsFetching ? 'Select user' : 'Loading...'}
-                    name="SelectedUser"
-                  />
-                  {usersError && <span>Failed to load list of users</span>}
-                </div>
-                <hr className="my-4" />
-              </Wizard.Page>
-              <Wizard.Page
-                title="Offboarding Settings"
-                description="Select the offboarding options."
-              >
-                <center>
-                  <h3 className="text-primary">Step 3</h3>
-                  <h5>Choose offboarding options</h5>
-                </center>
-                <hr className="my-4" />
-                <div className="mb-2">
-                  <RFFCFormSwitch name="RemoveLicenses" label="Remove Licenses" />
-                  <RFFCFormSwitch name="ConvertoSharedMailbox" label="Convert to Shared Mailbox" />
-                  <RFFCFormSwitch name="DisableUser" label="Disable Sign in" />
-                  <RFFCFormSwitch name="ResetPassword" label="Reset Password" />
-                  <RFFCFormSwitch name="RemoveGroups" label="Remove from all groups" />
-                  <RFFCFormSwitch name="HideGAL" label="Hide from Global Address List" />
-                  <CCol md={6}>
-                    <RFFCFormInput
-                      name="OOO"
-                      label="Out of Office"
-                      type="text"
-                      placeholder="leave blank to not set"
-                    />
-                  </CCol>
-                  <CCol md={6}>
-                    <RFFSelectSearch
-                      label="Give other user full access on mailbox without automapping"
-                      values={users?.map((user) => ({
-                        value: user.id,
-                        name: user.displayName,
-                      }))}
-                      placeholder={!usersIsFetching ? 'Select user' : 'Loading...'}
-                      name="UserNoAutomap"
-                    />
-                  </CCol>
-                  <CCol md={6}>
-                    <RFFSelectSearch
-                      label="Give other user full access on mailbox with automapping"
-                      values={users?.map((user) => ({
-                        value: user.id,
-                        name: user.displayName,
-                      }))}
-                      placeholder={!usersIsFetching ? 'Select user' : 'Loading...'}
-                      name="UserAutomap"
-                    />
-                  </CCol>
-                  <CCol md={6}>
-                    <RFFSelectSearch
-                      label="Give other user full access on Onedrive"
-                      values={users?.map((user) => ({
-                        value: user.id,
-                        name: user.displayName,
-                      }))}
-                      placeholder={!usersIsFetching ? 'Select user' : 'Loading...'}
-                      name="UserAutomapOneDrive"
-                    />
-                  </CCol>
-                  <RFFCFormSwitch name="Delete User" label="Delete user" />
-                </div>
+                <CForm onSubmit={handleSubmit}>
+                  <CRow>
+                    <CCol md={6}>
+                      <RFFCFormInput type="text" name="givenName" label="Chocolatey package name" />
+                    </CCol>
+                    <CCol md={6}>
+                      <RFFCFormInput type="text" name="surname" label="Application name" />
+                    </CCol>
+                  </CRow>
+                  <CRow>
+                    <CCol md={12}>
+                      <RFFCFormInput type="text" name="displayName" label="Description" />
+                    </CCol>
+                  </CRow>
+                  <CRow>
+                    <CCol md={12}>
+                      <RFFCFormInput
+                        type="text"
+                        name="mailNickname"
+                        label="Custom repository URL"
+                      />
+                    </CCol>
+                  </CRow>
+                  <RFFCFormSwitch name="InstallAsSystem" label="Install as system" />
+                  <RFFCFormRadio name="Assign" label="Do not assign"></RFFCFormRadio>
+                  <RFFCFormRadio name="Assign" label="Assign to all users"></RFFCFormRadio>
+                  <RFFCFormRadio name="Assign" label="Assign to all devices"></RFFCFormRadio>
+                  <RFFCFormRadio
+                    name="Assign"
+                    label="Assign to all users and devices"
+                  ></RFFCFormRadio>
+                </CForm>
                 <hr className="my-4" />
               </Wizard.Page>
               <Wizard.Page title="Review and Confirm" description="Confirm the settings to apply">
                 <center>
-                  <h3 className="text-primary">Step 4</h3>
-                  <h5 className="mb-4">Confirm and apply</h5>
-                  <hr className="my-4" />
+                  <h3 className="text-primary">Step 3</h3>
+                  <h5 className="card-title mb-4">Confirm and apply</h5>
                 </center>
-                <div className="mb-2">Show JSON here</div>
+                <hr className="my-4" />
+                {!postResults.isSuccess && (
+                  <CAlert color="warning" className="d-flex align-items-center">
+                    <FontAwesomeIcon color="warning" icon={faExclamationTriangle} size="2x" />
+                    <center>
+                      WARNING! Setting a standard will make changes to your tenants and set these
+                      standards on every 365 tenant you select. If you want to review only, please
+                      use the Best Practice Analyser.
+                    </center>
+                  </CAlert>
+                )}
+                {postResults.isSuccess && (
+                  <CAlert color="success">{postResults.data?.Results}</CAlert>
+                )}
                 <hr className="my-4" />
               </Wizard.Page>
             </Wizard>
@@ -190,4 +171,4 @@ const AddChocoApp = () => {
   )
 }
 
-export default AddChocoApp
+export default ApplyStandard
