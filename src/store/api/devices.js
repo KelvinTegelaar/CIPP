59b56a26f83c48cb32f5869356631869c1eb1da9
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2022-01-07 11:18:33 +0100

    fixes 503

--- src/store/api/devices.js
+++ src/store/api/devices.js
@@ -5,13 +5,21 @@ export const devicesApi = baseApi.injectEndpoints({
     listDevices: builder.query({
       query: ({ tenantDomain }) => ({ path: '/api/ListDevices', params: { tenantDomain } }),
     }),
+
     listUserDevices: builder.query({
       query: ({ userId, tenantDomain }) => ({
         path: '/api/ListUserDevices',
         params: { userId, tenantFilter: tenantDomain },
       }),
     }),
+    listDevicePolicies: builder.query({
+      query: ({ PolicyID, tenantDomain }) => ({
+        path: '/api/ListIntunePolicy',
+        params: { ID: PolicyID, tenantFilter: tenantDomain },
+      }),
+    }),
   }),
 })
 
-export const { useListDevicesQuery, useListUserDevicesQuery } = devicesApi
+export const { useListDevicesQuery, useListUserDevicesQuery, useListDevicePoliciesQuery } =
+  devicesApi
--- src/views/endpoint/MEM/MEMEditPolicy.js
+++ src/views/endpoint/MEM/MEMEditPolicy.js
@@ -1,10 +1,171 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {
+  CAlert,
+  CButton,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCardTitle,
+  CCol,
+  CForm,
+  CRow,
+  CSpinner,
+} from '@coreui/react'
+import useQuery from '../../../hooks/useQuery'
+import { setModalContent } from '../../../store/features/modal'
+import { useDispatch } from 'react-redux'
+import { Form } from 'react-final-form'
+import { RFFCFormInput, RFFCFormRadio } from '../../../components/RFFComponents'
+import { useLazyGenericPostRequestQuery } from 'src/store/api/app'
+import { useListDevicePoliciesQuery } from 'src/store/api/devices'
 
-const MEMEditPolicy = (props) => {
+const MEMEditPolicy = () => {
+  const dispatch = useDispatch()
+  let query = useQuery()
+  const policyID = query.get('ID')
+  const tenantDomain = query.get('tenantDomain')
+
+  const [queryError, setQueryError] = useState(false)
+
+  const {
+    data: group = {},
+    isFetching,
+    error,
+    isSuccess,
+  } = useListDevicePoliciesQuery({ tenantDomain, PolicyID: policyID })
+
+  useEffect(() => {
+    if (!policyID || !tenantDomain) {
+      dispatch(
+        setModalContent({
+          body: 'Error: Invalid request. Could not load requested policy.',
+          title: 'Invalid Request',
+          visible: true,
+        }),
+      )
+      setQueryError(true)
+    }
+  }, [policyID, tenantDomain, dispatch])
+  const [genericPostRequest, postResults] = useLazyGenericPostRequestQuery()
+  const onSubmit = (values) => {
+    const shippedValues = {
+      tenantID: tenantDomain,
+      GroupID: policyID,
+      displayName: values.displayName,
+      Description: values.description,
+      AssignTo: values.AssignTo,
+    }
+    //window.alert(JSON.stringify(shippedValues))
+    genericPostRequest({ path: '/api/EditPolicy', values: shippedValues })
+  }
+
+  const initialState = {
+    AssignTo: 'on',
+    ...group[0],
+  }
   return (
-    <div>
-      <h3>Endpoint Manager - Edit Policy</h3>
-    </div>
+    <>
+      {!queryError && (
+        <>
+          <CRow>
+            <CCol md={6}>
+              <CCard>
+                <CCardHeader>
+                  <CCardTitle>Group Details</CCardTitle>
+                </CCardHeader>
+                <CCardBody>
+                  {isFetching && <CSpinner />}
+                  {error && <span>Error loading Group</span>}
+                  {isSuccess && (
+                    <Form
+                      initialValues={{ ...initialState }}
+                      onSubmit={onSubmit}
+                      render={({ handleSubmit, submitting, values }) => {
+                        return (
+                          <CForm onSubmit={handleSubmit}>
+                            <CRow>
+                              <CCol>
+                                <RFFCFormInput
+                                  type="text"
+                                  name="displayName"
+                                  label="Display Name"
+                                  placeholder="Enter the Display Name"
+                                />
+                              </CCol>
+                            </CRow>
+                            <CRow>
+                              <CCol>
+                                <RFFCFormInput
+                                  type="text"
+                                  name="description"
+                                  label="Description"
+                                  placeholder="Enter the description"
+                                />
+                              </CCol>
+                            </CRow>
+                            <CRow>
+                              <CCol>
+                                <RFFCFormRadio
+                                  value="on"
+                                  name="AssignTo"
+                                  label="Do not assign"
+                                ></RFFCFormRadio>
+                                <RFFCFormRadio
+                                  value="allLicensedUsers"
+                                  name="AssignTo"
+                                  label="Assign to all users"
+                                ></RFFCFormRadio>
+                                <RFFCFormRadio
+                                  value="AllDevices"
+                                  name="AssignTo"
+                                  label="Assign to all devices"
+                                ></RFFCFormRadio>
+                                <RFFCFormRadio
+                                  value="AllDevicesAndUsers"
+                                  name="AssignTo"
+                                  label="Assign to all users and devices"
+                                ></RFFCFormRadio>
+                              </CCol>
+                            </CRow>
+
+                            <CRow className="mb-3">
+                              <CCol md={6}>
+                                <CButton type="submit" disabled={submitting}>
+                                  Edit Policy
+                                </CButton>
+                              </CCol>
+                            </CRow>
+                            {postResults.isSuccess && (
+                              <CAlert color="success">{postResults.data.Results}</CAlert>
+                            )}
+                          </CForm>
+                        )
+                      }}
+                    />
+                  )}
+                </CCardBody>
+              </CCard>
+            </CCol>
+            <CCol md={6}>
+              <CCard>
+                <CCardHeader>
+                  <CCardTitle>Policy Information</CCardTitle>
+                </CCardHeader>
+                <CCardBody>
+                  {isFetching && <CSpinner />}
+                  {isSuccess && (
+                    <>
+                      This is the (raw) information for this policy
+                      <pre>{JSON.stringify(group, null, 2)}</pre>
+                    </>
+                  )}
+                </CCardBody>
+              </CCard>
+            </CCol>
+          </CRow>
+        </>
+      )}
+    </>
   )
 }
 
