f0ba9ebb536a99710b79f720d213102b32ba1606
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2022-01-07 15:44:49 +0100

    offcanvas test

--- src/components/cipp/CippOffcanvas.js
+++ src/components/cipp/CippOffcanvas.js
@@ -1,57 +1,61 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { 
+import {
   CListGroup,
   CListGroupItem,
   COffcanvas,
   COffcanvasHeader,
   COffcanvasBody,
   COffcanvasTitle,
-  CDivider,
 } from '@coreui/react'
 
-export default function CippOffcanvas({ extendedInfo, actions, position, title }) {
+export default function CippOffcanvas({ extendedInfo, actions, position, title, visible }) {
   const extendedInfoContent = extendedInfo.map((info, index) => (
     <CListGroup layout="horizontal-md" key={index}>
-      <CListGroupItem className='cipp-extendedinfo-label'>{info.label}</CListGroupItem>
-      <CListGroupItem className='cipp-extendedinfo-value'>{info.value}</CListGroupItem>
+      <CListGroupItem className="cipp-extendedinfo-label">{info.label}</CListGroupItem>
+      <CListGroupItem className="cipp-extendedinfo-value">{info.value}</CListGroupItem>
     </CListGroup>
   ))
   const actionsContent = actions.map((action, index) => (
     <CListGroup layout="horizontal-md" key={index}>
-      <CListGroupItem className='cipp-action' component="button" href={action.link}>{action.icon}{action.label}</CListGroupItem>
+      <CListGroupItem className="cipp-action" component="button" href={action.link}>
+        {action.icon}
+        {action.label}
+      </CListGroupItem>
     </CListGroup>
   ))
   return (
-    <COffcanvas position={position}>
-      <COffcanvasHeader>{title}</COffcanvasHeader>
-      <COffcanvasBody>
-        <COffcanvasTitle>Extended Information</COffcanvasTitle>
-        {extendedInfoContent}
-        <CDivider />
-        <COffcanvasTitle>Actions</COffcanvasTitle>
-        {actionsContent}
-      </COffcanvasBody>
-    </COffcanvas>
+    <>
+      <COffcanvas visible={visible} placement={position}>
+        <COffcanvasHeader>{title}</COffcanvasHeader>
+        <COffcanvasBody>
+          <COffcanvasTitle>Extended Information</COffcanvasTitle>
+          {extendedInfoContent}
+          <COffcanvasTitle>Actions</COffcanvasTitle>
+          {actionsContent}
+        </COffcanvasBody>
+      </COffcanvas>
+    </>
   )
 }
 
 export const CippOffcanvasPropTypes = {
   extendedInfo: PropTypes.arrayOf(
     PropTypes.shape({
-        label: PropTypes.string,
-        value: PropTypes.any,
+      label: PropTypes.string,
+      value: PropTypes.any,
     }),
   ),
   actions: PropTypes.arrayOf(
     PropTypes.shape({
-        label: PropTypes.string,
-        link: PropTypes.string,
-        icon: PropTypes.component,
+      label: PropTypes.string,
+      link: PropTypes.string,
+      icon: PropTypes.component,
     }),
   ),
   position: PropTypes.string,
   title: PropTypes.string,
+  visible: PropTypes.bool,
 }
 
 CippOffcanvas.propTypes = CippOffcanvasPropTypes
