40e647abe50a7e226e6a77e97b72b7b8915f562f
k-grube <grube.kevin@gmail.com>
2022-01-08 20:13:37 -0800

    Fix AddUser, set form validation fields

--- src/views/identity/administration/AddUser.js
+++ src/views/identity/administration/AddUser.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react'
+import React from 'react'
 import {
   CButton,
-  CCallout,
   CCard,
   CCardBody,
   CCardHeader,
@@ -13,9 +12,9 @@ import {
   CSpinner,
   CAlert,
 } from '@coreui/react'
-import useQuery from '../../../hooks/useQuery'
 import { Form } from 'react-final-form'
 import {
+  CippPage,
   Condition,
   RFFCFormCheck,
   RFFCFormInput,
@@ -31,14 +30,19 @@ import { useListLicensesQuery } from '../../../store/api/licenses'
 import { useLazyGenericPostRequestQuery } from 'src/store/api/app'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
-import { TenantSelector } from 'src/components/cipp'
 import { useSelector } from 'react-redux'
 
-const AddUser = () => {
-  let query = useQuery()
-  const [queryError, setQueryError] = useState(false)
+const required = (value) => (value ? undefined : 'Required')
+const passwordRequired = (value, values) => {
+  if (!values.Autopassword && !values.password) {
+    return 'Password or automatically set password required'
+  }
+  return undefined
+}
 
-  const tenantDomain = useSelector((state) => state.app.currentTenant.defaultDomainName)
+const AddUser = () => {
+  const tenant = useSelector((state) => state.app.currentTenant)
+  const { defaultDomainName: tenantDomain } = tenant
 
   const {
     data: users = [],
@@ -102,131 +106,138 @@ const AddUser = () => {
   }
 
   return (
-    <>
-      <TenantSelector />
-      <hr />
-      <CCard className="page-card">
-        {!queryError && (
-          <>
-            {postResults.isSuccess && <CAlert color="success">{postResults.data?.Results}</CAlert>}
-            <CRow>
-              <CCol md={6}>
-                <CCard>
-                  <CCardHeader>
-                    <CCardTitle>Account Details</CCardTitle>
-                  </CCardHeader>
-                  <CCardBody>
-                    <Form
-                      initialValues={{ ...initialState }}
-                      onSubmit={onSubmit}
-                      render={({ handleSubmit, submitting, values }) => {
-                        return (
-                          <CForm onSubmit={handleSubmit}>
-                            <CRow>
-                              <CCol md={6}>
-                                <RFFCFormInput type="text" name="givenName" label="First Name" />
-                              </CCol>
-                              <CCol md={6}>
-                                <RFFCFormInput type="text" name="surname" label="Last Name" />
-                              </CCol>
-                            </CRow>
-                            <CRow>
-                              <CCol md={12}>
-                                <RFFCFormInput
-                                  type="text"
-                                  name="displayName"
-                                  label="Display Name"
-                                />
-                              </CCol>
-                            </CRow>
-                            <CRow>
-                              <CCol md={6}>
-                                <RFFCFormInput type="text" name="mailNickname" label="Username" />
-                              </CCol>
-                              <CCol md={6}>
-                                {domainsIsFetching && <CSpinner />}
-                                {!domainsIsFetching && (
-                                  <RFFCFormSelect
-                                    // label="Domain"
-                                    name="primDomain"
-                                    label="Primary Domain name"
-                                    placeholder={
-                                      !domainsIsFetching ? 'Select domain' : 'Loading...'
-                                    }
-                                    values={domains?.map((domain) => ({
-                                      value: domain.id,
-                                      label: domain.id,
-                                    }))}
-                                  />
-                                )}
-                                {domainsError && <span>Failed to load list of domains</span>}
-                              </CCol>
-                            </CRow>
-                            <CRow>
-                              <CCol md={12}>
-                                <RFFCFormTextarea
-                                  type="text"
-                                  name="addedAliases"
-                                  label="Add Aliases"
-                                  placeholder="Enter one alias per line"
-                                />
-                              </CCol>
-                            </CRow>
-                            <CRow>
-                              <CCol md={12}>
-                                <CFormLabel>Settings</CFormLabel>
+    <CippPage tenantSelector={true} title="Add User">
+      {postResults.isSuccess && (
+        <CAlert color="success" dismissible>
+          {postResults.data?.Results}
+        </CAlert>
+      )}
+      <CRow>
+        <CCol md={6}>
+          <CCard>
+            <CCardHeader>
+              <CCardTitle>Account Details</CCardTitle>
+            </CCardHeader>
+            <CCardBody>
+              <Form
+                initialValues={{ ...initialState }}
+                onSubmit={onSubmit}
+                render={({ handleSubmit, submitting, values }) => {
+                  return (
+                    <CForm onSubmit={handleSubmit}>
+                      <CRow>
+                        <CCol md={6}>
+                          <RFFCFormInput type="text" name="givenName" label="First Name" />
+                        </CCol>
+                        <CCol md={6}>
+                          <RFFCFormInput type="text" name="surname" label="Last Name" />
+                        </CCol>
+                      </CRow>
+                      <CRow>
+                        <CCol md={12}>
+                          <RFFCFormInput
+                            type="text"
+                            name="displayName"
+                            label="Display Name"
+                            validate={required}
+                          />
+                        </CCol>
+                      </CRow>
+                      <CRow>
+                        <CCol md={6}>
+                          <RFFCFormInput
+                            type="text"
+                            name="mailNickname"
+                            label="Username"
+                            validate={required}
+                          />
+                        </CCol>
+                        <CCol md={6}>
+                          {domainsIsFetching && <CSpinner />}
+                          {!domainsIsFetching && (
+                            <RFFCFormSelect
+                              // label="Domain"
+                              name="primDomain"
+                              label="Primary Domain name"
+                              placeholder={!domainsIsFetching ? 'Select domain' : 'Loading...'}
+                              values={domains?.map((domain) => ({
+                                value: domain.id,
+                                label: domain.id,
+                              }))}
+                              validate={required}
+                            />
+                          )}
+                          {domainsError && <span>Failed to load list of domains</span>}
+                        </CCol>
+                      </CRow>
+                      <CRow>
+                        <CCol md={12}>
+                          <RFFCFormTextarea
+                            type="text"
+                            name="addedAliases"
+                            label="Add Aliases"
+                            placeholder="Enter one alias per line"
+                          />
+                        </CCol>
+                      </CRow>
+                      <CRow>
+                        <CCol md={12}>
+                          <CFormLabel>Settings</CFormLabel>
+                          <RFFCFormCheck
+                            name="Autopassword"
+                            label="Automatically Set Password"
+                            validate={passwordRequired}
+                          />
+                          <Condition when="Autopassword" is={false}>
+                            <CCol md={12}>
+                              <RFFCFormInput
+                                type="password"
+                                name="password"
+                                label="Password"
+                                validate={passwordRequired}
+                              />
+                            </CCol>
+                          </Condition>
+                          <RFFCFormCheck
+                            name="MustChangePass"
+                            label="Require password change at next logon"
+                          />
+                        </CCol>
+                      </CRow>
+                      <CRow className="mb-3">
+                        <CCol md={12}>
+                          <RFFSelectSearch
+                            values={countryList.map(({ Code, Name }) => ({
+                              value: Code,
+                              name: Name,
+                            }))}
+                            name="usageLocation"
+                            placeholder="Type to search..."
+                            label="Usage Location"
+                            validate={required}
+                          />
+                        </CCol>
+                      </CRow>
+                      <CRow>
+                        <CCol md={12}>
+                          <RFFCFormSwitch name="licenses" label="Create user with license" />
+                          <Condition when="licenses" is={true}>
+                            <span>Licenses</span>
+                            <br />
+                            {licensesIsFetching && <CSpinner />}
+                            {licensesError && <span>Error loading licenses</span>}
+                            {!licensesIsFetching &&
+                              licenses?.map((license) => (
                                 <RFFCFormCheck
-                                  name="Autopassword"
-                                  label="Automatically Set Password"
+                                  key={license.id}
+                                  name={`license.License_${license.skuId}`}
+                                  label={`${license.skuPartNumber} (${license.availableUnits} available)`}
                                 />
-                                <Condition when="Autopassword" is={false}>
-                                  <CCol md={12}>
-                                    <RFFCFormInput
-                                      type="password"
-                                      name="password"
-                                      label="Password"
-                                    />
-                                  </CCol>
-                                </Condition>
-                                <RFFCFormCheck
-                                  name="MustChangePass"
-                                  label="Require password change at next logon"
-                                />
-                              </CCol>
-                            </CRow>
-                            <CRow className="mb-3">
-                              <CCol md={12}>
-                                <RFFSelectSearch
-                                  values={countryList.map(({ Code, Name }) => ({
-                                    value: Code,
-                                    name: Name,
-                                  }))}
-                                  name="usageLocation"
-                                  placeholder="Type to search..."
-                                  label="Usage Location"
-                                />
-                              </CCol>
-                            </CRow>
-                            <CRow>
-                              <CCol md={12}>
-                                <RFFCFormSwitch name="licenses" label="Create user with license" />
-                                <Condition when="licenses" is={true}>
-                                  <span>Licenses</span>
-                                  <br />
-                                  {licensesIsFetching && <CSpinner />}
-                                  {licensesError && <span>Error loading licenses</span>}
-                                  {!licensesIsFetching &&
-                                    licenses?.map((license) => (
-                                      <RFFCFormCheck
-                                        key={license.id}
-                                        name={`license.License_${license.skuId}`}
-                                        label={`${license.skuPartNumber} (${license.availableUnits} available)`}
-                                      />
-                                    ))}
-                                </Condition>
-                              </CCol>
-                            </CRow>
-                            {/* <CRow> Temporarily disabled, API does not support this yet.
+                              ))}
+                          </Condition>
+                        </CCol>
+                      </CRow>
+                      {/* <CRow> Temporarily disabled, API does not support this yet.
                               <CCol md={12}>
                                 <RFFCFormInput name="jobTitle" label="Job Title" type="text" />
                               </CCol>
@@ -271,50 +282,47 @@ const AddUser = () => {
                                 />
                               </CCol>
                             </CRow> */}
-                            <CRow className="mb-3">
-                              <CCol md={12}>
-                                <RFFSelectSearch
-                                  label="Copy group membership from other user"
-                                  values={users?.map((user) => ({
-                                    value: user.mail,
-                                    name: user.displayName,
-                                  }))}
-                                  placeholder={!usersIsFetching ? 'Select user' : 'Loading...'}
-                                  name="CopyFrom"
-                                />
-                                {usersError && <span>Failed to load list of users</span>}
-                              </CCol>
-                            </CRow>
-                            <CRow className="mb-3">
-                              <CCol md={6}>
-                                <CButton type="submit" disabled={submitting}>
-                                  Add User
-                                  {postResults.isFetching && (
-                                    <FontAwesomeIcon
-                                      icon={faCircleNotch}
-                                      spin
-                                      className="me-2"
-                                      size="1x"
-                                    />
-                                  )}
-                                </CButton>
-                              </CCol>
-                            </CRow>
-                            {postResults.isSuccess && (
-                              <CAlert color="success">{postResults.data?.Results}</CAlert>
+                      <CRow className="mb-3">
+                        <CCol md={12}>
+                          <RFFSelectSearch
+                            label="Copy group membership from other user"
+                            values={users?.map((user) => ({
+                              value: user.mail,
+                              name: user.displayName,
+                            }))}
+                            placeholder={!usersIsFetching ? 'Select user' : 'Loading...'}
+                            name="CopyFrom"
+                          />
+                          {usersError && <span>Failed to load list of users</span>}
+                        </CCol>
+                      </CRow>
+                      <CRow className="mb-3">
+                        <CCol md={6}>
+                          <CButton type="submit" disabled={submitting}>
+                            Add User
+                            {postResults.isFetching && (
+                              <FontAwesomeIcon
+                                icon={faCircleNotch}
+                                spin
+                                className="me-2"
+                                size="1x"
+                              />
                             )}
-                          </CForm>
-                        )
-                      }}
-                    />
-                  </CCardBody>
-                </CCard>
-              </CCol>
-            </CRow>
-          </>
-        )}
-      </CCard>
-    </>
+                          </CButton>
+                        </CCol>
+                      </CRow>
+                      {postResults.isSuccess && (
+                        <CAlert color="success">{postResults.data?.Results}</CAlert>
+                      )}
+                    </CForm>
+                  )
+                }}
+              />
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </CippPage>
   )
 }
 
