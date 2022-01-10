3651cf550900bca28001b4a38752c332964aa10d
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2022-01-10 15:31:01 +0100

    fixed remove standard

--- src/views/tenant/standards/ListAppliedStandards.js
+++ src/views/tenant/standards/ListAppliedStandards.js
@@ -2,94 +2,68 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { CippDatatable } from '../../../components/cipp'
 import {
-  CDropdown,
-  CDropdownItem,
-  CDropdownMenu,
-  CDropdownToggle,
   CCard,
   CCardBody,
   CCardTitle,
   CCardHeader,
   CSpinner,
+  CButton,
+  CCallout,
 } from '@coreui/react'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { ModalService } from '../../../components'
 import { useLazyGenericGetRequestQuery } from 'src/store/api/app'
 
-const Dropdown = (row, index, column) => {
-  const [ExecuteGetRequest, GetRequestResult] = useLazyGenericGetRequestQuery()
-  const handleDropdownConfirm = (apiurl) => {
-    ExecuteGetRequest({ url: apiurl })
-    //this isnt working all the way yet.
-    ModalService.confirm({
-      title: 'Results',
-      body: (
-        <div>
-          {GetRequestResult.isSuccess && (
-            <>
-              <CSpinner />
-            </>
-          )}
-          {GetRequestResult.isSuccess && GetRequestResult.data.Results}
-        </div>
-      ),
-      confirmLabel: 'Continue',
-    })
-  }
-  const handleDropdownEvent = (apiurl, message) => {
-    ModalService.confirm({
-      title: 'Confirm',
-      body: <div>{message}</div>,
-      onConfirm: () => handleDropdownConfirm(apiurl),
-      confirmLabel: 'Continue',
-      cancelLabel: 'Cancel',
-    })
-  }
-  return (
-    <CDropdown>
-      <CDropdownToggle size="sm" color="link">
-        <FontAwesomeIcon icon={faBars} />
-      </CDropdownToggle>
-      <CDropdownMenu>
-        <CDropdownItem
-          onClick={() =>
-            handleDropdownEvent(
-              `api/RemoveStandard?ID=${row.displayName}`,
-              `Are you sure you want to delete the standard for ${row.displayName}`,
-            )
-          }
-          href="#"
-        >
-          Delete Standard
-        </CDropdownItem>
-      </CDropdownMenu>
-    </CDropdown>
-  )
-}
-const columns = [
-  {
-    name: 'Tenant Name',
-    selector: (row) => row['displayName'],
-    sortable: true,
-  },
-  {
-    name: 'Standard',
-    selector: (row) => row['standardName'],
-    sortable: true,
-  },
-  {
-    name: 'Applied By',
-    selector: (row) => row['appliedBy'],
-    sortable: true,
-  },
-  {
-    name: 'Action',
-    cell: Dropdown,
-  },
-]
-
 const TenantsList = () => {
+  const [ExecuteGetRequest, getResults] = useLazyGenericGetRequestQuery()
+  const Dropdown = (row, index, column) => {
+    const handleDropdownEvent = (apiurl, message) => {
+      ModalService.confirm({
+        title: 'Confirm',
+        body: <div>{message}</div>,
+        onConfirm: () => ExecuteGetRequest({ path: apiurl }),
+        confirmLabel: 'Continue',
+        cancelLabel: 'Cancel',
+      })
+    }
+    return (
+      <CButton
+        size="sm"
+        variant="ghost"
+        color="danger"
+        onClick={() =>
+          handleDropdownEvent(
+            `api/RemoveStandard?ID=${row.displayName}`,
+            'Do you want to delete the standard?',
+          )
+        }
+      >
+        <FontAwesomeIcon icon={faTrash} href="" />
+      </CButton>
+    )
+  }
+  const columns = [
+    {
+      name: 'Tenant Name',
+      selector: (row) => row['displayName'],
+      sortable: true,
+    },
+    {
+      name: 'Standard',
+      selector: (row) => row['standardName'],
+      sortable: true,
+    },
+    {
+      name: 'Applied By',
+      selector: (row) => row['appliedBy'],
+      sortable: true,
+    },
+    {
+      name: 'Action',
+      cell: Dropdown,
+    },
+  ]
   const tenant = useSelector((state) => state.app.currentTenant)
 
   return (
@@ -99,6 +73,15 @@ const TenantsList = () => {
           <CCardTitle className="text-primary">Applied Standards</CCardTitle>
         </CCardHeader>
         <CCardBody>
+          {getResults.isFetching && (
+            <CCallout color="info">
+              <CSpinner>Loading</CSpinner>
+            </CCallout>
+          )}
+          {getResults.isSuccess && <CCallout color="info">{getResults.data?.Results}</CCallout>}
+          {getResults.isError && (
+            <CCallout color="danger">Could not connect to API: {getResults.error.message}</CCallout>
+          )}
           <CippDatatable
             keyField="id"
             reportName={`${tenant?.defaultDomainName}-AppliedStandards-List`}
