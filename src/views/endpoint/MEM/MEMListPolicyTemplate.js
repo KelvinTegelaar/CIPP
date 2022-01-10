8ea8b551d0f84ba6973040b32ecc154158f4c3a8
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2022-01-10 15:56:55 +0100

    dropdowns and actions

--- src/views/endpoint/MEM/MEMListPolicyTemplate.js
+++ src/views/endpoint/MEM/MEMListPolicyTemplate.js
@@ -1,46 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import CippDatatable from '../../../components/cipp/CippDatatable'
-import { CCard, CCardHeader, CCardTitle, CCardBody, CButton } from '@coreui/react'
+import {
+  CCard,
+  CCardHeader,
+  CCardTitle,
+  CCardBody,
+  CButton,
+  CCallout,
+  CSpinner,
+} from '@coreui/react'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-const Dropdown = (row, index, column) => {
-  return (
-    <>
-      <CButton size="sm" variant="ghost" color="danger">
-        <FontAwesomeIcon icon={faTrash} href="" />
-      </CButton>
-    </>
-  )
-}
-
-const columns = [
-  {
-    name: 'Display Name',
-    selector: (row) => row['Displayname'],
-    sortable: true,
-  },
-  {
-    name: 'Description',
-    selector: (row) => row['Description'],
-    sortable: true,
-  },
-  {
-    name: 'Type',
-    selector: (row) => row['Type'],
-    sortable: true,
-  },
-  {
-    name: 'GUID',
-    selector: (row) => row['GUID'],
-    omit: true,
-  },
-  {
-    name: 'Action',
-    cell: Dropdown,
-  },
-]
+import { useLazyGenericGetRequestQuery } from 'src/store/api/app'
+import { ModalService } from 'src/components'
 
 //todo: expandable with RAWJson property.
 /* eslint-disable-next-line react/prop-types */
@@ -48,6 +21,61 @@ const ExpandedComponent = ({ data }) => <pre>{data.RAWJson}</pre>
 
 const AutopilotListTemplates = () => {
   const tenant = useSelector((state) => state.app.currentTenant)
+
+  const [ExecuteGetRequest, getResults] = useLazyGenericGetRequestQuery()
+  const dropdown = (row, index, column) => {
+    const handleDropdownEvent = (apiurl, message) => {
+      ModalService.confirm({
+        title: 'Confirm',
+        body: <div>{message}</div>,
+        onConfirm: () => ExecuteGetRequest({ path: apiurl }),
+        confirmLabel: 'Continue',
+        cancelLabel: 'Cancel',
+      })
+    }
+    return (
+      <CButton
+        size="sm"
+        variant="ghost"
+        color="danger"
+        onClick={() =>
+          handleDropdownEvent(
+            `api/RemoveIntuneTemplate?ID=${row.GUID}`,
+            'Do you want to delete the template?',
+          )
+        }
+      >
+        <FontAwesomeIcon icon={faTrash} href="" />
+      </CButton>
+    )
+  }
+
+  const columns = [
+    {
+      name: 'Display Name',
+      selector: (row) => row['Displayname'],
+      sortable: true,
+    },
+    {
+      name: 'Description',
+      selector: (row) => row['Description'],
+      sortable: true,
+    },
+    {
+      name: 'Type',
+      selector: (row) => row['Type'],
+      sortable: true,
+    },
+    {
+      name: 'GUID',
+      selector: (row) => row['GUID'],
+      omit: true,
+    },
+    {
+      name: 'Action',
+      cell: dropdown,
+    },
+  ]
 
   return (
     <div>
@@ -57,6 +85,15 @@ const AutopilotListTemplates = () => {
           <CCardTitle>Available Endpoint Manager Templates</CCardTitle>
         </CCardHeader>
         <CCardBody>
+          {getResults.isFetching && (
+            <CCallout color="info">
+              <CSpinner>Loading</CSpinner>
+            </CCallout>
+          )}
+          {getResults.isSuccess && <CCallout color="info">{getResults.data?.Results}</CCallout>}
+          {getResults.isError && (
+            <CCallout color="danger">Could not connect to API: {getResults.error.message}</CCallout>
+          )}
           <CippDatatable
             tableProps={{
               expandableRows: true,
--- src/views/endpoint/autopilot/AutopilotListDevices.js
+++ src/views/endpoint/autopilot/AutopilotListDevices.js
@@ -1,71 +1,100 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { CButton } from '@coreui/react'
+import { CButton, CCallout, CSpinner } from '@coreui/react'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { CippPageList } from 'src/components'
-
-const dropdown = (row, rowIndex, formatExtraData) => {
-  return (
-    <>
-      <CButton size="sm" variant="ghost" color="danger">
-        <FontAwesomeIcon icon={faTrash} href="" />
-      </CButton>
-    </>
-  )
-}
-
-const columns = [
-  {
-    selector: (row) => row['displayName'],
-    name: 'Display Name',
-    sortable: true,
-  },
-  {
-    selector: (row) => row['serialNumber'],
-    name: 'Serial',
-    sortable: true,
-  },
-  {
-    selector: (row) => row['model'],
-    name: 'Model',
-    sortable: true,
-  },
-  {
-    selector: (row) => row['manufacturer'],
-    name: 'Manufacturer',
-    sortable: true,
-  },
-  {
-    selector: (row) => row['groupTag'],
-    name: 'Group Tag',
-    sortable: true,
-  },
-  {
-    selector: (row) => row['enrollmentState'],
-    name: 'Enrollment',
-    sortable: true,
-  },
-  {
-    name: (row) => row['Actions'],
-    cell: dropdown,
-  },
-]
+import { CippPageList, ModalService } from 'src/components'
+import { useLazyGenericGetRequestQuery } from 'src/store/api/app'
 
 const AutopilotListDevices = () => {
   const tenant = useSelector((state) => state.app.currentTenant)
+  const [ExecuteGetRequest, getResults] = useLazyGenericGetRequestQuery()
+  const dropdown = (row, index, column) => {
+    const handleDropdownEvent = (apiurl, message) => {
+      ModalService.confirm({
+        title: 'Confirm',
+        body: <div>{message}</div>,
+        onConfirm: () => ExecuteGetRequest({ path: apiurl }),
+        confirmLabel: 'Continue',
+        cancelLabel: 'Cancel',
+      })
+    }
+    return (
+      <CButton
+        size="sm"
+        variant="ghost"
+        color="danger"
+        onClick={() =>
+          handleDropdownEvent(
+            `api/RemoveAPDevice?ID=${row.id}&tenantFilter=${tenant.defaultDomainName}`,
+            'Do you want to delete the Autopilot Device?',
+          )
+        }
+      >
+        <FontAwesomeIcon icon={faTrash} href="" />
+      </CButton>
+    )
+  }
+
+  const columns = [
+    {
+      selector: (row) => row['displayName'],
+      name: 'Display Name',
+      sortable: true,
+    },
+    {
+      selector: (row) => row['serialNumber'],
+      name: 'Serial',
+      sortable: true,
+    },
+    {
+      selector: (row) => row['model'],
+      name: 'Model',
+      sortable: true,
+    },
+    {
+      selector: (row) => row['manufacturer'],
+      name: 'Manufacturer',
+      sortable: true,
+    },
+    {
+      selector: (row) => row['groupTag'],
+      name: 'Group Tag',
+      sortable: true,
+    },
+    {
+      selector: (row) => row['enrollmentState'],
+      name: 'Enrollment',
+      sortable: true,
+    },
+    {
+      name: (row) => row['Actions'],
+      cell: dropdown,
+    },
+  ]
 
   return (
-    <CippPageList
-      title="Autopilot Devices"
-      datatable={{
-        keyField: 'id',
-        reportName: `${tenant?.defaultDomainName}-AutopilotDevices-List`,
-        path: `/api/ListAPDevices`,
-        columns,
-        params: { TenantFilter: tenant?.defaultDomainName },
-      }}
-    />
+    <>
+      {getResults.isFetching && (
+        <CCallout color="info">
+          <CSpinner>Loading</CSpinner>
+        </CCallout>
+      )}
+      {getResults.isSuccess && <CCallout color="info">{getResults.data?.Results}</CCallout>}
+      {getResults.isError && (
+        <CCallout color="danger">Could not connect to API: {getResults.error.message}</CCallout>
+      )}
+      <CippPageList
+        title="Autopilot Devices"
+        datatable={{
+          keyField: 'id',
+          reportName: `${tenant?.defaultDomainName}-AutopilotDevices-List`,
+          path: `/api/ListAPDevices`,
+          columns,
+          params: { TenantFilter: tenant?.defaultDomainName },
+        }}
+      />
+    </>
   )
 }
 
