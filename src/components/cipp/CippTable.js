82a221a946158667d7529b5bcd4a102d2f575b12
Michael O'Toole <mikey@mjco.uk>
2022-01-13 17:41:30 +0000

    Fix build errors.

--- src/components/cipp/CippTable.js
+++ src/components/cipp/CippTable.js
@@ -142,7 +142,16 @@ export default function CippTable({
         <div className="w-50 d-flex justify-content-end">{defaultActions}</div>
       </>
     )
-  }, [filterText, resetPaginationToggle, columns, data, reportName, disablePDFExport, disableCSVExport, actions])
+  }, [
+    filterText,
+    resetPaginationToggle,
+    columns,
+    data,
+    reportName,
+    disablePDFExport,
+    disableCSVExport,
+    actions
+  ])
 
   return (
     <div className="ms-n3 me-n3 cipp-tablewrapper">
