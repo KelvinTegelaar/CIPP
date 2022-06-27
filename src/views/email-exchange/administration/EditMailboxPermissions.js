e16f8ea6a762f2a18fd64ac7aa6fd97b8748be57
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2022-06-27 12:12:56 +0200

    added refetch

--- src/views/email-exchange/administration/EditMailboxPermissions.js
+++ src/views/email-exchange/administration/EditMailboxPermissions.js
@@ -31,11 +31,13 @@ const EditMailboxPermission = () => {
   const [queryError, setQueryError] = useState(false)
 
   //const [EditMailboxPermission, { error: EditMailboxPermissionError, isFetching: EditMailboxPermissionIsFetching }] = useEditMailboxPermissionMutation()
+  const [genericPostRequest, postResults] = useLazyGenericPostRequestQuery()
 
   const {
     data: user = {},
     isFetching: userIsFetching,
     error: userError,
+    refetch: refetchPermissions,
   } = useListMailboxPermissionsQuery({ tenantDomain, userId })
 
   const {
@@ -45,6 +47,9 @@ const EditMailboxPermission = () => {
   } = useListUsersQuery({ tenantDomain })
 
   useEffect(() => {
+    if (postResults.isSuccess) {
+      refetchPermissions()
+    }
     if (!userId || !tenantDomain) {
       ModalService.open({
         body: 'Error invalid request, could not load requested user.',
@@ -54,8 +59,7 @@ const EditMailboxPermission = () => {
     } else {
       setQueryError(false)
     }
-  }, [userId, tenantDomain, dispatch])
-  const [genericPostRequest, postResults] = useLazyGenericPostRequestQuery()
+  }, [userId, tenantDomain, dispatch, postResults, refetchPermissions])
   const onSubmit = (values) => {
     const shippedValues = {
       userid: userId,
@@ -75,7 +79,6 @@ const EditMailboxPermission = () => {
     ...user,
   }
 
-  // this is dumb
   const formDisabled = queryError === true
 
   return (
@@ -102,9 +105,9 @@ const EditMailboxPermission = () => {
                   <CCardTitle>Account Details - {userId}</CCardTitle>
                 </CCardHeader>
                 <CCardBody>
-                  {userIsFetching && <CSpinner />}
+                  {usersIsFetching && <CSpinner />}
                   {userError && <span>Error loading user</span>}
-                  {!userIsFetching && (
+                  {!usersIsFetching && (
                     <Form
                       initialValues={{ ...initialState }}
                       onSubmit={onSubmit}
