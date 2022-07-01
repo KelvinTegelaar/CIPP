1b5e59f88350c7a977cc1ad805796a9d6b02defa
Brian <brian@thinkrelion.com>
2022-07-01 16:32:10 -0700

    fixed build error

--- src/Relion/components/TicketActivities.js
+++ src/Relion/components/TicketActivities.js
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from 'react'
+//import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import getTicketActivities from '../functions/getTicketActivities'
 
 export default function TicketActivities() {
-  const [ticketActivities, setTicketActivities] = useState()
+  //const [ticketActivities, setTicketActivities] = useState()
   const techId = useSelector((state) => state.ticket.techId)
   useEffect(() => {
     const fetchData = async () => {
       const response = await getTicketActivities(techId)
       console.log('Ticket Activities:')
       console.log(response)
-      setTicketActivities(response)
+      //setTicketActivities(response)
     }
     fetchData()
   }, [techId])
