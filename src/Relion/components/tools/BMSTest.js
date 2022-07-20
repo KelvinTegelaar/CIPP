b9bca55e31fccf54616d1dc63bf032405fb564af
Brian <brian@thinkrelion.com>
2022-07-20 07:20:03 -0700

    BMSTest TicketList

--- src/Relion/components/tools/BMSTest.js
+++ src/Relion/components/tools/BMSTest.js
@@ -3,19 +3,19 @@ import axios from 'axios'
 
 export default function Test() {
   useEffect(() => {
-    // const filter = {
-    //   filter: {
-    //     queueNames: 'Help Desk',
-    //     excludeCompleted: 1,
-    //   },
-    // }
+    const filter = {
+      filter: {
+        queueNames: 'Help Desk',
+        excludeCompleted: 1,
+      },
+    }
     const axiosParam = {
       method: 'get',
-      url: '/api/GetBMSToken',
+      url: '/api/GetBMSTicketlist',
       headers: {
         'Content-Type': 'application/json',
       },
-      //body: filter,
+      body: filter,
     }
     const fetch = async () => {
       const response = await axios(axiosParam)
