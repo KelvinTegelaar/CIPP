c464296d3b9daa69ea9ddc4e867dfecfac812257
Kelvin Tegelaar <you@example.com>
2022-08-16 13:15:53 +0200

    add deployment of named locations

--- src/routes.js
+++ src/routes.js
@@ -30,6 +30,9 @@ const ListConditionalTemplates = React.lazy(() =>
   import('src/views/tenant/conditional/ListCATemplates'),
 )
 
+const DeployNamedLocation = React.lazy(() =>
+  import('src/views/tenant/conditional/DeployNamedLocation'),
+)
 const AddConditionalTemplate = React.lazy(() =>
   import('src/views/tenant/conditional/AddCATemplate'),
 )
@@ -106,6 +109,8 @@ const IntuneListPolicyTemplate = React.lazy(() =>
   import('src/views/endpoint/MEM/MEMListPolicyTemplates'),
 )
 const ListDefender = React.lazy(() => import('src/views/security/defender/ListDefender'))
+const DeployDefender = React.lazy(() => import('src/views/security/defender/DeployDefender'))
+
 const OneDriveList = React.lazy(() => import('src/views/teams-share/onedrive/OneDriveList'))
 const SharepointList = React.lazy(() => import('src/views/teams-share/sharepoint/SharepointList'))
 const BusinessVoice = React.lazy(() => import('src/views/teams-share/teams/BusinessVoice'))
@@ -235,6 +240,11 @@ const routes = [
     component: DeployConditional,
   },
   {
+    path: '/tenant/conditional/deploy-named-location',
+    name: 'Deploy Named Locations',
+    component: DeployNamedLocation,
+  },
+  {
     path: '/tenant/conditional/list-template',
     name: 'Conditional Access Templates',
     component: ListConditionalTemplates,
@@ -345,6 +355,7 @@ const routes = [
     component: IntuneListPolicyTemplate,
   },
   { path: '/security/defender', name: 'Defender' },
+  { path: '/security/defender/deployment', name: 'Deploy Defender', component: DeployDefender },
   { path: '/security/defender/list-defender', name: 'List Defender', component: ListDefender },
   { path: '/teams-share', name: 'Teams & Sharepoint' },
   { path: '/teams-share/onedrive', name: 'OneDrive' },
