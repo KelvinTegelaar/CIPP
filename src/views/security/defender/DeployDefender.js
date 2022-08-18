ce9be897e940fac2c1b250ae515162a0dc71a11e
Kelvin Tegelaar <you@example.com>
2022-08-18 12:57:26 +0200

    finished defender deployment and dashboard

--- src/views/security/defender/DeployDefender.js
+++ src/views/security/defender/DeployDefender.js
@@ -6,7 +6,7 @@ import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 import { CippWizard } from 'src/components/layout'
 import { WizardTableField } from 'src/components/tables'
 import PropTypes from 'prop-types'
-import { RFFCFormSwitch } from 'src/components/forms'
+import { RFFCFormRadio, RFFCFormSwitch } from 'src/components/forms'
 import { useLazyGenericPostRequestQuery } from 'src/store/api/app'
 
 const Error = ({ name }) => (
@@ -32,25 +32,59 @@ const DeployDefender = () => {
   const [genericPostRequest, postResults] = useLazyGenericPostRequestQuery()
 
   const handleSubmit = async (values) => {
-    // @todo: clean this up api sided so we don't need to perform weird tricks.
-    Object.keys(values.standards).filter(function (x) {
-      if (values.standards[x] === false) {
-        delete values.standards[x]
-      }
-      return null
-    })
-
-    values.selectedTenants.map(
-      (tenant) =>
-        (values.standards[`Select_${tenant.defaultDomainName}`] = tenant.defaultDomainName),
-    )
-    //filter on only objects that are 'true'
-    genericPostRequest({ path: '/api/AddStandardsDeploy', values: values.standards })
+    genericPostRequest({ path: '/api/AddDefenderDeployment', values: values })
   }
 
   const formValues = {
     selectedTenants: [],
-    standards: {},
+    Compliance: {
+      AllowMEMEnforceCompliance: true,
+      AppSync: true,
+      BlockunsupportedOS: false,
+      ConnectAndroid: true,
+      ConnectIos: true,
+      ConnectIosCompliance: true,
+      ConnectWindows: true,
+    },
+    ASR: {
+      AssignTo: 'none',
+      BlockAdobeChild: true,
+      BlockCredentialStealing: true,
+      BlockExesMail: true,
+      BlockOfficeApps: true,
+      BlockOfficeExes: true,
+      BlockPSExec: true,
+      BlockUnsignedDrivers: true,
+      BlockUntrustedUSB: true,
+      BlockWin32Macro: true,
+      BlockYoungExe: true,
+      EnableRansomwareVac: true,
+      WMIPersistence: true,
+      blockJSVB: true,
+      blockOfficeChild: true,
+      blockOfficeComChild: true,
+    },
+    Policy: {
+      AssignTo: 'none',
+      AllowBehavior: true,
+      AllowCloudProtection: true,
+      AllowDownloadable: true,
+      AllowEmailScanning: true,
+      AllowFullScanNetwork: false,
+      AllowFullScanRemovable: true,
+      AllowIPS: true,
+      AllowNetwork: false,
+      AllowRealTime: true,
+      AllowUI: true,
+      CPULoad50: true,
+      CheckSigs: true,
+      DisableCatchupFullScan: false,
+      DisableCatchupQuickScan: false,
+      LowCPU: true,
+      NetworkProtectionAudit: false,
+      NetworkProtectionBlock: true,
+      ScanArchives: true,
+    },
   }
 
   return (
@@ -105,35 +139,38 @@ const DeployDefender = () => {
           <CRow className="mb-3">
             <CCol md={6}>
               <RFFCFormSwitch
-                name="AllowMEMEnforceCompliance"
+                name="Compliance.AllowMEMEnforceCompliance"
                 label="Allow Microsoft Defender for Endpoint to enforce Endpoint Security Configurations (Compliance)"
               />
               <RFFCFormSwitch
-                name="ConnectIosCompliance"
+                name="Compliance.ConnectIosCompliance"
                 label="Connect iOS/iPadOS devices version 13.0 and above to Microsoft Defender for Endpoint (Compliance)"
               />
               <RFFCFormSwitch
-                name="ConnectAndroid"
+                name="Compliance.ConnectAndroidCompliance"
                 label="Connect Android devices version 6.0.0 and above to Microsoft Defender for Endpoint (Compliance)"
               />
               <RFFCFormSwitch
-                name="ConnectWindows"
+                name="Compliance.ConnectWindows"
                 label="Connect Windows devices version 10.0.15063 and above to Microsoft Defender for Endpoint (Compliance)"
               />
             </CCol>
             <CCol md={6}>
               <RFFCFormSwitch
-                name="AppSync"
+                name="Compliance.AppSync"
                 label="Enable App Sync (sending application inventory) for iOS/iPadOS devices"
               />
-              <RFFCFormSwitch name="BlockunsupportedOS" label="Block unsupported OS versions" />
+              <RFFCFormSwitch
+                name="Compliance.BlockunsupportedOS"
+                label="Block unsupported OS versions"
+              />
 
               <RFFCFormSwitch
-                name="ConnectAndroid"
+                name="Compliance.ConnectAndroid"
                 label="Connect Android devices to Microsoft Defender for Endpoint"
               />
               <RFFCFormSwitch
-                name="ConnectIos"
+                name="Compliance.ConnectIos"
                 label="Connect iOS/iPadOS devices to Microsoft Defender for Endpoint"
               />
             </CCol>
@@ -153,41 +190,75 @@ const DeployDefender = () => {
         <div className="mb-2">
           <CRow className="mb-3">
             <CCol md={6}>
-              <RFFCFormSwitch name="ScanArchives" label="Allow Archive Scanning" />
-              <RFFCFormSwitch name="AllowBehavior" label="Allow behavior monitoring" />
-              <RFFCFormSwitch name="AllowCloudProtection" label="Allow Cloud Protection" />
-              <RFFCFormSwitch name="AllowEmailScanning" label="Allow e-mail scanning" />
-              <RFFCFormSwitch name="standards.SecurityDefaults" label="Enable Security Defaults" />
+              <RFFCFormSwitch name="Policy.ScanArchives" label="Allow Archive Scanning" />
+              <RFFCFormSwitch name="Policy.AllowBehavior" label="Allow behavior monitoring" />
+              <RFFCFormSwitch name="Policy.AllowCloudProtection" label="Allow Cloud Protection" />
+              <RFFCFormSwitch name="Policy.AllowEmailScanning" label="Allow e-mail scanning" />
               <RFFCFormSwitch
-                name="AllowFullScanNetwork"
+                name="Policy.AllowFullScanNetwork"
                 label="Allow Full Scan on Network Drives"
               />
               <RFFCFormSwitch
-                name="AllowFullScanRemovable"
+                name="Policy.AllowFullScanRemovable"
                 label="Allow Full Scan on Removable Drives"
               />
-              <RFFCFormSwitch name="AllowIPS" label="Allow Intrusion Prevention System" />
-              <RFFCFormSwitch name="LowCPU" label="Enable Low CPU priority" />
-              <RFFCFormSwitch name="CPULoad50" label="Set Maximum CPU load to 50%" />
+              <RFFCFormSwitch name="Policy.AllowScriptScan" label="Allow Script Scanning" />
+              <RFFCFormSwitch name="Policy.AllowIPS" label="Allow Intrusion Prevention System" />
+              <RFFCFormSwitch name="Policy.LowCPU" label="Enable Low CPU priority" />
             </CCol>
             <CCol md={6}>
-              <RFFCFormSwitch name="AllowDownloadable" label="Allow scanning of downloaded files" />
-              <RFFCFormSwitch name="AllowRealTime" label="Allow Realtime monitoring" />
-              <RFFCFormSwitch name="AllowNetwork" label="Allow scanning of mapped drives" />
-              <RFFCFormSwitch name="AllowUI" label="Allow users to access UI" />
               <RFFCFormSwitch
-                name="NetworkProtectionBlock"
+                name="Policy.AllowDownloadable"
+                label="Allow scanning of downloaded files"
+              />
+              <RFFCFormSwitch name="Policy.AllowRealTime" label="Allow Realtime monitoring" />
+              <RFFCFormSwitch name="Policy.AllowNetwork" label="Allow scanning of mapped drives" />
+              <RFFCFormSwitch name="Policy.AllowUI" label="Allow users to access UI" />
+              <RFFCFormSwitch
+                name="Policy.NetworkProtectionBlock"
                 label="Enable Network Protection in Block Mode"
               />
               <RFFCFormSwitch
-                name="NetworkProtectionAudit"
+                name="Policy.NetworkProtectionAudit"
                 label="Enable Network Protection in Audit Mode"
               />
 
-              <RFFCFormSwitch name="CheckSigs" label="Check Signatures before scan" />
-              <RFFCFormSwitch name="DisableCatchupFullScan" label="Disable Catchup Full Scan" />
-              <RFFCFormSwitch name="DisableCatchup Quick Scan" label="Disable Catchup Full Scan" />
+              <RFFCFormSwitch name="Policy.CheckSigs" label="Check Signatures before scan" />
+              <RFFCFormSwitch
+                name="Policy.DisableCatchupFullScan"
+                label="Disable Catchup Full Scan"
+              />
+              <RFFCFormSwitch
+                name="Policy.DisableCatchupQuickScan"
+                label="Disable Catchup Quick Scan"
+              />
             </CCol>
+            <center>
+              <h5 className="card-title mb-4">Assign to group</h5>
+            </center>
+            <RFFCFormRadio
+              value="none"
+              name="Policy.AssignTo"
+              label="Do not assign"
+              validate={false}
+            ></RFFCFormRadio>
+            <RFFCFormRadio
+              value="allLicensedUsers"
+              name="Policy.AssignTo"
+              label="Assign to all users"
+              validate={false}
+            ></RFFCFormRadio>
+            <RFFCFormRadio
+              value="AllDevices"
+              name="Policy.AssignTo"
+              label="Assign to all devices"
+              validate={false}
+            ></RFFCFormRadio>
+            <RFFCFormRadio
+              value="AllDevicesAndUsers"
+              name="Policy.AssignTo"
+              label="Assign to all users and devices"
+            ></RFFCFormRadio>
           </CRow>
         </div>
         <hr className="my-4" />
@@ -202,69 +273,95 @@ const DeployDefender = () => {
           <CRow className="mb-3">
             <CCol md={6}>
               <RFFCFormSwitch
-                name="BlockAdobeChild"
+                name="ASR.BlockAdobeChild"
                 label="Block Adobe Reader from creating child processes"
               />
               <RFFCFormSwitch
-                name="standards.DelegateSentItems"
+                name="ASR.BlockWin32Macro"
                 label="Block Win32 API calls from Office macros"
               />
               <RFFCFormSwitch
-                name="standards.SendFromAlias"
+                name="ASR.BlockCredentialStealing"
                 label="Block credential stealing from the Windows local security authority subsystem"
               />
               <RFFCFormSwitch
-                name="standards.DisableViva"
+                name="ASR.BlockPSExec"
                 label="Block process creations originating from PSExec and WMI commands"
               />
               <RFFCFormSwitch
-                name="standards.DisableViva"
+                name="ASR.WMIPersistence"
                 label="Block persistence through WMI event subscription"
               />
               <RFFCFormSwitch
-                name="standards.DisableViva"
+                name="ASR.BlockOfficeExes"
                 label="Block Office applications from creating executable content"
               />
               <RFFCFormSwitch
-                name="standards.DisableViva"
+                name="ASR.BlockOfficeApps"
                 label="Block Office applications from injecting code into other processes"
               />
             </CCol>
             <CCol md={6}>
               <RFFCFormSwitch
-                name="standards.AutoExpandArchive"
+                name="ASR.BlockYoungExe"
                 label="Block executable files from running unless they meet a prevalence, age, or trusted list criterion"
               />
               <RFFCFormSwitch
-                name="standards.SpoofWarn"
+                name="ASR.blockJSVB"
                 label="Block JavaScript or VBScript from launching downloaded executable content"
               />
 
               <RFFCFormSwitch
-                name="standards.DisableViva"
+                name="ASR.blockOfficeComChild"
                 label="Block Office communication application from creating child processes"
               />
               <RFFCFormSwitch
-                name="standards.DisableViva"
+                name="ASR.blockOfficeChild"
                 label="Block all Office applications from creating child processes"
               />
               <RFFCFormSwitch
-                name="standards.DisableViva"
+                name="ASR.BlockUntrustedUSB"
                 label="Block untrusted and unsigned processes that run from USB"
               />
               <RFFCFormSwitch
-                name="standards.DisableViva"
+                name="ASR.EnableRansomwareVac"
                 label="Use advanced protection against ransomware"
               />
               <RFFCFormSwitch
-                name="standards.DisableViva"
+                name="ASR.BlockExesMail"
                 label="Block executable content from email client and webmail"
               />
               <RFFCFormSwitch
-                name="standards.DisableViva"
+                name="ASR.BlockUnsignedDrivers"
                 label="Block abuse of exploited vulnerable signed drivers (Device)"
               />
             </CCol>
+            <center>
+              <h5 className="card-title mb-4">Assign to group</h5>
+            </center>
+            <RFFCFormRadio
+              value="none"
+              name="ASR.AssignTo"
+              label="Do not assign"
+              validate={false}
+            ></RFFCFormRadio>
+            <RFFCFormRadio
+              value="allLicensedUsers"
+              name="ASR.AssignTo"
+              label="Assign to all users"
+              validate={false}
+            ></RFFCFormRadio>
+            <RFFCFormRadio
+              value="AllDevices"
+              name="ASR.AssignTo"
+              label="Assign to all devices"
+              validate={false}
+            ></RFFCFormRadio>
+            <RFFCFormRadio
+              value="AllDevicesAndUsers"
+              name="ASR.AssignTo"
+              label="Assign to all users and devices"
+            ></RFFCFormRadio>
           </CRow>
         </div>
         <hr className="my-4" />
@@ -295,9 +392,25 @@ const DeployDefender = () => {
                         </li>
                       ))}
                     </CCallout>
-                    <h5 className="mb-0">Selected Standards</h5>
+                    <h5 className="mb-0">Selected MEM Compliance settings</h5>
                     <CCallout color="info">
-                      {Object.entries(props.values.standards).map(([key, value], idx) => (
+                      {Object.entries(props.values.Compliance).map(([key, value], idx) => (
+                        <li key={idx}>
+                          {key}: {value ? 'Enabled' : 'Disabled'}
+                        </li>
+                      ))}
+                    </CCallout>
+                    <h5 className="mb-0">Selected AV settings</h5>
+                    <CCallout color="info">
+                      {Object.entries(props.values.Policy).map(([key, value], idx) => (
+                        <li key={idx}>
+                          {key}: {value ? 'Enabled' : 'Disabled'}
+                        </li>
+                      ))}
+                    </CCallout>
+                    <h5 className="mb-0">Selected ASR Settings</h5>
+                    <CCallout color="info">
+                      {Object.entries(props.values.ASR).map(([key, value], idx) => (
                         <li key={idx}>
                           {key}: {value ? 'Enabled' : 'Disabled'}
                         </li>
@@ -310,7 +423,13 @@ const DeployDefender = () => {
             )}
           </FormSpy>
         )}
-        {postResults.isSuccess && <CCallout color="success">{postResults.data.Results}</CCallout>}
+        {postResults.isSuccess && (
+          <CCallout color="success">
+            {postResults.data.Results.map((result, idx) => {
+              return <li key={idx}>{result}</li>
+            })}
+          </CCallout>
+        )}
         <hr className="my-4" />
       </CippWizard.Page>
     </CippWizard>
