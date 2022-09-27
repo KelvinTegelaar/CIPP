ba92af19e4a016a0b697315fe92f8b1c3c23664f
Kelvin Tegelaar <you@example.com>
2022-09-27 09:39:56 +0200

    GDAP Wizard

--- src/views/tenant/administration/GDAPWizard.js
+++ src/views/tenant/administration/GDAPWizard.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { CCol, CRow, CForm, CListGroup, CListGroupItem, CCallout, CSpinner } from '@coreui/react'
+import { CCol, CRow, CForm, CCallout, CSpinner, CButton } from '@coreui/react'
 import { Field, FormSpy } from 'react-final-form'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck, faExclamationTriangle, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 import { CippWizard } from 'src/components/layout'
 import { WizardTableField } from 'src/components/tables'
 import PropTypes from 'prop-types'
-import { RFFCFormSwitch } from 'src/components/forms'
 import { useLazyGenericPostRequestQuery } from 'src/store/api/app'
 
 const Error = ({ name }) => (
@@ -45,12 +44,32 @@ const GDAPWizard = () => {
       onSubmit={handleSubmit}
       wizardTitle="GDAP Migration Wizard"
     >
+      <CippWizard.Page title="Tenant Choice" description="Setup">
+        <center>
+          <h3 className="text-primary">Step 1</h3>
+          <h5 className="card-title mb-4">Setup GDAP Migration tool</h5>
+        </center>
+        <hr className="my-4" />
+        <CCallout color="info">
+          The GDAP migration tool requires setup. Please check the documentation at{' '}
+          <a href="https://cipp.app" target="_blank">
+            cipp.app
+          </a>
+          <br /> <br />
+          This tool is offered as-is, without support, warranties, or guarantees. If you are having
+          GDAP migration problems, contact Microsoft via the Partner Support Center.
+          <br />
+          Please remember to add the CIPP-SAM service principal to the GDAP groups you would like
+          CIPP to manage.
+        </CCallout>
+        <hr className="my-4" />
+      </CippWizard.Page>
       <CippWizard.Page
         title="Tenant Choice"
         description="Choose the tenants you wish to create a GDAP relationship for"
       >
         <center>
-          <h3 className="text-primary">Step 1</h3>
+          <h3 className="text-primary">Step 2</h3>
           <h5 className="card-title mb-4">Choose a tenant</h5>
         </center>
         <hr className="my-4" />
@@ -86,7 +105,7 @@ const GDAPWizard = () => {
         description="Select which standards you want to apply."
       >
         <center>
-          <h3 className="text-primary">Step 2</h3>
+          <h3 className="text-primary">Step 3</h3>
           <h5 className="card-title mb-4">
             Select which roles you want to add to GDAP relationship.
           </h5>
@@ -132,7 +151,7 @@ const GDAPWizard = () => {
       </CippWizard.Page>
       <CippWizard.Page title="Review and Confirm" description="Confirm the settings to apply">
         <center>
-          <h3 className="text-primary">Step 3</h3>
+          <h3 className="text-primary">Step 4</h3>
           <h5 className="card-title mb-4">Confirm and apply</h5>
         </center>
         <hr className="my-4" />
