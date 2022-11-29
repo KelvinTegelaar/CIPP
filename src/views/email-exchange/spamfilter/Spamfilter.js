feb61b9e8e90bec9cdb038eb9ace9956c43d8815
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2022-11-29 12:56:51 +0100

    correct urls

--- src/views/email-exchange/spamfilter/Spamfilter.js
+++ src/views/email-exchange/spamfilter/Spamfilter.js
@@ -5,7 +5,7 @@ import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { CippPageList } from 'src/components/layout'
 import { CippActionsOffcanvas } from 'src/components/utilities'
-import { CellTip } from 'src/components/tables'
+import { cellBooleanFormatter, cellDateFormatter, CellTip } from 'src/components/tables'
 
 const Offcanvas = (row, rowIndex, formatExtraData) => {
   const tenant = useSelector((state) => state.app.currentTenant)
@@ -19,9 +19,40 @@ const Offcanvas = (row, rowIndex, formatExtraData) => {
       <CippActionsOffcanvas
         title="User Information"
         extendedInfo={[
-          { label: 'Created by', value: `${row.CreatedBy}` },
-          { label: 'Last edit by', value: `${row.LastModifiedBy}` },
-          { label: 'Description', value: `${row.Description}` },
+          {
+            label: 'Increase spam score with image links',
+            value: `${row.IncreaseScoreWithImageLinks}`,
+          },
+          {
+            label: 'Increase spam score with links that contain an ip',
+            value: `${row.IncreaseScoreWithNumericIps}`,
+          },
+          {
+            label: 'Increase Spam score with links that contain ports',
+            value: `${row.IncreaseScoreWithRedirectToOtherPort}`,
+          },
+          {
+            label: 'Increase Spam score for .Biz or .Info',
+            value: `${row.IncreaseScoreWithBizOrInfoUrls}`,
+          },
+          { label: 'Mark Empty Messages as spam', value: `${row.MarkAsSpamEmptyMessages}` },
+          {
+            label: 'Mark javascript in HTML as spam',
+            value: `${row.MarkAsSpamJavaScriptInHtml}`,
+          },
+          { label: 'Mark iFrames as spam', value: `${row.MarkAsSpamFramesInHtml}` },
+          { label: 'Mark Object tags as spam', value: `${row.MarkAsSpamObjectTagsInHtml}` },
+          { label: 'Mark Embedded HTML as spam', value: `${row.MarkAsSpamEmbedTagsInHtml}` },
+          { label: 'Mark Form tags as spam', value: `${row.MarkAsSpamFormTagsInHtml}` },
+          { label: 'Mark known html bugs as spam', value: `${row.MarkAsSpamWebBugsInHtml}` },
+          {
+            label: 'Mark Senstive wordlists as spam.',
+            value: `${row.MarkAsSpamSensitiveWordList}`,
+          },
+          { label: 'Mark SPF Hard Fail as spam', value: `${row.MarkAsSpamSpfRecordHardFail}` },
+          { label: 'Mark SenderID Fails as spam', value: `${row.MarkAsSpamFromAddressAuthFail}` },
+          { label: 'Mark known bulk mail as spam', value: `${row.MarkAsSpamBulkMail}` },
+          { label: 'Mark Backscatter as spam', value: `${row.MarkAsSpamNdrBackscatter}` },
         ]}
         actions={[
           {
@@ -39,7 +70,7 @@ const Offcanvas = (row, rowIndex, formatExtraData) => {
             color: 'info',
             icon: <FontAwesomeIcon icon={faCheck} className="me-2" />,
             modal: true,
-            modalUrl: `/api/EditTransportRule?State=Enable&TenantFilter=${tenant.defaultDomainName}&GUID=${row.Guid}`,
+            modalUrl: `/api/EditSpamfilter?State=enable&TenantFilter=${tenant.defaultDomainName}&name=${row.Name}`,
             modalMessage: 'Are you sure you want to enable this rule?',
           },
           {
@@ -47,7 +78,7 @@ const Offcanvas = (row, rowIndex, formatExtraData) => {
             color: 'info',
             icon: <FontAwesomeIcon icon={faBan} className="me-2" />,
             modal: true,
-            modalUrl: `/api/EditTransportRule?State=Disable&TenantFilter=${tenant.defaultDomainName}&GUID=${row.Guid}`,
+            modalUrl: `/api/EditSpamfilter?State=disable&TenantFilter=${tenant.defaultDomainName}&name=${row.Name}`,
             modalMessage: 'Are you sure you want to disable this rule?',
           },
           {
@@ -55,8 +86,8 @@ const Offcanvas = (row, rowIndex, formatExtraData) => {
             color: 'danger',
             modal: true,
             icon: <FontAwesomeIcon icon={faTrash} className="me-2" />,
-            modalUrl: `/api/RemoveTransportRule?TenantFilter=${tenant.defaultDomainName}&GUID=${row.Guid}`,
-            modalMessage: 'Are you sure you want to disable this rule?',
+            modalUrl: `/api/RemoveSpamFilter?TenantFilter=${tenant.defaultDomainName}&name=${row.Name}`,
+            modalMessage: 'Are you sure you want to delete this rule?',
           },
         ]}
         placement="end"
@@ -78,32 +109,55 @@ const columns = [
     exportSelector: 'Name',
   },
   {
-    name: 'State',
-    selector: (row) => row['State'],
+    name: 'Default Rule',
+    selector: (row) => row['IsDefault'],
     sortable: true,
-    exportSelector: 'State',
+    cell: cellBooleanFormatter(),
+    exportSelector: 'IsDefault',
   },
   {
-    name: 'Mode',
-    selector: (row) => row['Mode'],
+    name: 'Rule State',
+    selector: (row) => row['ruleState'],
     sortable: true,
-    exportSelector: 'Mode',
+    exportSelector: 'ruleState',
   },
   {
-    name: 'Error Action',
-    selector: (row) => row['RuleErrorAction'],
+    name: 'Priority',
+    selector: (row) => row['rulePrio'],
     sortable: true,
-    exportSelector: 'RuleErrorAction',
+    exportSelector: 'rulePrio',
   },
   {
-    name: 'description',
-    selector: (row) => row['Description'],
-    omit: true,
+    name: 'High Confidence Spam Action',
+    selector: (row) => row['HighConfidenceSpamAction'],
+    sortable: true,
+    exportSelector: 'HighConfidenceSpamAction',
   },
   {
-    name: 'GUID',
-    selector: (row) => row['Guid'],
-    omit: true,
+    name: 'Bulk Spam Action',
+    selector: (row) => row['BulkSpamAction'],
+    sortable: true,
+    exportSelector: 'BulkSpamAction',
+  },
+  {
+    name: 'Phish Spam Action',
+    selector: (row) => row['PhishSpamAction'],
+    sortable: true,
+    exportSelector: 'PhishSpamAction',
+  },
+  {
+    name: 'Creation Date',
+    selector: (row) => row['WhenCreated'],
+    sortable: true,
+    exportSelector: 'WhenChanged',
+    cell: cellDateFormatter(),
+  },
+  {
+    name: 'Last Edit Date',
+    selector: (row) => row['WhenChanged'],
+    sortable: true,
+    exportSelector: 'WhenChanged',
+    cell: cellDateFormatter(),
   },
   {
     name: 'Actions',
@@ -117,10 +171,10 @@ const SpamFilterList = () => {
 
   return (
     <CippPageList
-      title="Transport Rules"
+      title="Spam Filters"
       tenantSelector={true}
       datatable={{
-        reportName: `${tenant?.defaultDomainName}-transport-rules-list`,
+        reportName: `${tenant?.defaultDomainName}-Spamfilter-list`,
         path: '/api/Listspamfilter',
         params: { TenantFilter: tenant?.defaultDomainName },
         columns,
