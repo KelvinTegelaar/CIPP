e442cbdabd09542e7509ffbb79d08bb9e5aa98b0
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2023-04-24 17:04:56 +0200

    new alerts via header

--- src/components/layout/AppHeader.js
+++ src/components/layout/AppHeader.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import {
+  CAlert,
+  CAlertLink,
   CContainer,
   CHeader,
   CHeaderNav,
@@ -17,46 +19,67 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretSquareLeft, faCaretSquareRight } from '@fortawesome/free-solid-svg-icons'
 import { toggleSidebarShow } from 'src/store/features/app'
 import { useMediaPredicate } from 'react-media-hook'
+import { useGenericGetRequestQuery } from 'src/store/api/app'
 
 const AppHeader = () => {
   const dispatch = useDispatch()
   const sidebarShow = useSelector((state) => state.app.sidebarShow)
   const currentTheme = useSelector((state) => state.app.currentTheme)
   const preferredTheme = useMediaPredicate('(prefers-color-scheme: dark)') ? 'impact' : 'cyberdrain'
-  return (
-    <CHeader position="sticky">
-      <CContainer fluid>
-        <CSidebarBrand className="me-auto p-2" to="/">
-          <CImage
-            className="sidebar-brand-full me-2"
-            src={
-              currentTheme === 'cyberdrain' || preferredTheme === 'cyberdrain'
-                ? cyberdrainlogodark
-                : cyberdrainlogolight
-            }
-            height={80}
-          />
-          <CHeaderNav className="me-2 p-2">
-            <CHeaderToggler
-              className="me-2"
-              onClick={() => dispatch(toggleSidebarShow({ sidebarShow }))}
-            >
-              <FontAwesomeIcon
-                icon={sidebarShow ? faCaretSquareLeft : faCaretSquareRight}
-                size="lg"
-                className="me-2"
-              />
-            </CHeaderToggler>
-            <TenantSelector className="me-2" NavSelector={true} />
-          </CHeaderNav>
-        </CSidebarBrand>
+  const {
+    data: dashboard,
+    isLoading: isLoadingDash,
+    isSuccess: issuccessDash,
+  } = useGenericGetRequestQuery({ path: '/api/GetCippAlerts' })
 
-        <CHeaderNav className="ms-auto p-2">
-          <AppHeaderSearch />
-          <AppHeaderDropdown />
-        </CHeaderNav>
-      </CContainer>
-    </CHeader>
+  return (
+    <>
+      <CHeader position="sticky">
+        <CContainer fluid>
+          <CSidebarBrand className="me-auto p-2" to="/">
+            <CImage
+              className="sidebar-brand-full me-2"
+              src={
+                currentTheme === 'cyberdrain' || preferredTheme === 'cyberdrain'
+                  ? cyberdrainlogodark
+                  : cyberdrainlogolight
+              }
+              height={80}
+            />
+            <CHeaderNav className="me-2 p-2">
+              <CHeaderToggler
+                className="me-2"
+                onClick={() => dispatch(toggleSidebarShow({ sidebarShow }))}
+              >
+                <FontAwesomeIcon
+                  icon={sidebarShow ? faCaretSquareLeft : faCaretSquareRight}
+                  size="lg"
+                  className="me-2"
+                />
+              </CHeaderToggler>
+
+              <TenantSelector className="me-2" NavSelector={true} />
+            </CHeaderNav>
+          </CSidebarBrand>
+          <CHeaderNav className="ms-auto p-2">
+            <AppHeaderSearch />
+            <AppHeaderDropdown />
+          </CHeaderNav>
+        </CContainer>
+      </CHeader>
+
+      {issuccessDash &&
+        dashboard.map((item, index) => (
+          <div
+            className="mb-3"
+            style={{ zIndex: 10000, 'padding-left': '20rem', 'padding-right': '3rem' }}
+          >
+            <CAlert key={index} color={item.type} variant dismissible>
+              {item.Alert} <CAlertLink href={item.link}>Link</CAlertLink>
+            </CAlert>
+          </div>
+        ))}
+    </>
   )
 }
 
