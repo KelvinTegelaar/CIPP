21038ddeac3ae7e13cbcbccbdee9f72e5aa142e6
John Duprey <jwd@johnwduprey.com>
2023-05-16 17:35:37 -0400

    CippDatatable Queue Callout

--- src/components/tables/CippDatatable.js
+++ src/components/tables/CippDatatable.js
@@ -3,6 +3,7 @@ import { useListDatatableQuery } from 'src/store/api/datatable'
 import PropTypes from 'prop-types'
 import { CippTable } from 'src/components/tables'
 import { CippTablePropTypes } from 'src/components/tables/CippTable'
+import { CCallout } from '@coreui/react'
 
 export default function CippDatatable({ path, params, ...rest }) {
   const [refreshGuid, setRefreshGuid] = React.useState('')
@@ -13,14 +14,30 @@ export default function CippDatatable({ path, params, ...rest }) {
     error,
   } = useListDatatableQuery({ path, params: { refreshGuid, $filter: graphFilter, ...params } })
   return (
-    <CippTable
-      {...rest}
-      data={data}
-      isFetching={isFetching}
-      error={error}
-      refreshFunction={setRefreshGuid}
-      graphFilterFunction={setGraphFilter}
-    />
+    <>
+      {data[0]?.Queued ? (
+        <>
+          <CCallout color="info">{data[0]?.QueueMessage}</CCallout>
+          <CippTable
+            {...rest}
+            data={[]}
+            isFetching={isFetching}
+            error={error}
+            refreshFunction={setRefreshGuid}
+            graphFilterFunction={setGraphFilter}
+          />
+        </>
+      ) : (
+        <CippTable
+          {...rest}
+          data={data}
+          isFetching={isFetching}
+          error={error}
+          refreshFunction={setRefreshGuid}
+          graphFilterFunction={setGraphFilter}
+        />
+      )}
+    </>
   )
 }
 
