844f9abfb9ed4501378656bb57847df71598559d
Kristian Bentsen Kjærgård (KBK) <kbk@complea.dk>
2023-10-30 15:29:15 +0100

    One more time, but now with prettier

--- src/views/endpoint/intune/MEMAddPolicy.js
+++ src/views/endpoint/intune/MEMAddPolicy.js
@@ -130,8 +130,10 @@ const AddPolicy = () => {
           <h3 className="text-primary">Step 2</h3>
           <h5 className="card-title mb-4">
             Enter the raw JSON for this policy. See{' '}
-            <a href="https://docs.cipp.app/user-documentation/endpoint/mem/add-policy-template">this</a> for more
-            information.
+            <a href="https://docs.cipp.app/user-documentation/endpoint/mem/add-policy-template">
+              this
+            </a>{' '}
+            for more information.
           </h5>
         </center>
         <hr className="my-4" />
