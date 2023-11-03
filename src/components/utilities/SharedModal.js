34e6d99c8692ac52397c7ad532e110055874b3da
John Duprey <jwd@johnwduprey.com>
2023-11-03 13:50:23 -0400

    Mailbox Restores

--- src/components/utilities/SharedModal.js
+++ src/components/utilities/SharedModal.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { CButton, CModal, CModalBody, CModalFooter, CModalHeader, CModalTitle } from '@coreui/react'
 import PropTypes from 'prop-types'
 import { CippTable } from 'src/components/tables'
+import CippCodeBlock from 'src/components/utilities/CippCodeBlock'
 
 /**
  *
@@ -18,6 +19,8 @@ function mapBodyComponent({ componentType, data, componentProps }) {
       return <div>{Array.isArray(data) && data.map((el, idx) => <div key={idx}>{el}</div>)}</div>
     case 'text':
       return String(data)
+    case 'codeblock':
+      return <CippCodeBlock language="text" code={data} {...componentProps} />
     default:
       return String(data)
   }
