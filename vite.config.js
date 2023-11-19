4e3db81fbd1415603a48c1fd690e8aa9b1462215
Kevin Grube <kgrube@covenant-tech.net>
2023-11-18 20:31:38 -0800

    Update eslint config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,31 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import browserslistToEsbuild from 'browserslist-to-esbuild'
+// eslint-disable-next-line import/default
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      // if any eslint warnings, allow build to complete
+      // errors will fail the build
+      ...eslint({
+        failOnWarning: false,
+      }),
+      apply: 'build',
+    },
+    {
+      // in dev mode, still allow build to complete
+      ...eslint({
+        failOnError: false,
+        failOnWarning: false,
+      }),
+      apply: 'serve',
+      enforce: 'post',
+    },
+  ],
   build: {
     outDir: 'build',
     assetsDir: 'static',
