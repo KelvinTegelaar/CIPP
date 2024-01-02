85ec3437112f4c94195b7235ee86ca12bef2346b
John Duprey <jwd@johnwduprey.com>
2024-01-02 00:19:10 -0500

    GraphHelper Presets and bugfixes
    add custom preset support
    bugfixes

--- src/views/tenant/administration/GraphExplorer.jsx
+++ src/views/tenant/administration/GraphExplorer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import {
   CButton,
   CCard,
@@ -8,26 +8,42 @@ import {
   CCol,
   CCollapse,
   CForm,
+  CFormInput,
+  CFormLabel,
+  CFormSwitch,
+  CInputGroup,
   CRow,
+  CTooltip,
 } from '@coreui/react'
 import useQuery from 'src/hooks/useQuery'
 import { Field, Form, FormSpy } from 'react-final-form'
-import { RFFCFormInput, RFFCFormSelect, RFFCFormSwitch } from 'src/components/forms'
+import {
+  RFFCFormInput,
+  RFFCFormSelect,
+  RFFCFormSwitch,
+  RFFSelectSearch,
+} from 'src/components/forms'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight, faChevronDown, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { CippTable } from 'src/components/tables'
 import { useSelector } from 'react-redux'
 import { CippPage } from 'src/components/layout/CippPage'
-import { useLazyGenericGetRequestQuery } from 'src/store/api/app'
+import {
+  useGenericGetRequestQuery,
+  useLazyGenericGetRequestQuery,
+  useLazyGenericPostRequestQuery,
+} from 'src/store/api/app'
 import { OnChange } from 'react-final-form-listeners'
 import { cellGenericFormatter } from 'src/components/tables/CellGenericFormat'
 import PropTypes from 'prop-types'
+import { ModalService } from 'src/components/utilities'
 
 const GraphExplorer = () => {
   const tenant = useSelector((state) => state.app.currentTenant)
   let query = useQuery()
   const [params, setParams] = useState()
   const [random, setRandom] = useState('')
+  const [random2, setRandom2] = useState('')
   const [searchNow, setSearchNow] = useState(false)
   const [visibleA, setVisibleA] = useState(true)
   const handleSubmit = async (values) => {
@@ -36,6 +52,12 @@ const GraphExplorer = () => {
     setSearchNow(true)
   }
   const [execGraphRequest, graphrequest] = useLazyGenericGetRequestQuery()
+  const [execPostRequest, postRequest] = useLazyGenericPostRequestQuery()
+  const {
+    data: customPresets = [],
+    isFetching: presetsIsFetching,
+    error: presetsError,
+  } = useGenericGetRequestQuery({ path: '/api/ListGraphExplorerPresets', params: { random2 } })
   const QueryColumns = { set: false, data: [] }
 
   if (graphrequest.isSuccess) {
@@ -56,89 +78,112 @@ const GraphExplorer = () => {
     QueryColumns.set = true
   }
 
+  const handleManagePreset = ({ values, action, message }) => {
+    var params = {
+      action: action,
+      values: values,
+    }
+    ModalService.confirm({
+      title: 'Confirm',
+      body: <div>{message}</div>,
+      onConfirm: () => execPostRequest({ path: '/api/ExecGraphExplorerPreset', values: params }),
+      confirmLabel: action,
+      cancelLabel: 'Cancel',
+    })
+  }
+
   const presets = [
     {
-      label: 'All users with email addresses',
-      value: '6164e239-0c9a-4a27-9049-6250bf65a3e3',
+      name: 'All users with email addresses',
+      id: '6164e239-0c9a-4a27-9049-6250bf65a3e3',
       params: { endpoint: '/users', $select: 'userprincipalname,mail,proxyAddresses', $filter: '' },
+      isBuiltin: true,
     },
     {
-      label: 'All Devices listing ID, Displayname, and registration status',
-      value: 'e7fdc49a-72a9-4a70-9dbf-a74152495d80',
+      name: 'All Devices listing ID, Displayname, and registration status',
+      id: 'e7fdc49a-72a9-4a70-9dbf-a74152495d80',
       params: {
         endpoint: '/devices',
         $select: 'deviceId,DisplayName,profileType,registrationDateTime,trustType',
         $filter: '',
       },
+      isBuiltin: true,
     },
     {
-      label: 'All contacts and their mail addresses',
-      value: 'f1844e3d-cb3e-4611-9bab-f5f42169bcd0',
+      name: 'All contacts and their mail addresses',
+      id: 'f1844e3d-cb3e-4611-9bab-f5f42169bcd0',
       params: {
         endpoint: '/contacts',
         $select: 'CompanyName,DisplayName,Mail,ProxyAddresses',
         $filter: '',
       },
+      isBuiltin: true,
     },
     {
-      label: 'Outlook Agents used in last 90 days',
-      value: 'eea3cacb-ca95-4998-bcd9-ff1815a7a493',
+      name: 'Outlook Agents used in last 90 days',
+      id: 'eea3cacb-ca95-4998-bcd9-ff1815a7a493',
       params: {
         endpoint: `reports/getEmailAppUsageUserDetail(period='D90')`,
         $format: 'application/json',
         $filter: '',
         $select: '',
+        isBuiltin: true,
       },
     },
     {
-      label: 'Activated M365 Subscription installations',
-      value: 'ccbe5b0d-ff0d-4262-a789-ccbd8f8d52e1',
+      name: 'Activated M365 Subscription installations',
+      id: 'ccbe5b0d-ff0d-4262-a789-ccbd8f8d52e1',
       params: {
         endpoint: '/reports/getOffice365ActivationsUserDetail',
         $format: 'application/json',
         $filter: '',
         $select: '',
       },
+      isBuiltin: true,
     },
     {
-      label: 'Applications signed in in last 30 days',
-      value: 'a9ec9f2d-c102-4b4f-9b9d-c2bc57155990',
+      name: 'Applications signed in in last 30 days',
+      id: 'a9ec9f2d-c102-4b4f-9b9d-c2bc57155990',
       params: {
         endpoint: `reports/getAzureADApplicationSignInSummary(period='D30')`,
         $filter: '',
         $select: '',
       },
+      isBuiltin: true,
     },
     {
-      label: 'User Registration Report',
-      value: 'a00d251d-2743-484a-b8bb-8601199ceb68',
+      name: 'User Registration Report',
+      id: 'a00d251d-2743-484a-b8bb-8601199ceb68',
       params: {
         endpoint: '/reports/authenticationMethods/userRegistrationDetails',
         $filter: '',
         $select: '',
       },
+      isBuiltin: true,
     },
     {
-      label: 'All Global Admins',
-      value: 'c73df2bb-08fe-4fb2-b112-97006bdcf0a8',
+      name: 'All Global Admins',
+      id: 'c73df2bb-08fe-4fb2-b112-97006bdcf0a8',
       params: {
         endpoint: 'directoryRoles/roleTemplateId=62e90394-69f5-4237-9190-012177145e10/members',
         $filter: '',
         $select: '',
       },
+      isBuiltin: true,
     },
     {
-      label: 'Multifactor Authentication Report for Admins',
-      value: 'ae7b3dc4-822b-46b9-aa0a-0305b4c33632',
+      name: 'Multifactor Authentication Report for Admins',
+      id: 'ae7b3dc4-822b-46b9-aa0a-0305b4c33632',
       params: {
         endpoint: '/reports/authenticationMethods/userRegistrationDetails',
         $filter: 'IsAdmin eq true',
         $select: '',
       },
+      isBuiltin: true,
     },
     {
-      label: 'Secure Score with Current Score and Max Score',
-      value: 'bd6665e8-02c1-4cbf-bd3c-d2a52f17c2cf',
+      name: 'Secure Score with Current Score and Max Score',
+      id: 'bd6665e8-02c1-4cbf-bd3c-d2a52f17c2cf',
       params: {
         endpoint: 'security/secureScores',
         $top: 90,
@@ -146,8 +191,22 @@ const GraphExplorer = () => {
         $filter: '',
         NoPagination: true,
       },
+      isBuiltin: true,
     },
   ]
+
+  if (customPresets?.Results?.length > 0) {
+    presets.push({
+      name: '───────────────',
+      id: '',
+      props: {
+        isDisabled: true,
+      },
+    })
+    customPresets.Results.map((preset) => {
+      presets.push(preset)
+    })
+  }
 
   useEffect(() => {
     if (params?.endpoint) {
@@ -171,10 +230,18 @@ const GraphExplorer = () => {
           {({ form }) => (
             <OnChange name={field}>
               {(value) => {
-                let preset = presets.filter(function (obj) {
-                  return obj.value === value
-                })
-                onChange(preset[0]?.params[set])
+                if (value?.value) {
+                  let preset = presets.filter(function (obj) {
+                    return obj.id === value.value
+                  })
+                  if (preset[0]?.id !== '') {
+                    if (preset[0]?.params[set]) {
+                      onChange(preset[0]?.params[set])
+                    } else {
+                      onChange(preset[0][set])
+                    }
+                  }
+                }
               }}
             </OnChange>
           )}
@@ -221,16 +288,94 @@ const GraphExplorer = () => {
                       <CForm onSubmit={handleSubmit}>
                         <CRow>
                           <CCol xs={12} md={4}>
-                            <RFFCFormSelect
-                              name="reportTemplate"
-                              label="Select a report preset"
-                              placeholder="Select a report"
-                              values={presets}
-                            />
-                            <RFFCFormSwitch name="NoPagination" label="Disable Pagination" />
-                            <WhenFieldChanges field="reportTemplate" set="NoPagination" />
+                            <div className="mb-3">
+                              <RFFSelectSearch
+                                name="reportTemplate"
+                                label="Select a report preset"
+                                placeholder="Select a report"
+                                multi={false}
+                                values={presets.map((preset) => {
+                                  return {
+                                    name: preset?.name,
+                                    value: preset?.id,
+                                    props: preset?.props,
+                                  }
+                                })}
+                                refreshFunction={() =>
+                                  setRandom2((Math.random() + 1).toString(36).substring(7))
+                                }
+                                isLoading={presetsIsFetching}
+                              />
+                            </div>
+                            <RFFCFormInput type="text" name="name" label="Preset Name" />
+                            <WhenFieldChanges field="reportTemplate" set="name" />
+                            <RFFCFormSwitch name="IsShared" label="Share Preset" />
+                            <WhenFieldChanges field="reportTemplate" set="IsShared" />
+                            <FormSpy>
+                              {(props) => {
+                                let preset = presets.filter(function (obj) {
+                                  return obj.id === props?.values?.reportTemplate?.value
+                                })
+                                return (
+                                  <>
+                                    <div className="my-2">
+                                      {!preset[0]?.isBuiltin && preset[0]?.id && (
+                                        <CTooltip content="Save" placement="right">
+                                          <CButton
+                                            onClick={() =>
+                                              handleManagePreset({
+                                                action: 'Save',
+                                                message: 'Do you want to save this preset?',
+                                                values: props.values,
+                                              })
+                                            }
+                                            className="me-2"
+                                          >
+                                            <FontAwesomeIcon icon="save" />
+                                          </CButton>
+                                        </CTooltip>
+                                      )}
+                                      {preset[0]?.id !== '' && (
+                                        <CTooltip content="Copy" placement="right">
+                                          <CButton
+                                            onClick={() =>
+                                              handleManagePreset({
+                                                action: 'Copy',
+                                                message: 'Do you want to copy this preset?',
+                                                values: props.values,
+                                              })
+                                            }
+                                            className="me-2"
+                                          >
+                                            <FontAwesomeIcon icon="copy" />
+                                          </CButton>
+                                        </CTooltip>
+                                      )}
+                                      {!preset[0]?.isBuiltin && preset[0]?.id && (
+                                        <CTooltip content="Delete" placement="right">
+                                          <CButton
+                                            color="danger"
+                                            onClick={() =>
+                                              handleManagePreset({
+                                                action: 'Delete',
+                                                message: 'Do you want to delete this preset?',
+                                                values: props.values,
+                                              })
+                                            }
+                                          >
+                                            <FontAwesomeIcon icon="trash" />
+                                          </CButton>
+                                        </CTooltip>
+                                      )}
+                                    </div>
+                                  </>
+                                )
+                              }}
+                            </FormSpy>
                             <RFFCFormSwitch name="$count" label="Use $count" />
                             <WhenFieldChanges field="reportTemplate" set="$count" />
+                            <RFFCFormSwitch name="NoPagination" label="Disable Pagination" />
+                            <WhenFieldChanges field="reportTemplate" set="NoPagination" />
                             <RFFCFormInput
                               type="text"
                               name="$top"
