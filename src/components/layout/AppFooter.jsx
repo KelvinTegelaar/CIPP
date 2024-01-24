00a8851eb02962a4fe246cea8ff791aad306526f
ClintThomson <Clint.Thomson@Celeratec.com>
2024-01-23 23:08:39 -0600

    Adjust Footer Wording.

--- src/components/layout/AppFooter.jsx
+++ src/components/layout/AppFooter.jsx
@@ -20,7 +20,7 @@ const AppFooter = () => {
     <CFooter className="d-flex justify-content-between align-items-center stickyfooter">
       <div className="sponsors">
         <p>
-          This application is sponsored by
+          Quick Links: 
           <CLink className="me-2" href="https://www.huntress.com/">
             <CImage src={huntress} alt="Huntress" />
           </CLink>
