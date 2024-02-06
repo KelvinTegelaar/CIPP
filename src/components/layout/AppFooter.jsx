172500de84ee5d046c5aa5d3920a790ec8f0f95e
ClintThomson <Clint.Thomson@Celeratec.com>
2024-02-06 14:46:53 -0600

    Error Correction

--- src/components/layout/AppFooter.jsx
+++ src/components/layout/AppFooter.jsx
@@ -20,7 +20,7 @@ const AppFooter = () => {
     <CFooter className="d-flex justify-content-between align-items-center stickyfooter">
       <div className="sponsors">
         <p>
-          Quick Links: 
+          Quick Links - 
           <CLink className="me-2" href="https://www.huntress.com/">
             <CImage src={huntress} alt="Huntress" />
           </CLink>
