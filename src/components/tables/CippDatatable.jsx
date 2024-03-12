7eadc374da37f8bfeba4dc2414be23e8db6138e5
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-03-12 17:47:28 +0100

    add anon data warning.

--- src/components/tables/CippDatatable.jsx
+++ src/components/tables/CippDatatable.jsx
@@ -14,12 +14,35 @@ export default function CippDatatable({ path, params, ...rest }) {
     refetch,
   } = useListDatatableQuery({ path, params: { $filter: graphFilter, ...params } })
 
+  //Check if data[0].userPrincipalName OR data[0].UPN OR data.Results[0].upn OR data.Results[0].userPrincipalName
+  // follows this regex pattern ^[A-Z0-9]+$. If it does, set the let 'anonimized' to true.
+  let anonimized = false
+  if (Array.isArray(data?.Results)) {
+    anonimized =
+      data?.Results[0]?.userPrincipalName?.match(/^[A-Z0-9]+$/) ||
+      data?.Results[0]?.UPN?.match(/^[A-Z0-9]+$/)
+  } else {
+    anonimized = data?.userPrincipalName?.match(/^[A-Z0-9]+$/) || data?.UPN?.match(/^[A-Z0-9]+$/)
+  }
+
   var defaultFilterText = ''
   if (params?.Parameters?.$filter) {
     defaultFilterText = 'Graph: ' + params?.Parameters?.$filter
   }
   return (
     <>
+      {anonimized && (
+        <CCallout color="warning">
+          This table might contain anonymized data. Please check this
+          <a
+            className="m-1"
+            href="https://docs.cipp.app/troubleshooting/frequently-asked-questions#my-usernames-or-sites-are-guids-or-blank"
+          >
+            documentation link
+          </a>
+          to resolve this.
+        </CCallout>
+      )}
       {data?.Metadata?.Queued && <CCallout color="info">{data?.Metadata?.QueueMessage}</CCallout>}
       <CippTable
         {...rest}
