86137e5c5017651decf3e84a901617d168d95f4e
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-03-19 11:03:53 +0100

    users fix

--- src/views/identity/administration/Users.jsx
+++ src/views/identity/administration/Users.jsx
@@ -15,7 +15,9 @@ import CippGraphUserFilter from 'src/components/utilities/CippGraphUserFilter'
 const Offcanvas = (row, rowIndex, formatExtraData) => {
   const tenant = useSelector((state) => state.app.currentTenant)
   const [ocVisible, setOCVisible] = useState(false)
-  const viewLink = `/identity/administration/users/view?userId=${row.id}&tenantDomain=${tenant.defaultDomainName}&userEmail=${row.userPrincipalName}`
+  const viewLink = row?.tenant
+    ? `/identity/administration/users/view?userId=${row.id}&tenantDomain=${row.Tenant}&userEmail=${row.userPrincipalName}`
+    : `/identity/administration/users/view?userId=${row.id}&tenantDomain=${tenant.defaultDomainName}&userEmail=${row.userPrincipalName}`
   const editLink = row?.tenant
     ? `/identity/administration/users/edit?userId=${row.id}&tenantDomain=${row.Tenant}`
     : `/identity/administration/users/edit?userId=${row.id}&tenantDomain=${tenant.defaultDomainName}`
