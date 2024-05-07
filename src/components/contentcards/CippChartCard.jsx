55205d20cc8065f7b700c46f7cbd72a97ba26e57
John Duprey <jwd@johnwduprey.com>
2024-05-07 17:49:10 -0400

    Replace maintenance page with Durable Functions

--- src/components/contentcards/CippChartCard.jsx
+++ src/components/contentcards/CippChartCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { CCard, CCardBody, CCardFooter, CCardHeader, CCardTitle } from '@coreui/react'
+import { CButton, CCard, CCardBody, CCardFooter, CCardHeader, CCardTitle } from '@coreui/react'
 import Skeleton from 'react-loading-skeleton'
 import { CChart } from '@coreui/react-chartjs'
 import { getStyle } from '@coreui/utils'
+import PropTypes from 'prop-types'
 
 export default function CippChartCard({
   title,
@@ -13,12 +14,22 @@ export default function CippChartCard({
   ChartType = 'pie',
   LegendLocation = 'bottom',
   isFetching,
+  refreshFunction,
 }) {
   return (
     <CCard className="h-100 mb-3">
       <CCardHeader>
         <CCardTitle>
           {titleType === 'big' ? <h3 className="underline mb-3">{title}</h3> : title}
+          {refreshFunction && (
+            <CButton
+              className="position-absolute top-0 end-0 mt-2 me-2"
+              variant="ghost"
+              onClick={refreshFunction}
+            >
+              <FontAwesomeIcon icon="sync" />
+            </CButton>
+          )}
         </CCardTitle>
       </CCardHeader>
       <CCardBody>
@@ -30,6 +41,7 @@ export default function CippChartCard({
               labels: ChartLabels,
               datasets: [
                 {
+                  label: title,
                   backgroundColor: [
                     getStyle('--cyberdrain-warning'),
                     getStyle('--cyberdrain-info'),
@@ -58,4 +70,14 @@ export default function CippChartCard({
       </CCardBody>
     </CCard>
   )
+}
+CippChartCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  titleType: PropTypes.oneOf(['normal', 'big']),
+  ChartData: PropTypes.array.isRequired,
+  ChartLabels: PropTypes.array.isRequired,
+  ChartType: PropTypes.oneOf(['pie', 'bar', 'line']),
+  LegendLocation: PropTypes.oneOf(['top', 'bottom', 'left', 'right']),
+  isFetching: PropTypes.bool,
+  refreshFunction: PropTypes.func,
 }
