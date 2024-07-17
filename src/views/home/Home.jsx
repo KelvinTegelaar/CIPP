b16d539a8c92d41f32c160d02450e949ac82a917
mmostafa <mmostafa@digitalbunker365.com>
2024-07-17 15:18:08 +0300

    modify in home

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -525,7 +525,7 @@ const Home = () => {
     <>
       <CRow className="mb-3">
         <CCol>
-          <CippContentCard className="h-100" title="Lighthouse Search" titleType="big">
+          <CippContentCard className="h-100" title="Lighthouse Search Test" titleType="big">
             <CRow className="mb-3"></CRow>
             <CRow className="mb-3">
               <CCol>
