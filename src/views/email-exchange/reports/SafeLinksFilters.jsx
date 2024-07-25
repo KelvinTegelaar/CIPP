9213b34c7fd92c865af801973c092527ecd1f57d
Esco <OfficialEsco@users.noreply.github.com>
2024-07-25 12:30:54 +0200

    Added Edit SafeLinks Filter

--- src/views/email-exchange/reports/SafeLinksFilters.jsx
+++ src/views/email-exchange/reports/SafeLinksFilters.jsx
@@ -1,3 +1,8 @@
+import { CButton } from '@coreui/react'
+import { faBan, faBook, faCheck, faEllipsisV, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState } from 'react'
+import { CippActionsOffcanvas } from 'src/components/utilities'
 import { useSelector } from 'react-redux'
 import { CippPageList } from 'src/components/layout'
 import { cellDateFormatter, cellBooleanFormatter, CellTip } from 'src/components/tables'
@@ -192,6 +197,11 @@ const ListSafeLinksFilters = () => {
       exportSelector: 'WhenChanged',
       cell: cellDateFormatter(),
       maxWidth: '150px',
+    },
+    {
+      name: 'Actions',
+      cell: Offcanvas,
+      maxWidth: '80px',
     },
   ]
 
