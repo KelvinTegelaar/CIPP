26d3b557a648825e0ee21e637c72240ce03ea3bd
John Duprey <jwd@johnwduprey.com>
2024-09-03 14:12:43 -0400

    Update index.js

--- src/pages/[tenant]/identity/groups/index.js
+++ src/pages/[tenant]/identity/groups/index.js
@@ -5,11 +5,79 @@ const Page = () => {
   const pageTitle = "Groups";
   const actions = [
     {
-      label: "Delete User",
+      label: "Edit Group",
+      type: "LINK",
+      link: "/[tenant]/identity/groups/[id]",
+      linkParams: {
+        tenant: "TenantFilter",
+        id: "mail",
+      },
+    },
+    {
+      label: "Hide from Global Address List",
       type: "POST",
-      url: "api/DeleteUser",
-      data: { email: "DisplayName" },
-      confirmText: "Are you sure you want to delete this user?",
+      url: "api/ExecGroupsHideFromGAL",
+      data: {
+        TenantFilter: "TenantFilter",
+        ID: "mail",
+        GroupType: "calculatedGroupType",
+        HidefromGAL: true,
+      },
+      confirmText:
+        "Are you sure you want to hide this mailbox from the global address list? Remember this will not work if the group is AD Synched.",
+      multiPost: false,
+    },
+    {
+      label: "Unhide from Global Address List",
+      type: "POST",
+      url: "api/ExecGroupsHideFromGAL",
+      data: {
+        TenantFilter: "TenantFilter",
+        ID: "mail",
+        GroupType: "calculatedGroupType",
+      },
+      confirmText:
+        "Are you sure you want to unhide this mailbox from the global address list? Remember this will not work if the group is AD Synched.",
+      multiPost: false,
+    },
+    {
+      label: "Only allow messages from people inside the organisation",
+      type: "POST",
+      url: "api/ExecGroupsDeliveryManagement",
+      data: {
+        TenantFilter: "TenantFilter",
+        ID: "mail",
+        GroupType: "calculatedGroupType",
+        OnlyAllowInternal: true,
+      },
+      confirmText:
+        "Are you sure you want to only allow messages from people inside the organisation? Remember this will not work if the group is AD Synched.",
+      multiPost: false,
+    },
+    {
+      label: "Allow messages from people inside and outside the organisation",
+      type: "POST",
+      url: "api/ExecGroupsDeliveryManagement",
+      data: {
+        TenantFilter: "TenantFilter",
+        ID: "mail",
+        GroupType: "calculatedGroupType",
+      },
+      confirmText:
+        "Are you sure you want to allow messages from people inside and outside the organisation? Remember this will not work if the group is AD Synched.",
+      multiPost: false,
+    },
+    {
+      label: "Delete Group",
+      type: "POST",
+      url: "api/ExecGroupsDelete",
+      data: {
+        TenantFilter: "TenantFilter",
+        ID: "id",
+        GroupType: "calculatedGroupType",
+        DisplayName: "displayName",
+      },
+      confirmText: "Are you sure you want to delete this group.",
       multiPost: false,
     },
   ];
