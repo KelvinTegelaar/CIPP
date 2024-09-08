96d9ec4af0f2cf2bd838cb2ce296061ad1bcf04d
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-09-08 16:13:43 +0200

    added undraw illustrations

--- src/pages/unauthenticated.js
+++ src/pages/unauthenticated.js
@@ -2,6 +2,7 @@ import { Grid } from "@mui/material";
 import { Layout as DashboardLayout } from "../layouts/index.js";
 import Head from "next/head.js";
 import { Box, Container, Stack } from "@mui/system";
+import { CippImageCard } from "../components/CippCards/CippImageCard.jsx";
 
 const UnauthenticatedPage = () => {
   return (
@@ -19,7 +20,14 @@ const UnauthenticatedPage = () => {
           <Stack spacing={6}>
             <Grid container spacing={3}>
               <Grid item style={{ display: "flex" }} xs={12} md={6}>
-                Unauthenticated. Make pretty.
+                <CippImageCard
+                  isFetching={false}
+                  imageUrl="/assets/illustrations/undraw_writer_q06d.svg"
+                  text="You are not authenticated, how did you end up here? Press the button belong to get back to our home page."
+                  title="Unauthenticated"
+                  linkText={"Return"}
+                  link={"/"}
+                />
               </Grid>
             </Grid>
           </Stack>
