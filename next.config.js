3171676237f93176d257b49ff529e4e4b42d031b
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-09-09 21:37:42 +0200

    disable image optimization server sided

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: false,
+  images: {
+    unoptimized: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
