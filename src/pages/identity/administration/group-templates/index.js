b3c497b63ea031a768d20fdb176071d2f3f6249e
John Duprey <jwd@johnwduprey.com>
2024-09-14 15:31:23 -0400

    fix group templates
    - remove duplicate page definition
    - remove tenant filter

--- src/pages/identity/administration/group-templates/index.js
+++ src/pages/identity/administration/group-templates/index.js
@@ -1,4 +1,4 @@
-import { CippTablePage } from "../../../components/CippComponents/CippTablePage.jsx";
+import { CippTablePage } from "../../../../components/CippComponents/CippTablePage.jsx";
 import { Layout as DashboardLayout } from "../../../../layouts/index.js";
 
 const Page = () => {
@@ -34,63 +34,6 @@ const Page = () => {
     <CippTablePage
       title={pageTitle}
       apiUrl="/api/ListGroupTemplates"
-      apiData={{
-        TenantFilter: "TenantFilter",
-      }}
-      apiDataKey="Results"
-      actions={actions}
-      offCanvas={offCanvas}
-      simpleColumns={["Displayname", "Description", "groupType", "GUID"]}
-      titleButton={{
-        href: "/identity/administration/group-add-template", // Add Group Template Button
-        title: "Add Group Template",
-      }}
-    />
-  );
-};
-
-Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
-
-export default Page;
-import { CippTablePage } from "../../../components/CippComponents/CippTablePage.jsx";
-import { Layout as DashboardLayout } from "../../../../layouts/index.js"; // Absolute pathing applied
-
-const Page = () => {
-  const pageTitle = "Group Templates";
-
-  const actions = [
-    {
-      label: "View Template",
-      type: "OFFCANVAS",
-      offCanvasType: "GroupTemplate",
-      data: {
-        GUID: "GUID", // Pass the GUID dynamically
-      },
-    },
-    {
-      label: "Delete Template",
-      type: "POST",
-      url: "/api/RemoveGroupTemplate",
-      data: {
-        ID: "GUID",
-      },
-      confirmText: "Do you want to delete the template?",
-      multiPost: false,
-    },
-  ];
-
-  const offCanvas = {
-    extendedInfoFields: ["Displayname", "Description", "GUID"],
-    actions: actions,
-  };
-
-  return (
-    <CippTablePage
-      title={pageTitle}
-      apiUrl="/api/ListGroupTemplates"
-      apiData={{
-        TenantFilter: "TenantFilter",
-      }}
       apiDataKey="Results"
       actions={actions}
       offCanvas={offCanvas}
