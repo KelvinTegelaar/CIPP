2258eb909ca469d4c0fbd0c3fe2873cb87fb1b85
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-09-25 01:02:18 +0200

    revert

--- src/pages/401.js
+++ src/pages/401.js
@@ -2,55 +2,45 @@ import { Box, Container, Grid, Stack } from "@mui/material";
 import Head from "next/head";
 import { CippImageCard } from "../components/CippCards/CippImageCard.jsx";
 import { Layout as DashboardLayout } from "../layouts/index.js";
-import { useRouter } from "next/router.js";
 
-const Page = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (router.asPath.startsWith("/api")) {
-      return "Access Denied.";
-    }
-  }, [router.asPath]);
-  return (
-    <>
-      <DashboardLayout>
-        <Head>
-          <title>404 - Not Found</title>
-        </Head>
-        <Box
-          sx={{
-            flexGrow: 1,
-            py: 4,
-            height: "100vh", // Full height of the viewport
-          }}
-        >
-          <Container maxWidth={false}>
-            <Stack spacing={6} sx={{ height: "100%" }}>
-              <Grid
-                container
-                spacing={3}
-                justifyContent="center" // Center horizontally
-                alignItems="center" // Center vertically
-                sx={{ height: "100%" }} // Ensure the container takes full height
-              >
-                <Grid item xs={12} md={6}>
-                  <CippImageCard
-                    isFetching={false}
-                    imageUrl="/assets/illustrations/undraw_articles_wbpb.svg"
-                    text="It seems like you're not allowed to be here. Let's go to the homepage to see if we know where we need to go next."
-                    title="Error 401 - Not allowed"
-                    linkText={"Return"}
-                    link={"/"}
-                  />
-                </Grid>
+const Page = () => (
+  <>
+    <DashboardLayout>
+      <Head>
+        <title>404 - Not Found</title>
+      </Head>
+      <Box
+        sx={{
+          flexGrow: 1,
+          py: 4,
+          height: "100vh", // Full height of the viewport
+        }}
+      >
+        <Container maxWidth={false}>
+          <Stack spacing={6} sx={{ height: "100%" }}>
+            <Grid
+              container
+              spacing={3}
+              justifyContent="center" // Center horizontally
+              alignItems="center" // Center vertically
+              sx={{ height: "100%" }} // Ensure the container takes full height
+            >
+              <Grid item xs={12} md={6}>
+                <CippImageCard
+                  isFetching={false}
+                  imageUrl="/assets/illustrations/undraw_articles_wbpb.svg"
+                  text="It seems like you're not allowed to be here. Let's go to the homepage to see if we know where we need to go next."
+                  title="Error 401 - Not allowed"
+                  linkText={"Return"}
+                  link={"/"}
+                />
               </Grid>
-            </Stack>
-          </Container>
-        </Box>
-      </DashboardLayout>
-    </>
-  );
-};
+            </Grid>
+          </Stack>
+        </Container>
+      </Box>
+    </DashboardLayout>
+  </>
+);
 
 export default Page;
