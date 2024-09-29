acacf482515bce22072cfe3fb0c532b13fa963dd
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-09-29 23:15:59 +0200

    added tenantformcomponent

--- src/components/CippWizard/CippTenantStep.jsx
+++ src/components/CippWizard/CippTenantStep.jsx
@@ -1,6 +1,6 @@
 import { Stack } from "@mui/material";
 import { CippWizardStepButtons } from "./CippWizardStepButtons";
-import CippFormComponent from "../CippComponents/CippFormComponent";
+import { CippFormTenantSelector } from "../CippComponents/CippFormTenantSelector";
 
 export const CippTenantStep = (props) => {
   const {
@@ -11,27 +11,11 @@ export const CippTenantStep = (props) => {
     currentStep,
     onPreviousStep,
   } = props;
+
   return (
     <Stack spacing={3}>
       <label>Select a tenant</label>
-      <CippFormComponent
-        type="autoComplete"
-        name="tenantFilter"
-        formControl={formControl}
-        placeholder="Select a tenant"
-        api={{
-          excludeTenantFilter: true,
-          url: allTenants ? "/api/ListTenants" : "/api/ListTenants?AllTenantSelector=true",
-          queryKey: "ListTenants",
-          labelField: (option) => `${option.displayName} (${option.defaultDomainName})`,
-          valueField: "customerId",
-        }}
-        multiple={type === "single" ? false : true}
-        disableClearable={true}
-        validators={{
-          required: { value: true, message: "This field is required" },
-        }}
-      />
+      <CippFormTenantSelector formControl={formControl} allTenants={allTenants} type={type} />
       <CippWizardStepButtons
         currentStep={currentStep}
         onPreviousStep={onPreviousStep}
