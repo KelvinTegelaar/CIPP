1655fceeeba67fd48dff218104fbd256f8428eb4
KelvinTegelaar <Kelvin@limenetworks.nl>
2024-10-15 23:54:17 +0200

    optional formats

--- src/pages/tenant/standards/bpa-report/builder.js
+++ src/pages/tenant/standards/bpa-report/builder.js
@@ -24,6 +24,7 @@ import CippButtonCard from "../../../../components/CippCards/CippButtonCard";
 import CippFormComponent from "../../../../components/CippComponents/CippFormComponent";
 import { ArrowLeftIcon } from "@mui/x-date-pickers";
 import { useRouter } from "next/router";
+import { CippFormCondition } from "../../../../components/CippComponents/CippFormCondition";
 
 const Page = () => {
   const formControl = useForm({
@@ -134,34 +135,48 @@ const Page = () => {
             </Typography>
           </Stack>
           {/* Card for Layout Controls */}
-          <Card sx={{ mb: 3 }}>
-            <CardContent>
-              <FormControl fullWidth>
-                <InputLabel id="layout-mode-label">Layout Mode</InputLabel>
-                <Select
-                  labelId="layout-mode-label"
-                  id="layout-mode"
-                  value={layoutMode}
-                  onChange={handleLayoutModeChange}
-                  fullWidth
-                >
-                  <MenuItem value="Block">Block</MenuItem>
-                  <MenuItem value="Table">Table</MenuItem>
-                </Select>
-              </FormControl>
+          <CippButtonCard
+            CardButton={
+              <Button variant="contained" onClick={saveConfig} sx={{ ml: 2 }}>
+                Save Dashboard Configuration
+              </Button>
+            }
+            title="Layout Controls"
+          >
+            <Grid container spacing={4}>
+              {/* First item for Report Name and Layout Mode */}
+              <Grid item xs={12} sm={12} md={12}>
+                <CippFormComponent label="Report Name" name={`name`} formControl={formControl} />
+              </Grid>
 
+              <Grid item xs={12} sm={12} md={12}>
+                <FormControl fullWidth>
+                  <InputLabel id="layout-mode-label">Layout Mode</InputLabel>
+                  <Select
+                    labelId="layout-mode-label"
+                    id="layout-mode"
+                    value={layoutMode}
+                    onChange={handleLayoutModeChange}
+                    fullWidth
+                  >
+                    <MenuItem value="Block">Block</MenuItem>
+                    <MenuItem value="Table">Table</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+
+              {/* Third item for Buttons */}
               {layoutMode === "Block" && (
-                <Box sx={{ mt: 2 }}>
-                  <Button variant="contained" onClick={handleAddBlock}>
-                    Add Block
-                  </Button>
-                  <Button variant="contained" onClick={saveConfig} sx={{ ml: 2 }}>
-                    Save Dashboard Configuration
-                  </Button>
-                </Box>
+                <Grid item xs={12} sm={12} md={12}>
+                  <Box sx={{ mt: 2 }}>
+                    <Button variant="contained" onClick={handleAddBlock}>
+                      Add Block
+                    </Button>
+                  </Box>
+                </Grid>
               )}
-            </CardContent>
-          </Card>
+            </Grid>
+          </CippButtonCard>
 
           {/* Canvas Area */}
           <Typography variant="h6" gutterBottom>
@@ -192,13 +207,6 @@ const Page = () => {
                 >
                   {/* Form inside each card */}
                   <form onSubmit={handleSubmit(onSubmit)}>
-                    {/* Report Name */}
-                    <CippFormComponent
-                      label="Report Name"
-                      name={`Fields.${index}.name`}
-                      formControl={formControl}
-                    />
-
                     {/* Report Style */}
                     <CippFormComponent
                       label="Report Style"
@@ -208,102 +216,123 @@ const Page = () => {
                       type="autocomplete"
                     />
 
-                    {/* Use Existing Info Switch */}
-                    <CippFormComponent
-                      label="Use Existing Info?"
-                      name={`Fields.${index}.UseExistingInfo`}
-                      formControl={formControl}
-                      type="switch"
-                    />
-
                     {/* Frontend Fields */}
                     <CippFormComponent
-                      label="Frontend Field Name"
+                      label="Card Name"
                       name={`Fields.${index}.FrontendFields.0.name`}
                       formControl={formControl}
                     />
 
                     <CippFormComponent
-                      label="Frontend Field Value"
-                      name={`Fields.${index}.FrontendFields.0.value`}
-                      formControl={formControl}
-                    />
-
-                    <CippFormComponent
-                      label="Frontend Field Formatter"
-                      name={`Fields.${index}.FrontendFields.0.formatter`}
-                      formControl={formControl}
-                      autoCompleteOptions={[
-                        "string",
-                        "bool",
-                        "warnBool",
-                        "reverseBool",
-                        "table",
-                        "number",
-                      ]}
-                      type="autocomplete"
-                    />
-
-                    {/* Field Description */}
-                    <CippFormComponent
-                      label="Field Description"
+                      label="Card Description"
                       name={`Fields.${index}.desc`}
                       formControl={formControl}
                     />
 
-                    {/* Additional Fields */}
                     <CippFormComponent
-                      label="API"
-                      name={`Fields.${index}.API`}
+                      label="Card Formatter"
+                      name={`Fields.${index}.FrontendFields.0.formatter`}
                       formControl={formControl}
-                      autoCompleteOptions={["Graph", "Exchange", "CIPPFunction"]}
-                      type="autocomplete"
+                      multiple={false}
+                      options={[
+                        { label: "String - Show as text", value: "string" },
+                        { label: "Boolean - Show a true/false check", value: "bool" },
+                        {
+                          label: "Warn Boolean - Show a true/false check that is orange when false",
+                          value: "warnBool",
+                        },
+                        {
+                          label: "Reverse Boolean - False is green, True is red.",
+                          value: "reverseBool",
+                        },
+                        { label: "Table - Show the data in a table", value: "table" },
+                        { label: "Number - Show the value as a number", value: "number" },
+                        {
+                          label: "Percentage - Show the value as a percentage",
+                          value: "percentage",
+                        },
+                      ]}
+                      type="autoComplete"
                     />
 
                     <CippFormComponent
-                      label="Store As"
-                      name={`Fields.${index}.StoreAs`}
-                      formControl={formControl}
-                      autoCompleteOptions={["string", "JSON", "bool"]}
-                      type="autocomplete"
-                    />
-
-                    <CippFormComponent
-                      label="Extract Fields"
-                      name={`Fields.${index}.ExtractFields`}
-                      formControl={formControl}
-                      type="array"
-                    />
-
-                    <CippFormComponent
-                      label="Where"
-                      name={`Fields.${index}.where`}
-                      formControl={formControl}
-                    />
-
-                    <CippFormComponent
-                      label="Command"
-                      name={`Fields.${index}.Command`}
-                      formControl={formControl}
-                    />
-
-                    <CippFormComponent
-                      label="Graph URL"
-                      name={`Fields.${index}.URL`}
-                      formControl={formControl}
-                    />
-
-                    <CippFormComponent
-                      label="Use Application Permissions"
-                      name={`Fields.${index}.parameters.asApp`}
+                      label="Use information CIPP has previously gathered in another report"
+                      name={`Fields.${index}.UseExistingInfo`}
                       formControl={formControl}
                       type="switch"
                     />
+                    <CippFormCondition
+                      field={`Fields.${index}.UseExistingInfo`}
+                      compareValue={true}
+                      formControl={formControl}
+                    >
+                      //List all fields with label/value
+                    </CippFormCondition>
 
+                    <CippFormCondition
+                      field={`Fields.${index}.UseExistingInfo`}
+                      compareValue={false}
+                      formControl={formControl}
+                    >
+                      <CippFormComponent
+                        label="Data source"
+                        name={`Fields.${index}.API`}
+                        formControl={formControl}
+                        multiple={false}
+                        options={[
+                          { label: "Microsoft Graph", value: "Graph" },
+                          { label: "Exchange Online PowerShell", value: "Exchange" },
+                          { label: "CIPP Function", value: "CIPPFunction" },
+                        ]}
+                        type="autoComplete"
+                      />
+                      <CippFormCondition
+                        field={`Fields.${index}.API`}
+                        compareType="is"
+                        compareValue={{ label: "Microsoft Graph", value: "Graph" }}
+                        formControl={formControl}
+                      >
+                        <CippFormComponent
+                          label="Graph URL"
+                          name={`Fields.${index}.URL`}
+                          formControl={formControl}
+                        />
+                        <CippFormComponent
+                          label="Use Application Permissions"
+                          name={`Fields.${index}.parameters.asApp`}
+                          formControl={formControl}
+                          type="switch"
+                        />
+                      </CippFormCondition>
+
+                      <CippFormComponent
+                        label="What fields from the API response should we save?"
+                        name={`Fields.${index}.ExtractFields`}
+                        formControl={formControl}
+                        type="array"
+                      />
+
+                      <CippFormComponent
+                        label="Store As"
+                        name={`Fields.${index}.StoreAs`}
+                        formControl={formControl}
+                        autoCompleteOptions={["string", "JSON", "bool"]}
+                        type="autocomplete"
+                      />
+
+                      <CippFormComponent
+                        label="Where"
+                        name={`Fields.${index}.where`}
+                        formControl={formControl}
+                      />
+
+                      <CippFormComponent
+                        label="Command"
+                        name={`Fields.${index}.Command`}
+                        formControl={formControl}
+                      />
+                    </CippFormCondition>
                     {/* Submit Button */}
-                    <Button type="submit" variant="contained">
-                      Submit Report
-                    </Button>
                   </form>
                 </CippButtonCard>
               </Grid>
