3e0a94a0e32fe40aff2beabbd4316cecd60ff395
John Duprey <jwd@johnwduprey.com>
2024-10-14 23:09:27 -0400

    extension tweaks

--- src/pages/cipp/integrations/configure.js
+++ src/pages/cipp/integrations/configure.js
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Stack, Typography } from "@mui/material";
 import CippFormPage from "/src/components/CippFormPages/CippFormPage";
 import CippFormComponent from "/src/components/CippComponents/CippFormComponent";
 import { Layout as DashboardLayout } from "/src/layouts/index.js";
@@ -8,12 +8,15 @@ import { ApiGetCall } from "/src/api/ApiCall";
 import { useRouter } from "next/router";
 import extensions from "/src/data/Extensions.json";
 import { useEffect } from "react";
-import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
+import { ArrowPathIcon, ArrowTopRightOnSquareIcon, BeakerIcon } from "@heroicons/react/24/outline";
 import { SvgIcon } from "@mui/material";
+import { useState } from "react";
 
 const Page = () => {
   const router = useRouter();
   const settings = useSettings();
+
+  const [formIsDirty, setFormIsDirty] = useState(false);
   const preferredTheme = settings.currentTheme?.value;
   const integrations = ApiGetCall({
     url: "/api/ListExtensionsConfig",
@@ -41,6 +44,20 @@ const Page = () => {
     }
   }, [integrations.isSuccess]);
 
+  useEffect(() => {
+    // if form fields are not saved and differ from the default values disable the test/sync buttons
+    if (
+      formControl.formState.dirtyFields &&
+      Object.keys(formControl.formState.dirtyFields).length > 0 &&
+      !formIsDirty &&
+      !formControl.formState.isSubmitted
+    ) {
+      setFormIsDirty(true);
+    } else {
+      setFormIsDirty(false);
+    }
+  }, [formControl, setFormIsDirty]);
+
   return (
     <>
       {integrations.isSuccess && extension ? (
@@ -56,18 +73,36 @@ const Page = () => {
           <Typography variant="body2" paragraph style={{ marginTop: "1em" }}>
             {extension.helpText}
           </Typography>
-          {extension?.links && (
-            <>
-              {extension.links.map((link, index) => (
-                <Button href={link.url} target="_blank" rel="noreferrer" color="inherit">
-                  <SvgIcon fontSize="small" style={{ marginRight: "8" }}>
-                    <ArrowTopRightOnSquareIcon />
-                  </SvgIcon>
-                  {link.name}
-                </Button>
-              ))}
-            </>
-          )}
+          <Stack direction="row" spacing={2} sx={{ mb: 2 }}>
+            {extension?.hideTestButton !== true && (
+              <Button variant="contained" color="primary" disabled={formIsDirty}>
+                <SvgIcon fontSize="small" style={{ marginRight: "8" }}>
+                  <BeakerIcon />
+                </SvgIcon>
+                Test
+              </Button>
+            )}
+            {extension?.forceSyncButton && (
+              <Button variant="contained" color="primary" disabled={formIsDirty}>
+                <SvgIcon fontSize="small" style={{ marginRight: "8" }}>
+                  <ArrowPathIcon />
+                </SvgIcon>
+                Force Sync
+              </Button>
+            )}
+            {extension?.links && (
+              <>
+                {extension.links.map((link, index) => (
+                  <Button href={link.url} target="_blank" rel="noreferrer" color="inherit">
+                    <SvgIcon fontSize="small" style={{ marginRight: "8" }}>
+                      <ArrowTopRightOnSquareIcon />
+                    </SvgIcon>
+                    {link.name}
+                  </Button>
+                ))}
+              </>
+            )}
+          </Stack>
           <Box sx={{ my: 2 }}>
             {extension.SettingOptions.map((setting, index) => (
               <Box key={index} sx={{ mb: 2 }}>
