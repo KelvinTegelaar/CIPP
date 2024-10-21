5c70d94fa9c7dc59f78b327f6bfd8c979837baa8
cipp-ashe <ashley@cyberdrain.com>
2024-10-21 16:04:35 -0400

    adding mfa report page conversion - needs filter integration review

--- src/pages/identity/reports/mfa-report/index.js
+++ src/pages/identity/reports/mfa-report/index.js
@@ -1,17 +1,47 @@
-
 import { Layout as DashboardLayout } from "/src/layouts/index.js";
+import { CippTablePage } from "/src/components/CippComponents/CippTablePage.jsx";
 
 const Page = () => {
   const pageTitle = "MFA Report";
+  const apiUrl = "/api/ListMFAUsers";
+  const actions = [];
+  
+  const offCanvas = null;
+
+  const simpleColumns = [
+    "UPN", 
+    "AccountEnabled", 
+    "isLicensed", 
+    "MFARegistration", 
+    "PerUser", 
+    "CoveredBySD", 
+    "CoveredByCA", 
+    "MFAMethods", 
+    "CAPolicies"
+  ];
+
+  /* Filters not supported in the current structure, need dev attention for integration.
+  filterlist: [
+    { filterName: 'Enabled users', filter: '"accountEnabled":true' },
+    { filterName: 'Non-guest users', filter: 'Complex: UPN notlike #EXT#' },
+    { filterName: 'Licensed users', filter: 'Complex: IsLicensed eq true' },
+    { filterName: 'Enabled, licensed non-guest users missing MFA', filter: 'Complex: UPN notlike #EXT#; IsLicensed eq true; accountEnabled eq true; MFARegistration ne true' },
+    { filterName: 'No MFA methods registered', filter: 'Complex: MFARegistration ne true' },
+    { filterName: 'MFA methods registered', filter: 'Complex: MFARegistration eq true' },
+  ],
+  */
 
   return (
-    <div>
-      <h1>{pageTitle}</h1>
-      <p>This is a placeholder page for the mfa report section.</p>
-    </div>
+    <CippTablePage
+      title={pageTitle}
+      apiUrl={apiUrl}
+      apiDataKey="Results"
+      actions={actions}
+      offCanvas={offCanvas}
+      simpleColumns={simpleColumns}
+    />
   );
 };
 
 Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
-
 export default Page;
