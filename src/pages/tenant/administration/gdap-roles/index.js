f8601bbbe473bbbb70e04639e2c9cf25dc1ab0f1
cipp-ashe <ashley@cyberdrain.com>
2024-10-23 18:08:04 -0400

    importing the conversion to new format for the gdap roles page

--- src/pages/tenant/administration/gdap-roles/index.js
+++ src/pages/tenant/administration/gdap-roles/index.js
@@ -1,14 +1,38 @@
-
 import { Layout as DashboardLayout } from "/src/layouts/index.js";
+import { CippTablePage } from "/src/components/CippComponents/CippTablePage.jsx";
+import { Button } from "@mui/material";
+import Link from "next/link";
+
+const pageTitle = "GDAP Role List";
+
+const actions = [
+  {
+    label: "Delete Mapping",
+    modal: true,
+    modalUrl: "/api/ExecDeleteGDAPRoleMapping?&GroupId=[GroupId]",
+    modalMessage:
+      "Are you sure you want to delete this role mapping? (Note: This does not delete the associated security groups or modify any GDAP relationships.)",
+  },
+];
+
+const simpleColumns = ["RoleName", "GroupName"];
+
+const apiUrl = "/api/ListGDAPRoles";
 
 const Page = () => {
-  const pageTitle = "GDAP Roles";
-
   return (
-    <div>
-      <h1>{pageTitle}</h1>
-      <p>This is a placeholder page for the gdap roles section.</p>
-    </div>
+    <CippTablePage
+      title={pageTitle}
+      apiUrl={apiUrl}
+      apiDataKey="Results"
+      actions={actions}
+      simpleColumns={simpleColumns}
+      cardButton={
+        <Button component={Link} href="/tenant/administration/gdap-role-wizard">
+          Map GDAP Roles
+        </Button>
+      }
+    />
   );
 };
 
