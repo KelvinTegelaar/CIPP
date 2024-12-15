e071f9d4f67d78b66c0da7d0e1b256c7fcf7ad5c
John Duprey <jwd@johnwduprey.com>
2024-12-15 14:45:03 -0500

    geo ip lookup

--- src/pages/tenant/tools/geoiplookup/index.js
+++ src/pages/tenant/tools/geoiplookup/index.js
@@ -1,35 +1,42 @@
-import {
-  Box,
-  Button,
-  Container,
-  Grid,
-  Typography,
-  CircularProgress,
-  Skeleton,
-  Link,
-} from "@mui/material";
+import { Box, Button, Container, Skeleton } from "@mui/material";
+import { Grid, Stack } from "@mui/system";
 import { Layout as DashboardLayout } from "/src/layouts/index.js";
 import { useForm, useWatch } from "react-hook-form";
 import CippButtonCard from "../../../../components/CippCards/CippButtonCard";
-import { Search } from "@mui/icons-material";
+import { Delete, Search } from "@mui/icons-material";
 import CippFormComponent from "../../../../components/CippComponents/CippFormComponent";
-import { ApiGetCall, ApiPostCall } from "../../../../api/ApiCall";
+import { ApiPostCall } from "../../../../api/ApiCall";
 import { getCippValidator } from "../../../../utils/get-cipp-validator";
 import { CippDataTable } from "../../../../components/CippTable/CippDataTable";
 import { useSettings } from "../../../../hooks/use-settings";
 import { CippApiResults } from "../../../../components/CippComponents/CippApiResults";
+import CippGeoLocation from "../../../../components/CippComponents/CippGeoLocation";
+import { useState } from "react";
+import { MapPinIcon } from "@heroicons/react/24/outline";
 
 const Page = () => {
   const currentTenant = useSettings().currentTenant;
-  const actions = [];
   const formControl = useForm({ mode: "onBlur" });
   const ip = useWatch({ control: formControl.control, name: "ipAddress" });
-  const getGeoIP = ApiGetCall({
-    url: "/api/ExecGeoIPLookup",
-    data: { ip },
-    queryKey: `geoiplookup-${ip}`,
-    waiting: false,
-  });
+  const [ipAddress, setIpAddress] = useState(null);
+
+  const actions = [
+    {
+      label: "View Location",
+      customFunction: (row) => setIpAddress(row.RowKey),
+      noConfirm: true,
+      icon: <MapPinIcon />,
+    },
+    {
+      label: "Remove from Whitelist",
+      customFunction: (row) =>
+        addGeoIP.mutate({
+          url: `/api/ExecAddTrustedIP?IP=${row.RowKey}&TenantFilter=${currentTenant}&State=NotTrusted`,
+        }),
+      icon: <Delete />,
+      confirmText: "Are you sure you want to remove this IP from the whitelist?",
+    },
+  ];
 
   const addGeoIP = ApiPostCall({
     relatedQueryKeys: [`geoiplookup-${ip}`, "ListIPWhitelist"],
@@ -55,14 +62,14 @@ const Page = () => {
       }}
     >
       <Container maxWidth={false}>
-        <Grid container spacing={3}>
+        <Grid container spacing={1}>
           <Grid item xs={4}>
             <CippButtonCard
               title="Geo IP Check"
               cardSx={{ display: "flex", flexDirection: "column", height: "100%" }}
             >
               <Grid container spacing={2}>
-                <Grid item xs={8}>
+                <Grid item size={8}>
                   <CippFormComponent
                     formControl={formControl}
                     name="ipAddress"
@@ -74,10 +81,10 @@ const Page = () => {
                     required
                   />
                 </Grid>
-                <Grid item xs={4}>
+                <Grid item size={4}>
                   <Button
                     type="submit"
-                    onClick={() => getGeoIP.refetch()}
+                    onClick={() => setIpAddress(ip)}
                     variant="contained"
                     startIcon={<Search />}
                   >
@@ -87,87 +94,42 @@ const Page = () => {
               </Grid>
             </CippButtonCard>
           </Grid>
+          <Grid item size={8}></Grid>
 
           {/* Results Card */}
-          {getGeoIP.isFetching ? (
-            <Grid item xs={8}>
-              <CippButtonCard title="Fetching Results">
-                <Grid container spacing={2}>
-                  <Grid item xs={12} textAlign="center">
-                    <Skeleton width={"100%"} />
-                  </Grid>
-                </Grid>
-              </CippButtonCard>
-            </Grid>
-          ) : getGeoIP.data ? (
-            <Grid item xs={8}>
+          {ipAddress && (
+            <Grid item size={6}>
               <CippButtonCard title="Geo IP Results">
                 <Grid container spacing={2}>
-                  <Grid item xs={6}>
-                    <Typography variant="body1">
-                      <strong>IP Address:</strong> {ip}
-                    </Typography>
-                    <Typography variant="body1">
-                      <strong>Country:</strong> {getGeoIP.data?.country}
-                    </Typography>
-                    <Typography variant="body1">
-                      <strong>Region:</strong> {getGeoIP.data?.region}
-                    </Typography>
-                    <Typography variant="body1">
-                      <strong>City:</strong> {getGeoIP.data?.city}
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography variant="body1">
-                      <strong>ISP:</strong> {getGeoIP.data?.isp}
-                    </Typography>
-                    <Typography variant="body1">
-                      <strong>Latitude:</strong> {getGeoIP.data?.lat}
-                    </Typography>
-                    <Typography variant="body1">
-                      <strong>Longitude:</strong> {getGeoIP.data?.lon}
-                    </Typography>
-                    <Typography variant="body1">
-                      <strong>Organization:</strong> {getGeoIP.data?.org}
-                    </Typography>
-                    <Typography variant="body1">
-                      <strong>Map Location:</strong>{" "}
-                      <Link
-                        href={`https://www.google.com/maps/search/${getGeoIP.data?.lat}+${getGeoIP.data?.lon}`}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        View on Map
-                      </Link>
-                    </Typography>
+                  <Grid item size={12}>
+                    <CippGeoLocation ipAddress={ipAddress} />
                   </Grid>
                 </Grid>
                 <Grid container spacing={2} mt={2}>
-                  <Grid item xs={6}>
-                    <Button variant="contained" color="primary" onClick={handleAddToWhitelist}>
-                      Add to Whitelist
-                    </Button>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Button variant="outlined" color="error" onClick={handleRemoveFromWhitelist}>
-                      Remove from Whitelist
-                    </Button>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <CippApiResults apiObject={addGeoIP} />
+                  <Grid item size={12}>
+                    <Stack direction="row" spacing={1}>
+                      <Button variant="contained" color="primary" onClick={handleAddToWhitelist}>
+                        Add to Whitelist
+                      </Button>
+
+                      <Button variant="outlined" color="error" onClick={handleRemoveFromWhitelist}>
+                        Remove from Whitelist
+                      </Button>
+                    </Stack>
                   </Grid>
                 </Grid>
               </CippButtonCard>
             </Grid>
-          ) : null}
-          <Grid item xs={12}>
+          )}
+          <Grid item size={6}>
             <CippDataTable
               title={"IP Whitelist"}
               api={{ url: "/api/ListIPWhitelist" }}
               queryKey={"ListIPWhitelist"}
-              simple={false}
               simpleColumns={["PartitionKey", "state", "RowKey"]}
+              actions={actions}
             />
+            <CippApiResults apiObject={addGeoIP} />
           </Grid>
         </Grid>
       </Container>
