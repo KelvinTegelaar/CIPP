330e11298db6ca34b9e73675e20f020a24f14789
John Duprey <jwd@johnwduprey.com>
2025-01-09 17:33:27 -0500

    user exchange settings page tweaks

--- src/pages/identity/administration/users/user/exchange.jsx
+++ src/pages/identity/administration/users/user/exchange.jsx
@@ -7,7 +7,7 @@ import CalendarIcon from "@heroicons/react/24/outline/CalendarIcon";
 import { Check, Error, Mail } from "@mui/icons-material";
 import { HeaderedTabbedLayout } from "../../../../../layouts/HeaderedTabbedLayout";
 import tabOptions from "./tabOptions";
-import ReactTimeAgo from "react-time-ago";
+import { CippTimeAgo } from "../../../../../components/CippComponents/CippTimeAgo";
 import { CippCopyToClipBoard } from "../../../../../components/CippComponents/CippCopyToClipboard";
 import { Box, Stack } from "@mui/system";
 import Grid from "@mui/material/Grid2";
@@ -16,12 +16,13 @@ import { CippExchangeInfoCard } from "../../../../../components/CippCards/CippEx
 import { useEffect, useState } from "react";
 import CippExchangeSettingsForm from "../../../../../components/CippFormPages/CippExchangeSettingsForm";
 import { useForm } from "react-hook-form";
-import { CircularProgress } from "@mui/material";
+import { Alert, Button, Collapse, CircularProgress, Typography } from "@mui/material";
 import { CippApiResults } from "../../../../../components/CippComponents/CippApiResults";
 
 const Page = () => {
   const userSettingsDefaults = useSettings();
   const [waiting, setWaiting] = useState(false);
+  const [showDetails, setShowDetails] = useState(false);
   const router = useRouter();
   const { userId } = router.query;
 
@@ -31,7 +32,11 @@ const Page = () => {
       tenantFilter: userSettingsDefaults.currentTenant,
     },
   });
-
+  const graphUserRequest = ApiGetCall({
+    url: `/api/ListUsers?UserId=${userId}&tenantFilter=${userSettingsDefaults.currentTenant}`,
+    queryKey: `ListUsers-${userId}`,
+    waiting: waiting,
+  });
   const userRequest = ApiGetCall({
     url: `/api/ListUserMailboxDetails?UserId=${userId}&tenantFilter=${userSettingsDefaults.currentTenant}`,
     queryKey: `Mailbox-${userId}`,
@@ -76,26 +81,21 @@ const Page = () => {
     }
   }, [userId]);
 
-  const title = userRequest.isSuccess
-    ? userRequest.data?.[0]?.Mailbox?.[0]?.DisplayName
-    : "Loading...";
+  const title = graphUserRequest.isSuccess ? graphUserRequest.data?.[0]?.displayName : "Loading...";
 
-  const subtitle = userRequest.isSuccess
+  const subtitle = graphUserRequest.isSuccess
     ? [
         {
           icon: <Mail />,
           text: (
-            <CippCopyToClipBoard
-              type="chip"
-              text={userRequest.data?.[0]?.Mailbox?.[0]?.UserPrincipalName}
-            />
+            <CippCopyToClipBoard type="chip" text={graphUserRequest.data?.[0]?.userPrincipalName} />
           ),
         },
         {
           icon: <CalendarIcon />,
           text: (
             <>
-              Created: <ReactTimeAgo date={userRequest.data?.[0]?.Mailbox?.[0]?.WhenCreated} />
+              Created <CippTimeAgo data={graphUserRequest.data?.[0]?.createdDateTime} />
             </>
           ),
         },
@@ -173,30 +173,59 @@ const Page = () => {
           }}
         >
           <Grid container spacing={2}>
-            <Grid item size={4}>
-              <CippExchangeInfoCard exchangeData={data} isFetching={userRequest.isLoading} />
-            </Grid>
-            <Grid item size={8}>
-              <Stack spacing={3}>
-                <CippBannerListCard
-                  isFetching={userRequest.isLoading}
-                  items={permissions}
-                  isCollapsible={userRequest.data?.[0]?.Permissions?.length !== 0}
-                />
-                <CippBannerListCard
-                  isFetching={calPermissions.isLoading}
-                  items={calCard}
-                  isCollapsible={calPermissions.data?.length !== 0}
-                />
-                <CippExchangeSettingsForm
-                  userId={userId}
-                  calPermissions={calPermissions.data}
-                  currentSettings={userRequest.data?.[0]}
-                  isFetching={userRequest.isFetching}
-                  formControl={formControl}
-                />
-              </Stack>
-            </Grid>
+            {userRequest?.data?.[0]?.Mailbox?.[0]?.error && (
+              <Grid item size={12}>
+                <Alert severity="error">
+                  <Box display="flex" justifyContent="space-between">
+                    <Typography variant="body2">
+                      {userRequest?.data?.[0]?.Mailbox?.[0]?.error.includes(
+                        "Microsoft.Exchange.Configuration.Tasks.ManagementObjectNotFoundException"
+                      )
+                        ? "This user does not have a mailbox, make sure they are licensed for Exchange."
+                        : "An error occurred while fetching the mailbox details."}
+                    </Typography>
+                    <Button
+                      size="small"
+                      onClick={() => setShowDetails(!showDetails)}
+                      sx={{ mr: 1 }}
+                    >
+                      {showDetails ? "Hide Details" : "Show Details"}
+                    </Button>
+                  </Box>
+                  <Collapse in={showDetails}>
+                    <Box mt={2}>{userRequest?.data?.[0]?.Mailbox?.[0]?.error}</Box>
+                  </Collapse>
+                </Alert>
+              </Grid>
+            )}
+            {!userRequest?.data?.[0]?.Mailbox?.[0]?.error && (
+              <>
+                <Grid item size={4}>
+                  <CippExchangeInfoCard exchangeData={data} isFetching={userRequest.isLoading} />
+                </Grid>
+                <Grid item size={8}>
+                  <Stack spacing={3}>
+                    <CippBannerListCard
+                      isFetching={userRequest.isLoading}
+                      items={permissions}
+                      isCollapsible={userRequest.data?.[0]?.Permissions?.length !== 0}
+                    />
+                    <CippBannerListCard
+                      isFetching={calPermissions.isLoading}
+                      items={calCard}
+                      isCollapsible={calPermissions.data?.length !== 0}
+                    />
+                    <CippExchangeSettingsForm
+                      userId={userId}
+                      calPermissions={calPermissions.data}
+                      currentSettings={userRequest.data?.[0]}
+                      isFetching={userRequest.isFetching}
+                      formControl={formControl}
+                    />
+                  </Stack>
+                </Grid>
+              </>
+            )}
           </Grid>
         </Box>
       )}
