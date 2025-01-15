563cee5dfb234ce1ac5a017d5b86c7846a3689b2
Esco <OfficialEsco@users.noreply.github.com>
2025-01-15 14:26:07 +0100

    feat: added features to Inactive users page

--- src/pages/identity/reports/inactive-users-report/index.js
+++ src/pages/identity/reports/inactive-users-report/index.js
@@ -1,17 +1,57 @@
 import { Layout as DashboardLayout } from "/src/layouts/index.js";
 import { CippTablePage } from "/src/components/CippComponents/CippTablePage.jsx";
+import { EyeIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { Edit } from "@mui/icons-material";
 
 const Page = () => {
   const pageTitle = "Inactive users (6 months)";
   const apiUrl = "/api/ListInactiveAccounts";
-  const actions = [];
+  const actions = [
+    {
+      label: "View User",
+      link: "/identity/administration/users/user?userId=[azureAdUserId]",
+      multiPost: false,
+      icon: <EyeIcon />,
+      color: "success",
+    },
+    {
+      label: "Edit User",
+      link: "/identity/administration/users/user/edit?userId=[azureAdUserId]",
+      icon: <Edit />,
+      color: "success",
+      target: "_self",
+    },
+    {
+      label: "Delete User",
+      type: "GET",
+      icon: <TrashIcon />,
+      url: "/api/RemoveUser",
+      data: { ID: "azureAdUserId" },
+      confirmText: "Are you sure you want to delete this user?",
+      multiPost: false,
+    },
+  ];
 
-  const offCanvas = null;
+  const offCanvas = {
+    extendedInfoFields: [
+      "tenantDisplayName",
+      "displayName",
+      "userPrincipalName",
+      "userType",
+      "createdDateTime",
+      "lastSignInDateTime",
+      "lastNonInteractiveSignInDateTime",
+      "numberOfAssignedLicenses",
+      "lastRefreshedDateTime",
+    ],
+    actions: actions,
+  };
 
   const simpleColumns = [
     "tenantDisplayName",
     "userPrincipalName",
     "displayName",
+    "lastSignInDateTime",
     "lastNonInteractiveSignInDateTime",
     "numberOfAssignedLicenses",
     "lastRefreshedDateTime",
