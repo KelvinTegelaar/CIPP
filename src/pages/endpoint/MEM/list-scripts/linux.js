1f3b7558adacf81b570e05a37010d4ddcd9fc1c2
Kristian Kjærgård <kris6673@hotmail.com>
2025-01-21 01:21:07 +0100

    Add Intune scripts page

---
+++ src/pages/endpoint/MEM/list-scripts/linux.js
@@ -0,0 +1,61 @@
+﻿import { Layout as DashboardLayout } from "/src/layouts/index.js";
+import { CippTablePage } from "/src/components/CippComponents/CippTablePage.jsx";
+import { Code, TrashIcon } from "@heroicons/react/24/outline";
+import { TabbedLayout } from "/src/layouts/TabbedLayout";
+import tabOptions from "./tabOptions";
+
+const Page = () => {
+  const pageTitle = "Linux Scripts";
+
+  const actions = [
+    {
+      label: "Delete Script",
+      type: "POST",
+      url: "/api/RemovePolicy",
+      data: {
+        ID: "id",
+        URLName: "URLName",
+      },
+      confirmText: "Are you sure you want to delete this script?",
+      icon: <TrashIcon />,
+      color: "danger",
+    },
+  ];
+
+  const offCanvas = {
+    extendedInfoFields: [
+      "id",
+      "displayName",
+      "description",
+      "lastModifiedDateTime",
+      "createdDateTime",
+    ],
+    actions: actions,
+  };
+
+  const simpleColumns = [
+    "displayName",
+    "description",
+    "runAsAccount",
+    "executionFrequency",
+    "enforceSignatureCheck",
+    "lastModifiedDateTime",
+  ];
+
+  return (
+    <CippTablePage
+      title={pageTitle}
+      apiUrl="/api/ListIntunePolicy?type=linuxScript"
+      actions={actions}
+      offCanvas={offCanvas}
+      simpleColumns={simpleColumns}
+    />
+  );
+};
+
+Page.getLayout = (page) => (
+  <DashboardLayout>
+    <TabbedLayout tabOptions={tabOptions}>{page}</TabbedLayout>
+  </DashboardLayout>
+);
+export default Page;
---
+++ src/pages/endpoint/MEM/list-scripts/remediation.js
@@ -0,0 +1,69 @@
+﻿import { Layout as DashboardLayout } from "/src/layouts/index.js";
+import { CippTablePage } from "/src/components/CippComponents/CippTablePage.jsx";
+import { Code, TrashIcon } from "@heroicons/react/24/outline";
+import { TabbedLayout } from "/src/layouts/TabbedLayout";
+import tabOptions from "./tabOptions";
+
+const Page = () => {
+  const pageTitle = "Remediation Scripts";
+
+  const actions = [
+    {
+      label: "Delete Script",
+      type: "POST",
+      url: "/api/RemoveIntuneScript",
+      data: {
+        ID: "id",
+        displayName: "displayName",
+        ScriptType: "!remediate",
+      },
+      confirmText: "Are you sure you want to delete this script?",
+      icon: <TrashIcon />,
+      color: "danger",
+    },
+  ];
+
+  const offCanvas = {
+    extendedInfoFields: [
+      "id",
+      "fileName",
+      "displayName",
+      "description",
+      "lastModifiedDateTime",
+      "createdDateTime",
+    ],
+    actions: actions,
+  };
+
+  const simpleColumns = [
+    "displayName",
+    "description",
+    "runAsAccount",
+    "runAs32Bit",
+    "enforceSignatureCheck",
+    "lastModifiedDateTime",
+  ];
+
+  return (
+    <CippTablePage
+      title={pageTitle}
+      apiUrl="/api/ListGraphRequest"
+      apiData={{
+        Endpoint: "deviceManagement/deviceHealthScripts",
+        $orderby: "displayName",
+        manualPagination: true,
+      }}
+      apiDataKey="Results"
+      actions={actions}
+      offCanvas={offCanvas}
+      simpleColumns={simpleColumns}
+    />
+  );
+};
+
+Page.getLayout = (page) => (
+  <DashboardLayout>
+    <TabbedLayout tabOptions={tabOptions}>{page}</TabbedLayout>
+  </DashboardLayout>
+);
+export default Page;
