e378097e14962207fd7179f5231392669d9b9c21
John Duprey <jwd@johnwduprey.com>
2025-02-04 13:56:41 -0500

    add sam wizard form validation

--- src/components/CippWizard/CIPPDeploymentStep.js
+++ src/components/CippWizard/CIPPDeploymentStep.js
@@ -284,6 +284,13 @@ export const CippDeploymentStep = (props) => {
               name="TenantID"
               label="Tenant ID"
               placeholder="Enter the Tenant ID. Leave blank to retain previous key."
+              validators={{
+                validate: (value) => {
+                  const guidRegex =
+                    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+                  return value === "" || guidRegex.test(value) || "Invalid Tenant ID";
+                },
+              }}
             />
             <CippFormComponent
               formControl={formControl}
@@ -291,6 +298,13 @@ export const CippDeploymentStep = (props) => {
               name="ApplicationID"
               label="Application ID"
               placeholder="Enter the Application ID. Leave blank to retain previous key."
+              validators={{
+                validate: (value) => {
+                  const guidRegex =
+                    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+                  return value === "" || guidRegex.test(value) || "Invalid Application ID";
+                },
+              }}
             />
             <CippFormComponent
               formControl={formControl}
@@ -298,6 +312,12 @@ export const CippDeploymentStep = (props) => {
               name="ApplicationSecret"
               label="Application Secret"
               placeholder="Enter the application secret. Leave blank to retain previous key."
+              validators={{
+                validate: (value) => {
+                  const secretRegex = /^(?![0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$)[0-9a-zA-Z]{40}$/;
+                  return value === "" || secretRegex.test(value) || "This should be the secret value, not the secret ID";
+                },
+              }}
             />
             <CippFormComponent
               formControl={formControl}
@@ -305,6 +325,12 @@ export const CippDeploymentStep = (props) => {
               name="RefreshToken"
               label="Refresh Token"
               placeholder="Enter the refresh token. Leave blank to retain previous key."
+              validators={{
+                validate: (value) => {
+                  const jwtRegex = /^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/;
+                  return value === "" || jwtRegex.test(value) || "Invalid Refresh Token";
+                }
+              }}
             />
           </>
         )}
