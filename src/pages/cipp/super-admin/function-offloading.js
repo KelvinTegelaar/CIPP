ed2adc06b1e82021b98cf12d4cc8a249fe714e5a
John Duprey <john@cyberdrain.com>
2025-03-15 21:14:38 -0400

    Update function-offloading.js

--- src/pages/cipp/super-admin/function-offloading.js
+++ src/pages/cipp/super-admin/function-offloading.js
@@ -73,7 +73,6 @@ const Page = () => {
             simpleColumns={["Name", "Version"]}
             refreshFunction={execOffloadFunctions.refetch}
             isFetching={execOffloadFunctions.isFetching}
-            simple={true}
           />
         </Grid>
         {execOffloadFunctions.data?.Alerts?.length > 0 && (
