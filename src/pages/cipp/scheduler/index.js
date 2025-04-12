da805ecaf591e3260a4f8dfbd2cbe41233cbfa12
John Duprey <john@cyberdrain.com>
2025-04-12 16:47:19 -0400

    fix scheduler form and add run now

--- src/pages/cipp/scheduler/index.js
+++ src/pages/cipp/scheduler/index.js
@@ -2,13 +2,21 @@ import { Layout as DashboardLayout } from "/src/layouts/index.js";
 import CippTablePage from "/src/components/CippComponents/CippTablePage";
 import { Button, Typography } from "@mui/material";
 import Link from "next/link";
-import { CalendarDaysIcon, EyeIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { CalendarDaysIcon, TrashIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
-import { CopyAll, Edit } from "@mui/icons-material";
+import { CopyAll, Edit, PlayArrow } from "@mui/icons-material";
 import { CippCodeBlock } from "../../../components/CippComponents/CippCodeBlock";
 
 const Page = () => {
   const actions = [
+    {
+      label: "Run Now",
+      type: "POST",
+      url: "/api/AddScheduledItem",
+      data: { RowKey: "RowKey", RunNow: true },
+      icon: <PlayArrow />,
+      confirmText: "Are you sure you want to run [Name]?",
+    },
     {
       label: "Edit Job",
       link: "/cipp/scheduler/job?id=[RowKey]",
