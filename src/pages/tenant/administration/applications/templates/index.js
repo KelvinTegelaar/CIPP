20fa9bf6c287e750b61e724f08d7366ad8fd9409
John Duprey <john@cyberdrain.com>
2025-05-20 09:48:32 -0400

    add save to github action

--- src/pages/tenant/administration/applications/templates/index.js
+++ src/pages/tenant/administration/applications/templates/index.js
@@ -1,14 +1,23 @@
 import { Layout as DashboardLayout } from "/src/layouts/index.js";
 import { TabbedLayout } from "/src/layouts/TabbedLayout";
 import { CippTablePage } from "/src/components/CippComponents/CippTablePage.jsx";
-import { Edit, Delete, ContentCopy, Add } from "@mui/icons-material";
+import { Edit, Delete, ContentCopy, Add, GitHub } from "@mui/icons-material";
 import tabOptions from "../tabOptions";
 import { Button } from "@mui/material";
 import Link from "next/link";
+import { ApiGetCall } from "/src/api/ApiCall";
 
 const Page = () => {
   const pageTitle = "Templates";
   const apiUrl = "/api/ListAppApprovalTemplates";
+
+  // Fetch GitHub integration status
+  const integrations = ApiGetCall({
+    url: "/api/ListExtensionsConfig",
+    queryKey: "Integrations",
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+  });
 
   const actions = [
     {
@@ -22,6 +31,51 @@ const Page = () => {
       label: "Copy Template",
       color: "info",
       link: "/tenant/administration/applications/templates/add?template=[TemplateId]&copy=true&name=[TemplateName]",
+    },
+    {
+      icon: <GitHub />,
+      label: "Save to GitHub",
+      type: "POST",
+      url: "/api/ExecCommunityRepo",
+      data: {
+        Action: "UploadTemplate",
+        GUID: "TemplateId",
+        TemplateType: "AppApproval",
+      },
+      fields: [
+        {
+          label: "Repository",
+          name: "FullName",
+          type: "select",
+          api: {
+            url: "/api/ListCommunityRepos",
+            data: {
+              WriteAccess: true,
+            },
+            queryKey: "CommunityRepos-Write",
+            dataKey: "Results",
+            valueField: "FullName",
+            labelField: "FullName",
+          },
+          multiple: false,
+          creatable: false,
+          required: true,
+          validators: {
+            required: { value: true, message: "This field is required" },
+          },
+        },
+        {
+          label: "Commit Message",
+          placeholder: "Enter a commit message for adding this file to GitHub",
+          name: "Message",
+          type: "textField",
+          multiline: true,
+          required: true,
+          rows: 4,
+        },
+      ],
+      confirmText: "Are you sure you want to save this template to the selected repository?",
+      condition: () => integrations.isSuccess && integrations?.data?.GitHub?.Enabled,
     },
     {
       icon: <Delete />,
