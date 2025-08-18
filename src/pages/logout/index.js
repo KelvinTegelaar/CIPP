f5c2789110e4fc359e6b770efe58a792eeb2d9fd
ONI ADEMOLA <wiredmedia.digital@gmail.com>
2025-08-18 18:13:49 -0400

    Update index.js
    Signed-off-by: ONI ADEMOLA <wiredmedia.digital@gmail.com>

--- src/pages/logout/index.js
+++ src/pages/logout/index.js
@@ -1,42 +1,13 @@
-
-import { useCallback } from "react";
 import { Layout as DashboardLayout } from "/src/layouts/index.js";
-import {
-  Box,
-  Button,
-  Card,
-  CardContent,
-  Container,
-  Typography
-} from "@mui/material";
 
 const Page = () => {
-  const handleSignOut = useCallback(() => {
-    window.location.href =
-      "https://login.microsoftonline.com/5197ded8-c845-4a45-8c20-09122ae12852/oauth2/v2.0/logout?post_logout_redirect_uri=https://cipp.cloudvanguard-it.com/login";
-  }, []);
+  const pageTitle = "Log Out";
 
   return (
-    <Container maxWidth="sm" sx={{ mt: 10 }}>
-      <Card sx={{ p: 3, textAlign: "center" }}>
-        <CardContent>
-          <Typography variant="h4" gutterBottom>
-            Sign out
-          </Typography>
-          <Typography variant="body1" sx={{ mb: 3 }}>
-            You are about to sign out of your Microsoft account.
-          </Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            size="large"
-            onClick={handleSignOut}
-          >
-            Sign out
-          </Button>
-        </CardContent>
-      </Card>
-    </Container>
+    <div>
+      <h1>{pageTitle}</h1>
+      <p>This is a placeholder page for the log out section.</p>
+    </div>
   );
 };
 
