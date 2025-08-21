4d7dcb51b352f20872b74b7f95f40e888305a57a
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2025-08-21 12:42:50 +0200

    added edu licenses which don't show up in the default files

--- src/utils/get-cipp-license-translation.js
+++ src/utils/get-cipp-license-translation.js
@@ -1,6 +1,9 @@
-import M365Licenses from "../data/M365Licenses.json";
+import M365LicensesDefault from "../data/M365Licenses.json";
+import M365LicensesAdditional from "../data/M365Licenses-additional.json";
 
 export const getCippLicenseTranslation = (licenseArray) => {
+  //combine M365LicensesDefault and M365LicensesAdditional to one array
+  const M365Licenses = [...M365LicensesDefault, ...M365LicensesAdditional];
   let licenses = [];
 
   if (!Array.isArray(licenseArray) && typeof licenseArray === "object") {
