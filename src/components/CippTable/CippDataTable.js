f6383a62a57e4419603212fcc04cc8fe711703e9
John Duprey <john@cyberdrain.com>
2025-09-16 20:54:33 -0400

    fix numeric sorting

--- src/components/CippTable/CippDataTable.js
+++ src/components/CippTable/CippDataTable.js
@@ -459,6 +459,20 @@ export const CippDataTable = (props) => {
         }
         return aVal > bVal ? 1 : -1;
       },
+      number: (a, b, id) => {
+        const aVal = a?.original?.[id] ?? null;
+        const bVal = b?.original?.[id] ?? null;
+        if (aVal === null && bVal === null) {
+          return 0;
+        }
+        if (aVal === null) {
+          return 1;
+        }
+        if (bVal === null) {
+          return -1;
+        }
+        return aVal - bVal;
+      },
     },
     filterFns: {
       notContains: (row, columnId, value) => {
