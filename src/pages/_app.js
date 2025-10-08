c7a87f2b5c97f59fa16a977abe48818800e3002b
John Duprey <john@cyberdrain.com>
2025-10-08 12:14:41 -0400

    add clear cache button

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -27,6 +27,7 @@ import {
   AutoStories,
   Gavel,
   Celebration,
+  ClearAll as ClearAllIcon,
 } from "@mui/icons-material";
 import { SvgIcon } from "@mui/material";
 import discordIcon from "../../public/discord-mark-blue.svg";
@@ -196,6 +197,29 @@ const App = (props) => {
           },
         ]
       : []), // toRemove
+    {
+      // add clear cache action that removes the persisted query cache from local storage and reloads the page
+      id: "clearCache",
+      icon: <ClearAllIcon />,
+      name: "Clear Cache",
+      onClick: () => {
+        // Clear the TanStack Query cache
+        queryClient.clear();
+
+        // Remove persisted cache from localStorage
+        if (typeof window !== "undefined") {
+          // Remove the persisted query cache keys
+          Object.keys(localStorage).forEach((key) => {
+            if (key.startsWith("REACT_QUERY_OFFLINE_CACHE")) {
+              localStorage.removeItem(key);
+            }
+          });
+        }
+
+        // Reload the page to ensure clean state
+        window.location.reload();
+      },
+    },
     {
       id: "license",
       icon: <Gavel />,
