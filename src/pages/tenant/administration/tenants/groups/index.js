5bb4349f4d3bf2b4d73187ab167bcd5417dd5044
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2025-10-23 12:16:05 +0200

    automated onboarding, default group creation

--- src/pages/tenant/administration/tenants/groups/index.js
+++ src/pages/tenant/administration/tenants/groups/index.js
@@ -2,15 +2,18 @@ import { Layout as DashboardLayout } from "/src/layouts/index.js";
 import { TabbedLayout } from "/src/layouts/TabbedLayout";
 import { CippTablePage } from "/src/components/CippComponents/CippTablePage.jsx";
 import tabOptions from "../tabOptions";
-import { Edit, PlayArrow } from "@mui/icons-material";
+import { Edit, PlayArrow, GroupAdd } from "@mui/icons-material";
 import { TrashIcon } from "@heroicons/react/24/outline";
 import { CippAddTenantGroupDrawer } from "/src/components/CippComponents/CippAddTenantGroupDrawer";
 import { CippApiLogsDrawer } from "/src/components/CippComponents/CippApiLogsDrawer";
 import { CippTenantGroupOffCanvas } from "/src/components/CippComponents/CippTenantGroupOffCanvas";
-import { Box } from "@mui/material";
+import { CippApiDialog } from "/src/components/CippComponents/CippApiDialog.jsx";
+import { Box, Button } from "@mui/material";
+import { useDialog } from "/src/hooks/use-dialog.js";
 
 const Page = () => {
   const pageTitle = "Tenant Groups";
+  const createDefaultGroupsDialog = useDialog();
 
   const simpleColumns = ["Name", "Description", "GroupType", "Members"];
 
@@ -48,26 +51,45 @@ const Page = () => {
   ];
 
   return (
-    <CippTablePage
-      title={pageTitle}
-      tenantInTitle={false}
-      simpleColumns={simpleColumns}
-      apiUrl="/api/ListTenantGroups"
-      queryKey="TenantGroupListPage"
-      apiDataKey="Results"
-      actions={actions}
-      cardButton={
-        <Box sx={{ display: "flex", gap: 1 }}>
-          <CippAddTenantGroupDrawer />
-          <CippApiLogsDrawer
-            apiFilter="TenantGroups"
-            buttonText="View Logs"
-            title="Tenant Groups Logs"
-          />
-        </Box>
-      }
-      offCanvas={offcanvas}
-    />
+    <>
+      <CippTablePage
+        title={pageTitle}
+        tenantInTitle={false}
+        simpleColumns={simpleColumns}
+        apiUrl="/api/ListTenantGroups"
+        queryKey="TenantGroupListPage"
+        apiDataKey="Results"
+        actions={actions}
+        cardButton={
+          <Box sx={{ display: "flex", gap: 1 }}>
+            <CippAddTenantGroupDrawer />
+            <Button
+              onClick={createDefaultGroupsDialog.handleOpen}
+              startIcon={<GroupAdd />}
+            >
+              Create Default Groups
+            </Button>
+            <CippApiLogsDrawer
+              apiFilter="TenantGroups"
+              buttonText="View Logs"
+              title="Tenant Groups Logs"
+            />
+          </Box>
+        }
+        offCanvas={offcanvas}
+      />
+      <CippApiDialog
+        title="Create Default Groups"
+        createDialog={createDefaultGroupsDialog}
+        api={{
+          type: "POST",
+          url: "/api/ExecCreateDefaultGroups",
+          data: {},
+          confirmText: "Are you sure you want to create default tenant groups? This will create standard groups like 'All Tenants', 'Enterprise Tenants', etc.",
+        }}
+        queryKey="TenantGroupListPage"
+      />
+    </>
   );
 };
 
