cefd906f974b1aefe78f7ea80c7d1ae809db10d6
John Duprey <jwd@johnwduprey.com>
2025-12-19 13:14:21 -0500

    Refactor standards pages and update alignment routes
    Moved and renamed standards-related pages for better organization, including aligning routes under /tenant/standards/alignment and /tenant/standards/templates. Updated navigation paths and tab options accordingly. Enhanced the alignment page with new drift management actions and removed obsolete files.

--- src/pages/tenant/standards/alignment/index.js
+++ src/pages/tenant/standards/alignment/index.js
@@ -1,11 +1,12 @@
 import { CippTablePage } from "/src/components/CippComponents/CippTablePage.jsx";
 import { Layout as DashboardLayout } from "/src/layouts/index.js";
 import { TabbedLayout } from "/src/layouts/TabbedLayout";
+import { Delete, Add } from "@mui/icons-material";
 import { EyeIcon } from "@heroicons/react/24/outline";
 import tabOptions from "../tabOptions.json";
 
 const Page = () => {
-  const pageTitle = "Drift Alignment";
+  const pageTitle = "Standard & Drift Alignment";
 
   const actions = [
     {
@@ -15,6 +16,28 @@ const Page = () => {
       color: "info",
       target: "_self",
     },
+    {
+      label: "Manage Drift",
+      link: "/tenant/manage/drift?templateId=[standardId]&tenantFilter=[tenantFilter]",
+      icon: <EyeIcon />,
+      color: "info",
+      target: "_self",
+      condition: (row) => row.standardType === "drift",
+    },
+    {
+      label: "Remove Drift Customization",
+      type: "POST",
+      url: "/api/ExecUpdateDriftDeviation",
+      icon: <Delete />,
+      data: {
+        RemoveDriftCustomization: "true",
+        tenantFilter: "tenantFilter",
+      },
+      confirmText:
+        "Are you sure you want to remove all drift customizations? This resets the Drift Standard to the default template, and will generate alerts for the drifted items.",
+      multiPost: false,
+      condition: (row) => row.standardType === "drift",
+    },
   ];
 
   return (
@@ -23,15 +46,15 @@ const Page = () => {
       apiUrl="/api/ListTenantAlignment"
       tenantInTitle={false}
       actions={actions}
-      tableFilter={<div></div>}
       simpleColumns={[
         "tenantFilter",
         "standardName",
+        "standardType",
         "alignmentScore",
         "LicenseMissingPercentage",
         "combinedAlignmentScore",
       ]}
-      queryKey="listDriftAlignment"
+      queryKey="listTenantAlignment"
     />
   );
 };
