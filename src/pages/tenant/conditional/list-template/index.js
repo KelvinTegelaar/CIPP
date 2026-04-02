88bc10a527efe58a986bf96c551c07c74c3f6ab5
James Tarran <jtarran@techary.com>
2026-04-02 19:03:48 +0100

    Added UI Elements for adding conditional access policies to package tags
    Added UI Elements for adding conditional access policies to package tags

--- src/pages/tenant/conditional/list-template/index.js
+++ src/pages/tenant/conditional/list-template/index.js
@@ -2,7 +2,7 @@ import { Layout as DashboardLayout } from "../../../../layouts/index.js";
 import { CippTablePage } from "../../../../components/CippComponents/CippTablePage.jsx";
 import { Button, Box } from "@mui/material";
 import CippJsonView from "../../../../components/CippFormPages/CippJSONView";
-import { Delete, GitHub, Edit, RocketLaunch } from "@mui/icons-material";
+import { Delete, GitHub, Edit, RocketLaunch, LocalOffer, LocalOfferOutlined } from "@mui/icons-material";
 import { ApiGetCall } from "../../../../api/ApiCall";
 import { CippPolicyImportDrawer } from "../../../../components/CippComponents/CippPolicyImportDrawer.jsx";
 import { CippCADeployDrawer } from "../../../../components/CippComponents/CippCADeployDrawer.jsx";
@@ -41,6 +41,37 @@ const Page = () => {
       link: "/tenant/conditional/list-template/edit?GUID=[GUID]",
       icon: <Edit />,
       color: "info",
+    },
+    {
+      label: "Add to package",
+      type: "POST",
+      url: "/api/ExecSetPackageTag",
+      data: { GUID: "GUID" },
+      fields: [
+        {
+          type: "textField",
+          name: "Package",
+          label: "Package Name",
+          required: true,
+          validators: {
+            required: { value: true, message: "Package name is required" },
+          },
+        },
+      ],
+      confirmText: "Enter the package name to assign to the selected template(s).",
+      multiPost: true,
+      icon: <LocalOffer />,
+      color: "info",
+    },
+    {
+      label: "Remove from package",
+      type: "POST",
+      url: "/api/ExecSetPackageTag",
+      data: { GUID: "GUID", Remove: true },
+      confirmText: "Are you sure you want to remove the selected template(s) from their package?",
+      multiPost: true,
+      icon: <LocalOfferOutlined />,
+      color: "warning",
     },
     {
       label: "Save to GitHub",
@@ -110,7 +141,7 @@ const Page = () => {
         queryKey="ListCATemplates-table"
         actions={actions}
         offCanvas={offCanvas}
-        simpleColumns={["displayName", "GUID"]}
+        simpleColumns={["displayName", "package", "GUID"]}
         cardButton={
           <Box sx={{ display: "flex", gap: 1 }}>
             <Button key="template-lib" href="/cipp/template-library" title="Add Template Library" />
