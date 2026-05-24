d4f458a15b67d7c8cdd8b8095d3a638a040a9dd4
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2026-05-24 23:07:33 +0200

    Third party text

--- src/pages/cipp/integrations/index.js
+++ src/pages/cipp/integrations/index.js
@@ -141,7 +141,7 @@ const Page = () => {
                           height: 8,
                         }}
                       />
-                      <Typography variant="body2">Coming Soon through Third-Party</Typography>
+                      <Typography variant="body2">Coming Soon Through Third-Party</Typography>
                     </>
                   ) : (
                     <>
