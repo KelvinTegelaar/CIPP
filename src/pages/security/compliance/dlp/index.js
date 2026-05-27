7a40854272a60617d57a28570406953a9cca913c
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2026-05-27 20:37:02 +0200

    fix query keys

--- src/pages/security/compliance/dlp/index.js
+++ src/pages/security/compliance/dlp/index.js
@@ -1,99 +1,101 @@
-import { Layout as DashboardLayout } from "../../../../layouts/index.js";
-import { CippTablePage } from "../../../../components/CippComponents/CippTablePage.jsx";
-import { Book, Block, Check } from "@mui/icons-material";
-import { TrashIcon } from "@heroicons/react/24/outline";
-import { CippDeployCompliancePolicyDrawer } from "../../../../components/CippComponents/CippDeployCompliancePolicyDrawer.jsx";
-import { PermissionButton } from "../../../../utils/permissions.js";
+import { Layout as DashboardLayout } from '../../../../layouts/index.js'
+import { CippTablePage } from '../../../../components/CippComponents/CippTablePage.jsx'
+import { Book, Block, Check } from '@mui/icons-material'
+import { TrashIcon } from '@heroicons/react/24/outline'
+import { CippDeployCompliancePolicyDrawer } from '../../../../components/CippComponents/CippDeployCompliancePolicyDrawer.jsx'
+import { PermissionButton } from '../../../../utils/permissions.js'
+import { useSettings } from '../../../../hooks/use-settings'
 
 const Page = () => {
-  const pageTitle = "DLP Compliance Policies";
-  const apiUrl = "/api/ListDlpCompliancePolicy";
-  const cardButtonPermissions = ["Security.DlpCompliancePolicy.ReadWrite"];
+  const pageTitle = 'DLP Compliance Policies'
+  const apiUrl = '/api/ListDlpCompliancePolicy'
+  const tenantFilter = useSettings().currentTenant
+  const cardButtonPermissions = ['Security.DlpCompliancePolicy.ReadWrite']
 
   const actions = [
     {
-      label: "Create template based on policy",
-      type: "POST",
+      label: 'Create template based on policy',
+      type: 'POST',
       icon: <Book />,
-      url: "/api/AddDlpCompliancePolicyTemplate",
+      url: '/api/AddDlpCompliancePolicyTemplate',
       dataFunction: (data) => {
-        return { ...data };
+        return { ...data }
       },
-      confirmText: "Are you sure you want to create a template based on this DLP policy?",
+      confirmText: 'Are you sure you want to create a template based on this DLP policy?',
     },
     {
-      label: "Enable Policy",
-      type: "POST",
+      label: 'Enable Policy',
+      type: 'POST',
       icon: <Check />,
-      url: "/api/EditDlpCompliancePolicy",
+      url: '/api/EditDlpCompliancePolicy',
       data: {
-        State: "!enable",
-        Identity: "Name",
+        State: '!enable',
+        Identity: 'Name',
       },
-      confirmText: "Are you sure you want to enable this DLP policy?",
+      confirmText: 'Are you sure you want to enable this DLP policy?',
       condition: (row) => row.Enabled === false,
     },
     {
-      label: "Disable Policy",
-      type: "POST",
+      label: 'Disable Policy',
+      type: 'POST',
       icon: <Block />,
-      url: "/api/EditDlpCompliancePolicy",
+      url: '/api/EditDlpCompliancePolicy',
       data: {
-        State: "!disable",
-        Identity: "Name",
+        State: '!disable',
+        Identity: 'Name',
       },
-      confirmText: "Are you sure you want to disable this DLP policy?",
+      confirmText: 'Are you sure you want to disable this DLP policy?',
       condition: (row) => row.Enabled === true,
     },
     {
-      label: "Delete Policy",
-      type: "POST",
+      label: 'Delete Policy',
+      type: 'POST',
       icon: <TrashIcon />,
-      url: "/api/RemoveDlpCompliancePolicy",
+      url: '/api/RemoveDlpCompliancePolicy',
       data: {
-        Identity: "Name",
+        Identity: 'Name',
       },
-      confirmText: "Are you sure you want to delete this DLP policy?",
-      color: "danger",
+      confirmText: 'Are you sure you want to delete this DLP policy?',
+      color: 'danger',
     },
-  ];
+  ]
 
   const offCanvas = {
     extendedInfoFields: [
-      "Name",
-      "Comment",
-      "Mode",
-      "Enabled",
-      "Workload",
-      "ExchangeLocation",
-      "SharePointLocation",
-      "OneDriveLocation",
-      "TeamsLocation",
-      "EndpointDlpLocation",
-      "RuleCount",
-      "CreatedBy",
-      "WhenCreatedUTC",
-      "WhenChangedUTC",
+      'Name',
+      'Comment',
+      'Mode',
+      'Enabled',
+      'Workload',
+      'ExchangeLocation',
+      'SharePointLocation',
+      'OneDriveLocation',
+      'TeamsLocation',
+      'EndpointDlpLocation',
+      'RuleCount',
+      'CreatedBy',
+      'WhenCreatedUTC',
+      'WhenChangedUTC',
     ],
     actions: actions,
-  };
+  }
 
   const simpleColumns = [
-    "Name",
-    "Mode",
-    "Enabled",
-    "Workload",
-    "RuleCount",
-    "CreatedBy",
-    "WhenCreatedUTC",
-    "WhenChangedUTC",
-  ];
+    'Name',
+    'Mode',
+    'Enabled',
+    'Workload',
+    'RuleCount',
+    'CreatedBy',
+    'WhenCreatedUTC',
+    'WhenChangedUTC',
+  ]
 
   return (
     <CippTablePage
       title={pageTitle}
       apiUrl={apiUrl}
-      queryKey="ListDlpCompliancePolicy"
+      queryKey={`ListDlpCompliancePolicy-${tenantFilter}`}
       actions={actions}
       offCanvas={offCanvas}
       simpleColumns={simpleColumns}
@@ -105,8 +107,8 @@ const Page = () => {
         />
       }
     />
-  );
-};
+  )
+}
 
-Page.getLayout = (page) => <DashboardLayout allTenantsSupport={false}>{page}</DashboardLayout>;
-export default Page;
+Page.getLayout = (page) => <DashboardLayout allTenantsSupport={false}>{page}</DashboardLayout>
+export default Page
--- src/pages/security/compliance/labels/index.js
+++ src/pages/security/compliance/labels/index.js
@@ -1,78 +1,80 @@
-import { Layout as DashboardLayout } from "../../../../layouts/index.js";
-import { CippTablePage } from "../../../../components/CippComponents/CippTablePage.jsx";
-import { Book } from "@mui/icons-material";
-import { TrashIcon } from "@heroicons/react/24/outline";
-import { CippDeployCompliancePolicyDrawer } from "../../../../components/CippComponents/CippDeployCompliancePolicyDrawer.jsx";
-import { PermissionButton } from "../../../../utils/permissions.js";
+import { Layout as DashboardLayout } from '../../../../layouts/index.js'
+import { CippTablePage } from '../../../../components/CippComponents/CippTablePage.jsx'
+import { Book } from '@mui/icons-material'
+import { TrashIcon } from '@heroicons/react/24/outline'
+import { CippDeployCompliancePolicyDrawer } from '../../../../components/CippComponents/CippDeployCompliancePolicyDrawer.jsx'
+import { PermissionButton } from '../../../../utils/permissions.js'
+import { useSettings } from '../../../../hooks/use-settings'
 
 const Page = () => {
-  const pageTitle = "Sensitivity Labels";
-  const apiUrl = "/api/ListSensitivityLabel";
-  const cardButtonPermissions = ["Security.SensitivityLabel.ReadWrite"];
+  const pageTitle = 'Sensitivity Labels'
+  const apiUrl = '/api/ListSensitivityLabel'
+  const tenantFilter = useSettings().currentTenant
+  const cardButtonPermissions = ['Security.SensitivityLabel.ReadWrite']
 
   const actions = [
     {
-      label: "Create template based on label",
-      type: "POST",
+      label: 'Create template based on label',
+      type: 'POST',
       icon: <Book />,
-      url: "/api/AddSensitivityLabelTemplate",
+      url: '/api/AddSensitivityLabelTemplate',
       dataFunction: (data) => {
-        return { ...data };
+        return { ...data }
       },
-      confirmText: "Are you sure you want to create a template based on this sensitivity label?",
+      confirmText: 'Are you sure you want to create a template based on this sensitivity label?',
     },
     {
-      label: "Delete Label",
-      type: "POST",
+      label: 'Delete Label',
+      type: 'POST',
       icon: <TrashIcon />,
-      url: "/api/RemoveSensitivityLabel",
+      url: '/api/RemoveSensitivityLabel',
       data: {
-        Identity: "Guid",
+        Identity: 'Guid',
       },
       confirmText:
-        "Are you sure you want to delete this sensitivity label? Labels currently published to users will be removed from policies.",
-      color: "danger",
+        'Are you sure you want to delete this sensitivity label? Labels currently published to users will be removed from policies.',
+      color: 'danger',
     },
-  ];
+  ]
 
   const offCanvas = {
     extendedInfoFields: [
-      "DisplayName",
-      "Name",
-      "Comment",
-      "Tooltip",
-      "ParentId",
-      "ContentType",
-      "EncryptionEnabled",
-      "EncryptionProtectionType",
-      "ContentMarkingHeaderEnabled",
-      "ContentMarkingFooterEnabled",
-      "ContentMarkingWatermarkEnabled",
-      "SiteAndGroupProtectionEnabled",
-      "Priority",
-      "Disabled",
-      "PublishedInPolicies",
+      'DisplayName',
+      'Name',
+      'Comment',
+      'Tooltip',
+      'ParentId',
+      'ContentType',
+      'EncryptionEnabled',
+      'EncryptionProtectionType',
+      'ContentMarkingHeaderEnabled',
+      'ContentMarkingFooterEnabled',
+      'ContentMarkingWatermarkEnabled',
+      'SiteAndGroupProtectionEnabled',
+      'Priority',
+      'Disabled',
+      'PublishedInPolicies',
     ],
     actions: actions,
-  };
+  }
 
   const simpleColumns = [
-    "DisplayName",
-    "Name",
-    "ContentType",
-    "EncryptionEnabled",
-    "ContentMarkingHeaderEnabled",
-    "ContentMarkingWatermarkEnabled",
-    "SiteAndGroupProtectionEnabled",
-    "Priority",
-    "Disabled",
-  ];
+    'DisplayName',
+    'Name',
+    'ContentType',
+    'EncryptionEnabled',
+    'ContentMarkingHeaderEnabled',
+    'ContentMarkingWatermarkEnabled',
+    'SiteAndGroupProtectionEnabled',
+    'Priority',
+    'Disabled',
+  ]
 
   return (
     <CippTablePage
       title={pageTitle}
       apiUrl={apiUrl}
-      queryKey="ListSensitivityLabel"
+      queryKey={`ListSensitivityLabel-${tenantFilter}`}
       actions={actions}
       offCanvas={offCanvas}
       simpleColumns={simpleColumns}
@@ -84,8 +86,8 @@ const Page = () => {
         />
       }
     />
-  );
-};
+  )
+}
 
-Page.getLayout = (page) => <DashboardLayout allTenantsSupport={false}>{page}</DashboardLayout>;
-export default Page;
+Page.getLayout = (page) => <DashboardLayout allTenantsSupport={false}>{page}</DashboardLayout>
+export default Page
