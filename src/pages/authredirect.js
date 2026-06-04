67039061cb901709c0cd35b2baa1fca1026cdc1e
KelvinTegelaar <49186168+KelvinTegelaar@users.noreply.github.com>
2026-06-04 21:52:28 +0200

    Auth changes to use sedndmessage

--- src/pages/authredirect.js
+++ src/pages/authredirect.js
@@ -1,40 +1,39 @@
-import { useEffect } from "react";
-import Head from "next/head";
+import { useEffect } from 'react'
+import Head from 'next/head'
 
 const Page = () => {
   useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const code = params.get("code");
-    const state = params.get("state");
-    const error = params.get("error");
-    const errorDescription = params.get("error_description");
+    const params = new URLSearchParams(window.location.search)
+    const code = params.get('code')
+    const state = params.get('state')
+    const error = params.get('error')
+    const errorDescription = params.get('error_description')
 
     if (window.opener) {
       if (code && state) {
-        window.opener.postMessage(
-          { type: "auth_code", code, state },
-          window.location.origin
-        );
+        window.opener.postMessage({ type: 'auth_code', code, state }, window.location.origin)
       } else if (error) {
         window.opener.postMessage(
-          { type: "auth_error", error, errorDescription },
+          { type: 'auth_error', error, errorDescription },
           window.location.origin
-        );
+        )
       }
-      window.close();
+      window.close()
     }
-  }, []);
+  }, [])
 
   return (
     <>
       <Head>
         <title>Authentication complete</title>
       </Head>
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <div
+        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}
+      >
         <p>Authentication complete. This window will close automatically.</p>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
